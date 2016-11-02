c4f00b415dc1661baf20e63f73b50741fc740a76
kmelve <knut@netliferesearch.com>
2016-11-02 13:15:19 +0100

    add inuitcss config

--- packages/ndla-ui/.storybook/config.js
+++ packages/ndla-ui/.storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@kadira/storybook';
 import { setOptions } from '@kadira/storybook-addon-options';
-import '../src/index.css';
+import '../src/index.scss';
 
 setOptions({
   name: 'NDLA Styleguide',

e89511f0be6b2014e05cd1e590dcc50cd5270c80
kmelve <knut@netliferesearch.com>
2016-11-16 10:37:53 +0100

    add more atoms

--- packages/ndla-ui/stories/atoms.jsx
+++ packages/ndla-ui/stories/atoms.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { storiesOf, action } from '@kadira/storybook';
-
+import article from '../dummydata/index';
 import { Center, InlineContainer } from './helpers';
-import { Aside, Button, Table } from '../src';
+import { Aside, Button } from '../src';
 
+const articleHTML = document.createElement('div');
+articleHTML.innerHTML = article.article4.content[0].content;
 
-storiesOf('Atomer', module)
+storiesOf('Enkle komponenter', module)
   .add('Knapper', () => (
     <Center>
       <h1 className="e-heading">Knapper</h1>
@@ -28,6 +30,7 @@ storiesOf('Atomer', module)
       <h1 className="e-heading">Faktaboks</h1>
       <p>Faktabokser er for ekstra informasjon og veier videre.</p>
       <Aside>Ekstra informasjon</Aside>
+      <div dangerouslySetInnerHTML={{ __html: articleHTML.outerHTML }} />
     </Center>
   ))
   ;

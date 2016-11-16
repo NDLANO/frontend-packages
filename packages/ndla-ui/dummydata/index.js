7a8e6be08b3cd52746fceb27066a1eca367227d5
kmelve <knut@netliferesearch.com>
2016-11-16 10:34:20 +0100

    add index

---
+++ packages/ndla-ui/dummydata/index.js
@@ -0,0 +1,13 @@
+import article4 from './article4.json';
+import article89 from './article89.json';
+import article93 from './article93.json';
+import article149 from './article149.json';
+
+const article = {
+  article4,
+  article89,
+  article93,
+  article149,
+};
+
+export default article;

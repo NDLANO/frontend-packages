395c4737e745b77537f343ae6e57bbbfd992d5b3
kmelve <knut@netliferesearch.com>
2016-11-23 14:25:53 +0100

    fix styling

--- packages/ndla-ui/stories/atoms.jsx
+++ packages/ndla-ui/stories/atoms.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import article from '../dummydata/index';
 import { Center, InlineContainer } from './helpers';
-import { Aside, Button } from '../src';
+import { Button } from '../src';
 
 const articleHTML = document.createElement('div');
 articleHTML.innerHTML = article.article4.content[0].content;
@@ -11,8 +11,11 @@ articleHTML.innerHTML = article.article4.content[0].content;
 storiesOf('Enkle komponenter', module)
   .add('Knapper', () => (
     <Center>
-      <h1 className="u-heading">Knapper</h1>
-      <p>Knapper er til å klikke på, ikke for å lenke til.</p>
+      <section className="c-factbox">
+        <h1 className="u-heading">Knapper</h1>
+        <p>Knapper er til å klikke på, ikke for å lenke til.</p>
+      </section>
+      <h2 className="u-heading">Eksempel</h2>
       <InlineContainer>
         <Button onClick={action('clicked')}>Knapp</Button>
         <Button outline onClick={action('clicked')}>Knapp</Button>
@@ -27,16 +30,21 @@ storiesOf('Enkle komponenter', module)
   ))
   .add('Boks i tekst', () => (
     <Center>
-      <h1 className="u-heading">Boks i tekst</h1>
-      <p>Boks i tekst</p>
+      <section className="c-factbox">
+        <h1 className="u-heading">Boks i tekst</h1>
+        <p>Boks i tekst</p>
+      </section>
+      <h2 className="u-heading">Eksempel</h2>
       <div className="c-bodybox">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, praesentium.</div>
     </Center>
   ))
   .add('Faktaboks', () => (
     <Center>
-      <h1 className="u-heading">Faktaboks</h1>
-      <p>Faktabokser er for ekstra informasjon og veier videre.</p>
-      <Aside>Ekstra informasjon</Aside>
+      <section className="c-factbox">
+        <h1 className="u-heading">Faktaboks</h1>
+        <p>Faktabokser er for ekstra informasjon og veier videre.</p>
+      </section>
+      <h2 className="u-heading">Eksempel</h2>
       <div dangerouslySetInnerHTML={{ __html: articleHTML.outerHTML }} />
     </Center>
   ))

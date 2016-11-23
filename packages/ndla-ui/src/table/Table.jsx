c8efd568d2f4ee117a21b615df56ef03ba2ac4e5
kmelve <knut@netliferesearch.com>
2016-11-23 14:25:40 +0100

    remove table component

--- packages/ndla-ui/src/table/Table.jsx
+++ packages/ndla-ui/src/table/Table.jsx
@@ -1,8 +1,6 @@
 import React, { PropTypes } from 'react';
 import BEMHelper from 'react-bem-helper';
 
-require('./table.scss');
-
 const classes = new BEMHelper('c-table');
 
 export const Table = ({ ...rest }) => <table {...classes({ extra: ['o-table'] })}>{rest.children}</table>;

c9b09d6a5c02a19c06feccd6806f76fc23f2d77b
kmelve <knut@netliferesearch.com>
2016-11-30 12:45:02 +0100

    add datehelper from ndla-frontend

--- packages/ndla-ui/stories/helpers.jsx
+++ packages/ndla-ui/stories/helpers.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import format from 'date-fns/format';
 
 export const Center = ({ children, style }) =>
   <div style={{ margin: '0 auto', padding: '10px', maxWidth: '900px', ...style }}>{ children }</div>;
@@ -21,3 +22,10 @@ DottedContainer.propTypes = {
 };
 
 export const articleUrl = id => <span>Hentet fra <a href={`http://api.test.ndla.no:8082/article/${id}`}>{`http://api.test.ndla.no:8082/article/${id}`}</a></span>;
+
+export default function formatDate(date, locale) {
+  if (locale === 'nb' || locale === 'nn') {
+    return format(date, 'DD.MM.YYYY');
+  }
+  return format(date, 'MM/DD/YYYY');
+}

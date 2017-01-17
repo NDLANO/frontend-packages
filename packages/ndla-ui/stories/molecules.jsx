e8022cb4eb5102552261363ad2d15afa644cd309
kmelve <knut@netliferesearch.com>
2017-01-17 15:17:49 +0100

    max-width on aside toggle button

--- packages/ndla-ui/stories/molecules.jsx
+++ packages/ndla-ui/stories/molecules.jsx
@@ -121,7 +121,7 @@ storiesOf('Sammensatte moduler', module)
           <p><a href="http://www.vilbli.no" title="Les mer på Vilbli.no">Les mer på Vilbli.no</a></p>  
           <p>Landslaget for medieundervisning har en god oversikt over</p> <p><a href="http://www.mediepedagogene.no/undervisning-og-utdanning/medieutdanning?wpmp_switcher=mobile" title="medieutdanning i Norge">medieutdanning i Norge</a>.</p></div>
       </Aside>
-      <Button onClick={toggleAside}>Åpne</Button>
+      <Button className="c-aside__button" onClick={toggleAside}>Åpne</Button>
     </Center>
   ))
   .add('Sidenavigasjon', () => (

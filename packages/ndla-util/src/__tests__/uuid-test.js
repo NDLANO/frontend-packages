157e0e0340c47c9fd107da48f2522e14350d686a
Øyvind Marthinsen <oma@knowit.no>
2017-01-20 15:52:44 +0100

    Add ndla util package

---
+++ packages/ndla-util/src/__tests__/uuid-test.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+ /* eslint-env jest */
+
+import { uuid } from '../index';
+
+test('ndla-util/uuid sanity test', () => {
+  expect(uuid()).not.toEqual(uuid());
+});

d47728640da516e77901d4813f3efa427a69b804
thomaslgj <thomas@netliferesearch.com>
2017-02-03 14:09:03 +0100

    Add articles for license display (image,video), style license in figcaption

--- packages/ndla-ui/stories/pages.jsx
+++ packages/ndla-ui/stories/pages.jsx
@@ -4,7 +4,9 @@ import { storiesOf } from '@kadira/storybook';
 
 import Tabs from 'ndla-tabs';
 
-import { SiteNav, PageContainer, SiteNavItem, Masthead, MastheadItem, Logo, Footer, Hero, OneColumn } from '../src';
+import { CC, BY, NC, ND, SA, getLicenseByAbbreviation } from 'ndla-licenses';
+
+import { SiteNav, PageContainer, SiteNavItem, Masthead, MastheadItem, Logo, Footer, Hero, OneColumn, LicenseIconList, LicenseByline, ClickableLicenseByline } from '../src';
 
 import ArticleLoader from './article/ArticleLoader';
 
@@ -49,6 +51,7 @@ const ResourcesTab1Content = () => (
     <p><button>Se alle læringsstier</button></p>
   </div>
 );
+
 
 const ResourcesTab1 = () => (
   <div className="c-resources_content u-margin-top-small u-margin-bottom">
@@ -96,6 +99,49 @@ const MastheadExample = () => (
     </MastheadItem>
   </Masthead>
 );
+
+
+const ExamplePage1 = () => (
+  <article>
+    <section>
+    <aside>
+      <p><embed data-align="" data-alt="TV-studio sidespalte" data-caption="" data-id="7" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/114" /></p><h2>Huskelappen</h2><p><strong>En pitch er en kortvarig presentasjon av en idé eller et prosjekt.</strong></p><p><strong>Hensikten med en pitch er å skaffe seg samarbeidspartnere eller kunder, slik at man kan realisere idéen.</strong></p><h2>Ressurser</h2><p><embed data-align="" data-alt="Ingressbilde oppgave (film)" data-caption="" data-id="6" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/198" /></p><p><a href="http://www.nfi.no/bransje/kurs/videointervjuer/hva-er-en-god-pitch-katrine-adair-gir-r%C3%A5d" title="">Hva er en god pitch?</a></p><p>Her får du konkrete råd av Katrine Adair (Norsk filminstitutt).</p><p>Syv intense minutter kan bestemme om idéen blir film – eller ikke. Det kalles <a href="http://www.nrk.no/video/pitching/D40D56E046FD0BBD/" title="">pitch.</a></p><p><embed data-align="" data-alt="Filmproduksjon" data-caption="" data-id="3" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/197" /></p><p>Nå er det din tur!</p><p>Lykke til med din første pitch!</p>
+    </aside>
+    <h1>Tittel</h1>
+    <p>Du har en kjempegod idé til en kortfilm. Men det koster mange penger å produsere filmen. Derfor er du avhengig av at noen tenner på idéen din og bestemmer seg for å bruke ressurser på nettopp dette prosjektet.</p>
+    <p>En pitch er en kortvarig framføring av en idé for en potensiell samarbeidspartner eller kunde. I løpet av noen få minutter skal du få andre til å tenne på idéen din og se potensialet i den.</p>
+    <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du pitcher, blir idéen og historien i den filmen du planlegger å lage, tydeligere for både deg selv og dem du eventuelt jobber sammen med i klassen.</p>
+    <figure><img src="https://images.unsplash.com/photo-1476903930099-d0ddfec9a475?dpr=1&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1124&amp;q=80&amp;cs=tinysrgb&amp;crop=" alt="" /></figure>
+    <figcaption className="c-figcaption">
+      <div className="c-figcaption__info">I værmeldingene til NRK på 1980-tallet var symbolet for strålende solskinn en hvit sirkel. Ved skiftende vær var sirkelen delt i to med en hvit og en svart halvdel. Foto: Lars Knutsen Rønvig, NTB.</div>
+      <div className="c-figcaption__licenses"><ClickableLicenseByline license={getLicenseByAbbreviation('by-nc-nd')} /></div>
+    </figcaption>
+    <p>En pitch er en kortvarig framføring av en idé for en potensiell samarbeidspartner eller kunde. I løpet av noen få minutter skal du få andre til å tenne på idéen din og se potensialet i den.</p>
+    <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du pitcher, blir idéen og historien i den filmen du planlegger å lage, tydeligere for både deg selv og dem du eventuelt jobber sammen med i klassen.</p>
+  </section>
+  </article>
+);
+const ExamplePage2 = () => (
+  <article>
+    <section>
+    <aside>
+      <p><embed data-align="" data-alt="TV-studio sidespalte" data-caption="" data-id="7" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/114" /></p><h2>Huskelappen</h2><p><strong>En pitch er en kortvarig presentasjon av en idé eller et prosjekt.</strong></p><p><strong>Hensikten med en pitch er å skaffe seg samarbeidspartnere eller kunder, slik at man kan realisere idéen.</strong></p><h2>Ressurser</h2><p><embed data-align="" data-alt="Ingressbilde oppgave (film)" data-caption="" data-id="6" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/198" /></p><p><a href="http://www.nfi.no/bransje/kurs/videointervjuer/hva-er-en-god-pitch-katrine-adair-gir-r%C3%A5d" title="">Hva er en god pitch?</a></p><p>Her får du konkrete råd av Katrine Adair (Norsk filminstitutt).</p><p>Syv intense minutter kan bestemme om idéen blir film – eller ikke. Det kalles <a href="http://www.nrk.no/video/pitching/D40D56E046FD0BBD/" title="">pitch.</a></p><p><embed data-align="" data-alt="Filmproduksjon" data-caption="" data-id="3" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/197" /></p><p>Nå er det din tur!</p><p>Lykke til med din første pitch!</p>
+    </aside>
+    <h1>Tittel</h1>
+    <p>Du har en kjempegod idé til en kortfilm. Men det koster mange penger å produsere filmen. Derfor er du avhengig av at noen tenner på idéen din og bestemmer seg for å bruke ressurser på nettopp dette prosjektet.</p>
+    <p>En pitch er en kortvarig framføring av en idé for en potensiell samarbeidspartner eller kunde. I løpet av noen få minutter skal du få andre til å tenne på idéen din og se potensialet i den.</p>
+    <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du pitcher, blir idéen og historien i den filmen du planlegger å lage, tydeligere for både deg selv og dem du eventuelt jobber sammen med i klassen.</p>
+    <figure class="article__oembed"><iframe width="480" height="270" src="https://www.youtube.com/embed/f9VriNNRn0U?feature=oembed" frameborder="0" allowfullscreen=""></iframe></figure>
+    <figcaption className="c-figcaption">
+      <div className="c-figcaption__info">En video om nyheter, men ikke fake news.</div>
+      <div className="c-figcaption__licenses"><ClickableLicenseByline license={getLicenseByAbbreviation('by-nc-nd')} /></div>
+    </figcaption>
+    <p>En pitch er en kortvarig framføring av en idé for en potensiell samarbeidspartner eller kunde. I løpet av noen få minutter skal du få andre til å tenne på idéen din og se potensialet i den.</p>
+    <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du pitcher, blir idéen og historien i den filmen du planlegger å lage, tydeligere for både deg selv og dem du eventuelt jobber sammen med i klassen.</p>
+  </section>
+  </article>
+);
+
 
 storiesOf('Sidevisninger', module)
   .add('Empty page', () => (
@@ -150,14 +196,23 @@ storiesOf('Sidevisninger', module)
     </PageContainer>
   ));
 storiesOf('Sidevisninger alternativ', module)
-    .add('ArticlePage', () => (
+    .add('Artikkel med bilde', () => (
       <PageContainer>
         <MastheadExample />
         <Hero url="https://images.unsplash.com/photo-1469082993720-0b12bbd9e68b?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
         <OneColumn>
-          <article>
-            <div dangerouslySetInnerHTML={{ __html: articleHTML.outerHTML }} />
-          </article>
+          <ExamplePage1 />
+        </OneColumn>
+        <ResourcesExample />
+        <FooterExample />
+      </PageContainer>
+    ))
+    .add('Artikkel med video', () => (
+      <PageContainer>
+        <MastheadExample />
+        <Hero url="https://images.unsplash.com/photo-1469082993720-0b12bbd9e68b?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
+        <OneColumn>
+          <ExamplePage2 />
         </OneColumn>
         <ResourcesExample />
         <FooterExample />

2ca10ae1275296b849f881e696e76b4b4c4f56bd
thomaslgj <thomas@netliferesearch.com>
2017-02-03 14:39:05 +0100

    Adjust tables styling, add article with table

--- packages/ndla-ui/stories/pages.jsx
+++ packages/ndla-ui/stories/pages.jsx
@@ -141,6 +141,24 @@ const ExamplePage2 = () => (
   </section>
   </article>
 );
+const ExamplePage3 = () => (
+  <article>
+    <section>
+    <aside>
+      <p><embed data-align="" data-alt="TV-studio sidespalte" data-caption="" data-id="7" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/114" /></p><h2>Huskelappen</h2><p><strong>En pitch er en kortvarig presentasjon av en idé eller et prosjekt.</strong></p><p><strong>Hensikten med en pitch er å skaffe seg samarbeidspartnere eller kunder, slik at man kan realisere idéen.</strong></p><h2>Ressurser</h2><p><embed data-align="" data-alt="Ingressbilde oppgave (film)" data-caption="" data-id="6" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/198" /></p><p><a href="http://www.nfi.no/bransje/kurs/videointervjuer/hva-er-en-god-pitch-katrine-adair-gir-r%C3%A5d" title="">Hva er en god pitch?</a></p><p>Her får du konkrete råd av Katrine Adair (Norsk filminstitutt).</p><p>Syv intense minutter kan bestemme om idéen blir film – eller ikke. Det kalles <a href="http://www.nrk.no/video/pitching/D40D56E046FD0BBD/" title="">pitch.</a></p><p><embed data-align="" data-alt="Filmproduksjon" data-caption="" data-id="3" data-resource="image" data-size="hoyrespalte" data-url="http://api.staging.ndla.no/images/197" /></p><p>Nå er det din tur!</p><p>Lykke til med din første pitch!</p>
+    </aside>
+    <h1>Tittel</h1>
+    <p>Du har en kjempegod idé til en kortfilm. Men det koster mange penger å produsere filmen. Derfor er du avhengig av at noen tenner på idéen din og bestemmer seg for å bruke ressurser på nettopp dette prosjektet.</p>
+    <p>En pitch er en kortvarig framføring av en idé for en potensiell samarbeidspartner eller kunde. I løpet av noen få minutter skal du få andre til å tenne på idéen din og se potensialet i den.</p>
+    <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du pitcher, blir idéen og historien i den filmen du planlegger å lage, tydeligere for både deg selv og dem du eventuelt jobber sammen med i klassen.</p>
+
+    <table><tbody><tr><th>Hovedkategorier</th><th>Sjangre</th><th>Kjennetegn</th></tr><tr><td><strong>Subjektive sjangre</strong></td><td>Leder</td><td><ul><li>skrives ofte av en av redaktørene </li><li>er redaksjonens syn på en sak </li><li>står ofte på side 2 eller på egen meningsside</li></ul></td></tr><tr><td></td><td>Anmeldelse</td><td><ul><li>en av journalistenes mening om en ny film, bok, konsert e.l. </li><li>skal være en bruksanvisning slik at målgruppen vet om produktet er noe for dem</li></ul></td></tr><tr><td></td><td>Leserinnlegg og kommentarfelt</td><td><ul><li>lar den enkelte mottaker komme med sitt syn på en sak</li><li>leserinnlegg i avis, kommentarfelt på nett </li></ul></td></tr><tr><td></td><td>Kommentar og kronikk</td><td><ul><li>kan skrives av en i redaksjonen, men gjerne også av en fagperson utenfor redaksjonen </li><li>skal gi dybdekunnskap om et tema </li></ul></td></tr><tr><td><strong>Objektive sjangre</strong></td><td>Nyhetsartikkel</td><td><ul><li>kort, konsis, svarer på grunnleggende spørsmål</li><li>det viktigste først </li><li>lar kildene komme til orde</li></ul></td></tr><tr><td></td><td>Nyhetsreportasje</td><td><ul><li>går gjerne mer i dybden enn en nyhetsartikkel </li><li>bruker flere kilder </li><li>har fokus på bakgrunn og årsak</li></ul></td></tr><tr><td /><td>Intervju</td><td><ul><li>enkelt oppsett med spørsmål og svar </li><li>brukes gjerne som en del av en nyhetsreportasje eller artikkel</li></ul></td></tr><tr><td></td><td>Notis</td><td><ul><li>kort nyhetsmelding som gir grunnleggende informasjon om en aktuell hendelse</li><li>gjerne bare 10–15 setninger</li></ul></td></tr><tr><td><strong>Hybridsjangre</strong></td><td>Feature</td><td><ul><li>hører til i objektiv-kategorien, men journalisten bruker alle sanser og litterære virkemidler </li><li>går i dybden, gir leseren en større forståelse av et tema</li></ul></td></tr><tr><td></td><td>Portrettintervju</td><td><ul><li>hører til i objektiv-kategorien, men journalisten bruker alle sanser og litterære virkemidler </li><li>gir leseren dybdekunnskap om en aktuell person i nyhetsbildet</li></ul></td></tr></tbody></table>
+
+    <p>En pitch er en kortvarig framføring av en idé for en potensiell samarbeidspartner eller kunde. I løpet av noen få minutter skal du få andre til å tenne på idéen din og se potensialet i den.</p>
+    <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du pitcher, blir idéen og historien i den filmen du planlegger å lage, tydeligere for både deg selv og dem du eventuelt jobber sammen med i klassen.</p>
+  </section>
+  </article>
+);
 
 
 storiesOf('Sidevisninger', module)
@@ -213,6 +231,17 @@ storiesOf('Sidevisninger alternativ', module)
         <Hero url="https://images.unsplash.com/photo-1469082993720-0b12bbd9e68b?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
         <OneColumn>
           <ExamplePage2 />
+        </OneColumn>
+        <ResourcesExample />
+        <FooterExample />
+      </PageContainer>
+    ))
+    .add('Artikkel med tabell', () => (
+      <PageContainer>
+        <MastheadExample />
+        <Hero url="https://images.unsplash.com/photo-1469082993720-0b12bbd9e68b?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" />
+        <OneColumn>
+          <ExamplePage3 />
         </OneColumn>
         <ResourcesExample />
         <FooterExample />

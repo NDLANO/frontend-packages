304b93eda88a9e3463d9a7dd18413db0855fdc0b
Øyvind Marthinsen <oma@knowit.no>
2017-02-16 21:14:38 +0100

    Add article script tests

---
+++ packages/ndla-article-scripts/src/__tests__/articleScripts-test.js
@@ -0,0 +1,62 @@
+
+/* eslint-env jest */
+
+import {
+  addAsideClickListener,
+  removeAsideClickListener,
+} from '../index';
+
+const byId = id => document.getElementById(id);
+
+test('articleScripts/addAsideClickListener toggle expanded className on click', () => {
+  document.body.innerHTML = `
+    <section>
+      <p>Lorem ipsum</p>
+      <aside id="aside-1">
+        <button id="button-1" class="c-aside__button"></button>
+      </aside>
+      <aside id="aside-2">
+        <button id="button-2" class="c-aside__button"></button>
+      </aside>
+    </section>
+  `;
+
+  addAsideClickListener();
+  byId('button-1').click();
+
+  expect(byId('aside-1').className).toBe('expanded');
+  expect(byId('aside-2').className).toBe('');
+
+  byId('button-2').click();
+
+  expect(byId('aside-1').className).toBe('expanded');
+  expect(byId('aside-2').className).toBe('expanded');
+
+  byId('button-1').click();
+  byId('button-2').click();
+
+  expect(byId('aside-1').className).toBe('');
+  expect(byId('aside-2').className).toBe('');
+});
+
+test('articleScripts/removeAsideClickListener removes aside click listner', () => {
+  document.body.innerHTML = `
+    <section>
+      <p>Lorem ipsum</p>
+      <aside id="aside-1">
+        <button id="button-1" class="c-aside__button"></button>
+      </aside>
+      <aside id="aside-2">
+        <button id="button-2" class="c-aside__button"></button>
+      </aside>
+    </section>
+  `;
+
+  addAsideClickListener();
+  removeAsideClickListener();
+  byId('button-1').click();
+  byId('button-2').click();
+
+  expect(byId('aside-1').className).toBe('');
+  expect(byId('aside-2').className).toBe('');
+});

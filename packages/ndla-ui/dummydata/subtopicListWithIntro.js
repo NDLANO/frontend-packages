43c89a38af254248da628c1ebb8923547808d384
thomaslgj <thomas@netliferesearch.com>
2017-02-28 10:54:18 +0100

    New sub topic names in list

--- packages/ndla-ui/dummydata/subtopicListWithIntro.js
+++ packages/ndla-ui/dummydata/subtopicListWithIntro.js
@@ -2,19 +2,19 @@
 export default [
   {
     id: 'urn:topic:158919',
-    name: 'Internett og programmering',
+    name: 'Kommunikasjon',
     introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     contentUri: null,
   },
   {
     id: 'urn:topic:160297',
-    name: 'Nettstruktur og brukeropplevelser',
+    name: 'Kost og ernæring',
     introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     contentUri: null,
   },
   {
     id: 'urn:topic:170364',
-    name: 'Publiser et nettsted i Wordpress',
+    name: 'Førstehjelp',
     introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     contentUri: null,
   },

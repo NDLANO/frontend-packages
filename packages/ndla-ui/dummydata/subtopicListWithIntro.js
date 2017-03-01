e483b51a0f85c69fde47be831529a47c07b67a6a
thomaslgj <thomas@netliferesearch.com>
2017-03-01 14:41:19 +0100

    Add shorter lead texts to sub topics

--- packages/ndla-ui/dummydata/subtopicListWithIntro.js
+++ packages/ndla-ui/dummydata/subtopicListWithIntro.js
@@ -12,17 +12,17 @@ export default [
   },
   {
     name: 'Kommunikasjon med alvorlig syke brukere ',
-    introduction: 'Som helsefagarbeider kan du komme tett på en brukers ønsker i den siste fasen av livet og døden. Alt helsepersonell skal sørge for at pasienter som ønsker det får mulighet til å gi tilkjenne sine ønsker og verdier rundt livets siste fase. En viktig del av kommunikasjonen med brukere er å gi dem mulighet til å snakke om dette når de selv ønsker det. ',
+    introduction: 'Som helsefagarbeider kan du komme tett på en brukers ønsker den siste fasen av livet og døden. En viktig del av kommunikasjonen er å gi alle mulighet til å snakke om dette når de selv ønsker det.',
     contentUri: null,
   },
   {
     name: 'Kommunikasjon med pårørende ',
-    introduction: 'En helsefagarbeiders rolle er også å kommunisere med pårørende på en profesjonell måte. God kommunikasjon er viktig når man møter pårørende.  Man må møte dem med respekt og empati. Du må være bevisst ditt eget kroppsspråk og legge til rette for at pårørende får ro og god nok informasjon om alt de lurer på i enhver situasjon.',
+    introduction: 'God kommunikasjon er viktig når man møter pårørende. Man må være bevisst sitt eget kroppsspråk og legge til rette for at pårørende får god nok informasjon om alt de lurer på i enhver situasjon.',
     contentUri: null,
   },
   {
     name: 'Kommunikasjon med personer med demens',
-    introduction: 'Gjennom bevisst kommunikasjon er det mulig å styrke pasientens identitet og følelse av mestring. Ett av de vanligste problemene mange med demens opplever, er vansker med å finne ord og problemer med å oppfatte hva andre sier. Derfor er det viktig å være rolig og gi tid til samtalen. ',
+    introduction: 'Personer med demens kan ha vansker med å finne de riktige ordene samt oppfatte hva andre sier.  For å få til en god samtale er det viktig å være rolig og sette av tid til kommunikasjon.',
     contentUri: null,
   },
 ];

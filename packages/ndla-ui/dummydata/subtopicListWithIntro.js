b07773cbbd7f7052f5d8ed4c6ca0b48095f0d0e1
thomaslgj <thomas@netliferesearch.com>
2017-03-01 09:28:42 +0100

    New texts on topic lists

--- packages/ndla-ui/dummydata/subtopicListWithIntro.js
+++ packages/ndla-ui/dummydata/subtopicListWithIntro.js
@@ -1,19 +1,26 @@
 
 export default [
   {
-    id: 'urn:topic:158919',
     name: 'Profesjonell kommunikasjon',
     introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     contentUri: null,
   },
   {
-    id: 'urn:topic:160297',
     name: 'Å kommunisere med mennesker med ulike forutsetninger',
     introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     contentUri: null,
   },
   {
-    id: 'urn:topic:170364',
+    name: 'Noe her',
+    introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    contentUri: null,
+  },
+  {
+    name: 'Noe her',
+    introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    contentUri: null,
+  },
+  {
     name: 'Noe her',
     introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     contentUri: null,
--- packages/ndla-ui/dummydata/topicListWithIntro.js
+++ packages/ndla-ui/dummydata/topicListWithIntro.js
@@ -1,14 +1,27 @@
 
 export default [
   {
-    id: 'urn:topic:169379',
     name: 'Kommunikasjon for fagarbeideren',
     introduction: 'Kommunikasjon er et stort fagfelt for helsefagarbeidere. En helsefagarbeider har opparbeidet seg tilfredstillende kommunikasjonsferdigheter for å bli godkjent yrkesutøver.',
     contentUri: null,
   },
   {
-    id: 'urn:topic:170356',
-    name: 'Emne 2',
+    name: 'Menneskekroppen',
+    introduction: 'I dette emnet kan du lære mer om hvordan du kan unngå skader når du må jobbe lange økter foran datamaskinen, og når du er ute på oppdrag med ulikt medieutstyr. I tillegg kan du lære litt om programvare og hvordan du kan levere profesjonelle formater i mediebransjen.',
+    contentUri: null,
+  },
+  {
+    name: 'Sykepleie',
+    introduction: 'I dette emnet kan du lære mer om hvordan du kan unngå skader når du må jobbe lange økter foran datamaskinen, og når du er ute på oppdrag med ulikt medieutstyr. I tillegg kan du lære litt om programvare og hvordan du kan levere profesjonelle formater i mediebransjen.',
+    contentUri: null,
+  },
+  {
+    name: 'Prosedyrer',
+    introduction: 'I dette emnet kan du lære mer om hvordan du kan unngå skader når du må jobbe lange økter foran datamaskinen, og når du er ute på oppdrag med ulikt medieutstyr. I tillegg kan du lære litt om programvare og hvordan du kan levere profesjonelle formater i mediebransjen.',
+    contentUri: null,
+  },
+  {
+    name: 'Hygiene',
     introduction: 'I dette emnet kan du lære mer om hvordan du kan unngå skader når du må jobbe lange økter foran datamaskinen, og når du er ute på oppdrag med ulikt medieutstyr. I tillegg kan du lære litt om programvare og hvordan du kan levere profesjonelle formater i mediebransjen.',
     contentUri: null,
   },

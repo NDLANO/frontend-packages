fe5497332d77e3fd8e644bc574faed21381145be
thomaslgj <thomas@netliferesearch.com>
2017-03-01 10:46:05 +0100

    Add aside content to new article page example

--- packages/ndla-ui/stories/pages.jsx
+++ packages/ndla-ui/stories/pages.jsx
@@ -389,6 +389,93 @@ const ExamplePage4 = () => (
   </article>
 );
 
+const ExampleWithSummary = () => (
+  <article className="o-wrapper--narrow">
+    <section className="c-article-content">
+      <div id="breadcrumbs" className="c-breadcrumbs">
+        <strong>Du er her:</strong>
+        <a href="" className="c-breadcrumbs__item">Fag</a> &#x203A; <a href="" className="c-breadcrumbs__item">Medieuttrykk og Helsearbeiderfag</a> &#x203A; <a href="" className="c-breadcrumbs__item">Mediekommunikasjon </a>
+      </div>
+      <h1>Tittel om pitching</h1>
+      <p className="article_introduction">Du har en kjempegod idé til en kortfilm. Men det koster mange penger
+         å produsere filmen.
+      </p>
+      <div className="c-article__byline">
+        <p>Skrevet av [Opphavsperson]. Publisert [dato]</p>
+      </div>
+      <p>Du har en kjempegod idé til en kortfilm. Men det koster mange penger
+        å produsere filmen. Derfor er du avhengig av at noen tenner på idéen
+        din og bestemmer seg for å bruke ressurser på nettopp dette
+      prosjektet.</p>
+      <p>En pitch er en kortvarig framføring av en idé for en potensiell
+        samarbeidspartner eller kunde. I løpet av noen få minutter skal du
+      få andre til å tenne på idéen din og se potensialet i den.</p>
+      <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du
+        pitcher, blir idéen og historien i den filmen du planlegger å lage,
+          tydeligere for både deg selv og dem du eventuelt jobber sammen med
+           i klassen.</p>
+      <LicenseToggle>
+        <iframe src="http://ndla.no/nb/h5p/embed/146132?fag=127756" width="260" height="373" frameBorder="0" allowFullScreen="allowfullscreen" /><script src="http://ndla.no/sites/all/modules/h5p/library/js/h5p-resizer.js?fag=127756" charSet="UTF-8" />
+      </LicenseToggle>
+      <p>En pitch er en kortvarig framføring av en idé for en potensiell
+         samarbeidspartner eller kunde. I løpet av noen få minutter skal du
+          få andre til å tenne på idéen din og se potensialet i den.</p>
+      <p>Pitching er også en god måte å bevisstgjøre seg selv på. Når du
+         pitcher, blir idéen og historien i den filmen du planlegger å lage,
+          tydeligere for både deg selv og dem du eventuelt jobber sammen med
+           i klassen.</p>
+
+      <aside className="c-aside">
+        <div className="c-aside__content">
+          <div>
+            <h2>Hva vil du bli?</h2>
+            <p>Søknadsfristen til høgskoler og universiteter er 15.april.</p>
+            <p>Er du en av dem som akkurat nå gjør et viktig valg? Vi hjelper
+               deg å velge!</p><h2>Siris tips</h2>
+            <div>Siri Knudsen i NRK P3 gir deg noen gode råd med på veien.</div>
+            <div>
+              <figure className="article_audio">
+                <audio
+                  type="audio/mpeg"
+                  src="http://test.api.ndla.no/audio/files/Siri_knudsen_mars2012.mp3"
+                />
+                <figcaption>medieutdanning</figcaption>
+              </figure>
+            </div>
+            <h2>Ressurser</h2>
+            <p>
+              <a
+                href="http://utdanning.no/tema/yrke_og_karriere/finn?s=media"
+                title="Utdanning.no: Søk på yrke"
+              >
+                Utdanning.no: Søk på yrke
+              </a>
+            </p>
+            <p>
+              <a
+                href="http://www.vilbli.no"
+                title="Les mer på Vilbli.no"
+              >
+                Les mer på Vilbli.no
+              </a>
+            </p>
+            <p>Landslaget for medieundervisning har en god oversikt over</p>
+            <p>
+              <a
+                href="http://www.mediepedagogene.no/undervisning-og-utdanning/medieutdanning?wpmp_switcher=mobile"
+                title="medieutdanning i Norge"
+              >
+                medieutdanning i Norge
+              </a>
+            </p>
+          </div>
+        </div>
+        <button className="c-button c-aside__button" type="button" />
+      </aside>
+    </section>
+  </article>
+);
+
 const SubTopic = () => (
   <ArticleLoader articleId="5948" notitle />
 );
@@ -578,6 +665,17 @@ storiesOf('Artikkelmaler', module)
         <Hero />
         <OneColumn>
           <IdealExamplePage1 />
+        </OneColumn>
+        <ResourcesExample />
+        <FooterExample />
+      </PageContainer>
+    ))
+    .add('Artikkel med oppsummeringsboks', () => (
+      <PageContainer>
+        <MastheadWithTopicMenu />
+        <Hero />
+        <OneColumn>
+          <ExampleWithSummary />
         </OneColumn>
         <ResourcesExample />
         <FooterExample />

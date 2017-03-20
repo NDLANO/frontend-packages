30b897ec3ba0ab5ff10d3893a6c96a0744e2d693
thomaslgj <thomas@netliferesearch.com>
2017-03-20 10:13:32 +0100

    Explanations for pagination

--- packages/ndla-ui/stories/molecules.jsx
+++ packages/ndla-ui/stories/molecules.jsx
@@ -118,11 +118,29 @@ storiesOf('Sammensatte moduler', module)
   ))
   .add('Paginering', () => (
     <Center>
-      <Pager page={3} lastPage={10} query={{ query: 'Medier' }} pathname="#" />
-      <Pager page={4} lastPage={4} query={{ query: 'Medier' }} pathname="#" />
-      <Pager page={1} lastPage={3} query={{ query: 'Medier' }} pathname="#" />
-      <Pager page={3} lastPage={3} query={{ query: 'Medier' }} pathname="#" />
-      <Pager page={1} lastPage={1} query={{ query: 'Medier' }} pathname="#" />
+      <article className="o-wrapper--narrow">
+        <h2>Eksempler på bruk:</h2>
+        <section>
+          <h3>Side 3 valgt av 10 sider:</h3>
+          <Pager page={3} lastPage={10} query={{ query: 'Medier' }} pathname="#" />
+        </section>
+        <section>
+          <h3>Side 4 valgt av 4 sider:</h3>
+          <Pager page={4} lastPage={4} query={{ query: 'Medier' }} pathname="#" />
+        </section>
+        <section>
+          <h3>Side 1 valgt av 3 sider:</h3>
+          <Pager page={1} lastPage={3} query={{ query: 'Medier' }} pathname="#" />
+        </section>
+        <section>
+          <h3>Side 3 valgt av 3 sider:</h3>
+          <Pager page={3} lastPage={3} query={{ query: 'Medier' }} pathname="#" />
+        </section>
+        <section>
+          <h3>Side 1 valgt av 1 side:</h3>
+          <Pager page={1} lastPage={1} query={{ query: 'Medier' }} pathname="#" />
+        </section>
+      </article>
     </Center>
   ))
   .add('Sidefot', () => (

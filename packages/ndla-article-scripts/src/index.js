274a2eabf4763e7a06acfd3275664e28c751f98f
Øyvind Marthinsen <oma@knowit.no>
2017-03-23 15:17:19 +0100

    Add scripts for figure detail toggling

--- packages/ndla-article-scripts/src/index.js
+++ packages/ndla-article-scripts/src/index.js
@@ -6,8 +6,81 @@
  *
  */
 
-export const updateIFrameDimensions = () => {
-  document.querySelectorAll('.article__oembed iframe')
+ import { createElement, findAncestorByClass, removeModifiers, removeElementById, wrapElement } from './domHelpers';
+
+ export const addAsideClickListener = () => {
+   document.querySelectorAll('.c-aside__button')
+    .forEach((el) => {
+      const target = el;
+      target.onclick = () => target.parentNode.classList.toggle('expanded');
+    });
+ };
+
+ export const removeAsideClickListener = () => {
+   document.querySelectorAll('.c-aside__button')
+    .forEach((el) => {
+      const target = el;
+      target.onclick = undefined;
+    });
+ };
+
+ export const addCloseFigureDetailsClickListners = () => {
+   document.querySelectorAll('.c-figure .c-figure__close').forEach((el) => {
+     const target = el;
+     target.onclick = () => {
+       removeElementById('c-license-icon-description');
+       target.parentNode.classList.remove('c-figure--active');
+       target.parentNode.querySelector('figcaption').classList.remove('u-hidden');
+     };
+   });
+ };
+
+ export const addShowFigureDetailsClickListners = () => {
+   document.querySelectorAll('.c-figure .c-figure__captionbtn').forEach((el) => {
+     const target = el;
+     target.onclick = () => {
+       removeElementById('c-license-icon-description');
+       const figure = findAncestorByClass(target, 'c-figure');
+       figure.classList.add('c-figure--active');
+
+       const figcaption = findAncestorByClass(target, 'c-figure__caption');
+       figcaption.classList.add('u-hidden');
+     };
+   });
+ };
+
+ export const makeFigureLicenseIconsClickable = () => {
+   document.querySelectorAll('.c-figure .c-license-icons__item').forEach((el) => {
+     const iconEl = el.querySelector('.c-license-icons__icon');
+
+     const button = document.createElement('button');
+     button.className = 'c-button c-button--stripped';
+
+     button.onclick = () => {
+       const isActive = el.classList.contains('c-license-icons__item--active');
+
+       // cleanup
+       removeElementById('c-license-icon-description');
+       removeModifiers('c-license-icons__item', 'active', '.c-figure'); // remove existing modifier classes
+
+       if (isActive) { // Only do cleanup if already active
+         button.blur();
+         return;
+       }
+
+       // Add active modifier and append description element
+       el.classList.add('c-license-icons__item--active');
+       const description = iconEl.getAttribute('aria-label');
+       const byline = findAncestorByClass(el, 'license-byline');
+       const descriptionEl = createElement('div', 'c-license-icon-description', 'license-byline__body license-byline__body--black', `<span>${description}</span>`);
+       byline.appendChild(descriptionEl);
+     };
+     wrapElement(iconEl, button);
+   });
+ };
+
+ export const updateIFrameDimensions = () => {
+   document.querySelectorAll('.article__oembed iframe')
     .forEach((el) => {
       const iframe = el;
       const parentWidth = iframe.parentNode.clientWidth;
@@ -15,38 +88,35 @@ export const updateIFrameDimensions = () => {
       iframe.height = newHeight;
       iframe.width = parentWidth;
     });
-};
+ };
 
-export const addAsideClickListener = () => {
-  document.querySelectorAll('.c-aside__button')
-    .forEach((el) => {
-      const target = el;
-      target.onclick = () => target.parentNode.classList.toggle('expanded');
-    });
-};
+ export const addEventListenerForResize = () => {
+   window.addEventListener('resize', updateIFrameDimensions);
+ };
 
-export const removeAsideClickListener = () => {
-  document.querySelectorAll('.c-aside__button')
-    .forEach((el) => {
-      const target = el;
-      target.onclick = undefined;
-    });
-};
+ export const removeEventListenerForResize = () => {
+   window.removeEventListener('resize', updateIFrameDimensions);
+ };
 
-export const addEventListenerForResize = () => {
-  window.addEventListener('resize', updateIFrameDimensions);
-};
+ export const initArticleScripts = () => {
+   addEventListenerForResize();
+   updateIFrameDimensions();
+   addAsideClickListener();
+   addCloseFigureDetailsClickListners();
+   addShowFigureDetailsClickListners();
+   makeFigureLicenseIconsClickable();
+ };
 
-export const removeEventListenerForResize = () => {
-  window.removeEventListener('resize', updateIFrameDimensions);
-};
+ export default {
+   updateIFrameDimensions,
 
-export default {
-  updateIFrameDimensions,
+   addAsideClickListener,
+   removeAsideClickListener,
 
-  addAsideClickListener,
-  removeAsideClickListener,
+   addEventListenerForResize,
+   removeEventListenerForResize,
 
-  addEventListenerForResize,
-  removeEventListenerForResize,
-};
+   addCloseFigureDetailsClickListners,
+   addShowFigureDetailsClickListners,
+   makeFigureLicenseIconsClickable,
+ };

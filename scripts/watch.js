bf77d9f9b48a7ae48463d55766624f93f811cc18
Øyvind Marthinsen <oma@knowit.no>
2017-04-04 10:44:44 +0200

    Improve watch script

--- scripts/watch.js
+++ scripts/watch.js
@@ -8,47 +8,40 @@
 
 const chokidar = require('chokidar');
 const chalk = require('chalk');
-const babel = require('babel-core');
 const fs = require('fs');
 const path = require('path');
+const { buildFile, removeBuildFile } = require('./build');
 
-// const SRC_DIR = 'src';
-// const JS_FILES_PATTERN = '**/*.js';
+const SRC_DIR = 'src';
+const JS_FILES_PATTERN = '**/*.js*';
 const IGNORE_PATTERN = '**/__tests__/**';
 const PACKAGES_DIR = path.resolve(__dirname, '..', './packages');
 
-const babelOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '.babelrc'), 'utf8'));
-babelOptions.babelrc = false;
+// Get absolute paths of all directories under packages/*
+function getPackages() {
+  return fs.readdirSync(PACKAGES_DIR)
+    .map(file => path.resolve(PACKAGES_DIR, file))
+    .filter(f => fs.lstatSync(path.resolve(f)).isDirectory());
+}
 
-// Initialize watcher.
-const watcher = chokidar.watch('packages/ndla-editor/src/**/*.js*', {
-  ignored: [/(^|[/\\])\../, IGNORE_PATTERN],
-  persistent: true,
+const packagePatterns =
+  getPackages()
+    .filter(f => f.indexOf('ndla-ui') === -1 && f.indexOf('eslint-config-ndla') === -1) // skip ndla-ui and eslint-config-ndla
+    .map(p => path.resolve(p, SRC_DIR, JS_FILES_PATTERN));
+
+
+// Initialize watcher
+const watcher = chokidar.watch(packagePatterns, {
+  ignored: [IGNORE_PATTERN],
 });
 
 
-function getDestPath(packageBuildPath, relativeToSrcPath) {
-  const destPath = path.resolve(packageBuildPath, relativeToSrcPath);
-  if (destPath.indexOf('.jsx') > -1) {
-    return destPath.substring(0, destPath.length - 1);
-  }
-  return destPath;
-}
-
-function buildFile(file) {
-  // log(`File ${file} has been changed`);
-  const packageName = path.relative(PACKAGES_DIR, file).split(path.sep)[0];
-  const packageSrcPath = path.resolve(PACKAGES_DIR, packageName, 'src');
-  const packageBuildPath = path.resolve(PACKAGES_DIR, packageName, 'lib');
-  const relativeToSrcPath = path.relative(packageSrcPath, file);
-  // const destPath = path.resolve(packageBuildPath, relativeToSrcPath);
-  const destPath = getDestPath(packageBuildPath, relativeToSrcPath);
-  const transformed = babel.transformFileSync(file, babelOptions).code;
-  fs.writeFileSync(destPath, transformed);
-  process.stdout.write(`${chalk.green('  \u2022 ') + path.relative(PACKAGES_DIR, file) + chalk.green(' \u21D2 ') + path.relative(PACKAGES_DIR, destPath)}\n`);
-}
-// Add event listeners.
 watcher
-  .on('add', p => console.log(`File ${p} has been added`))
   .on('change', buildFile)
-  .on('unlink', p => console.log(`File ${p} has been removed`));
+  .on('ready', () => {
+    // Attach add event listner after initial scan is completed.
+    watcher.on('add', buildFile);
+  })
+  .on('unlink', removeBuildFile);
+
+process.stdout.write(`${chalk.red('->') + chalk.cyan('Watching for changes...')}`);

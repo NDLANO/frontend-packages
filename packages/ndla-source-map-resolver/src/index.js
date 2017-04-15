26887e7363655c997e546fe7e194b431fef5d2f8
Øyvind Marthinsen <oma@knowit.no>
2017-04-15 17:52:15 +0200

    Read and resolve stacktrace from source map

--- packages/ndla-source-map-resolver/src/index.js
+++ packages/ndla-source-map-resolver/src/index.js
@@ -5,3 +5,26 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
+
+const fs = require('fs');
+const path = require('path');
+const sourceMap = require('source-map');
+const stackTrace = require('./stacktrace.json');
+
+
+function loadSourceMap(mapFile) {
+  const sourceMapData = fs.readFileSync(path.resolve(__dirname, mapFile)).toString();
+  const mapConsumer = new sourceMap.SourceMapConsumer(sourceMapData);
+  return {
+    mapConsumer,
+  };
+}
+
+function printOriginalPosition(orgPos) {
+  console.log(`at ${orgPos.name} (${orgPos.source}:${orgPos.line}:${orgPos.column}) `);
+}
+
+const { mapConsumer } = loadSourceMap('main.fa4ad8bb7fdf85c2f2a4.js.map');
+
+const { stackInfo: { stack } } = stackTrace;
+stack.forEach(item => printOriginalPosition(mapConsumer.originalPositionFor(item)));

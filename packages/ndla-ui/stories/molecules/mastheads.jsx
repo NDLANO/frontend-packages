758706a3c6b7d99210ffa96ccb61c5f5b63a84d8
kmelve <knut@netliferesearch.com>
2017-05-19 09:15:51 +0200

    fix link

--- packages/ndla-ui/stories/molecules/mastheads.jsx
+++ packages/ndla-ui/stories/molecules/mastheads.jsx
@@ -38,7 +38,7 @@ export const MastheadWithTopicMenu = () => (
     <MastheadItem left>
       <SiteNav>
         <ClickToggle title="Meny" className="c-topic-menu-container" buttonClassName="c-btn c-button--outline c-topic-menu-toggle-button">
-          <TopicMenu toTopic={(id) => `#{id}`} topics={topicMenu} />
+          <TopicMenu toTopic={() => '#'} topics={topicMenu} />
         </ClickToggle>
       </SiteNav>
     </MastheadItem>

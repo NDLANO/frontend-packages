101f7010067465416c168337acf16ab5c51b8d46
kmelve <knut@netliferesearch.com>
2017-05-23 10:11:01 +0200

    introduce popover component (wip)

---
+++ packages/ndla-ui/src/popover/Popover.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import SelectionPopover from 'react-selection-popover'
+
+const Popover = () => (<SelectionPopover
+            showPopover={this.state.showPopover}
+            onSelect={() => {this.setState({showPopover: true})}}
+            onDeselect={() => {this.setState({showPopover: false})}}
+          >
+            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button' onClick={() => this.toggleExplainator() }>Forklar begrep(er)</button>
+            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button'>Les høyt</button>
+            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button'>Lagre utdrag</button>
+          </SelectionPopover>)
+}
+

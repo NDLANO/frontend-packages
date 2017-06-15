e0aecd52c54f550d3b9bff755bb01bf8c5fe8371
Øyvind Marthinsen <oma@knowit.no>
2017-06-15 14:43:33 +0200

    Hack to accommodate storybook-to-ghpages script

--- scripts/buildStorybook.js
+++ scripts/buildStorybook.js
@@ -5,6 +5,11 @@ const runCommand = require('./runCommand');
 const isWindows = process.platform === 'win32';
 const storybook = isWindows ? 'build-storybook.cmd' : 'build-storybook';
 const storybookCmd = path.resolve(__dirname, `../packages/ndla-ui/node_modules/.bin/${storybook}`);
-const outputDir = path.resolve(__dirname, '../.out/');
+let args = process.argv.slice(-2);
 
-runCommand(storybookCmd, ['-o', outputDir], path.resolve(__dirname, '../packages/ndla-ui/'));
+if (args[0] === '-o') { // hack to accommodate storybook-to-ghpages script
+  args = [args[0], path.resolve(__dirname, '../packages/ndla-ui/', `../../${args[1]}`)];
+}
+
+
+runCommand(storybookCmd, args, path.resolve(__dirname, '../packages/ndla-ui/'));

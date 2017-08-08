2d8e222768e8cb14faeb8125e9825f55ed0512d9
Øyvind Marthinsen <oma@knowit.no>
2017-08-08 10:06:22 +0200

    Move prettier script

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -1,28 +1,12 @@
 const path = require('path');
-const spawn = require('child_process').spawnSync;
 const chalk = require('chalk');
 
-function runCommand(cmd, args, cwd = __dirname) {
-  const displayArgs =
-    args.length > 25 ? `${args.slice(0, 25)}...` : args.join(' ');
-  console.log(chalk.dim(`$ cwd ${cwd}\n$ ${cmd} ${displayArgs}\n`));
-  const result = spawn(cmd, args, {
-    cwd,
-    shell: true,
-    stdio: 'inherit',
-  });
-  if (result.error || result.status !== 0) {
-    const message = 'Error running command.';
-    const error = new Error(message);
-    error.stack = message;
-    throw error;
-  }
-}
+const runCommand = require('./runCommand');
 
 const shouldWrite = process.argv[2] === 'write';
 const isWindows = process.platform === 'win32';
 const prettier = isWindows ? 'prettier.cmd' : 'prettier';
-const prettierCmd = path.resolve(__dirname, `node_modules/.bin/${prettier}`);
+const prettierCmd = path.resolve(__dirname, `../node_modules/.bin/${prettier}`);
 
 const options = {
   'jsx-bracket-same-line': 'true',
@@ -36,14 +20,12 @@ const args = Object.keys(options)
   .concat(
     `--${shouldWrite ? 'write' : 'l'}`,
     '"scripts/*.js"',
-    // '"packages/*/{*,!lib,!es}/**/*(*.js|*.jsx)"',
     '"packages/*/!(lib|es|dist)/**/*(*.js|*.jsx)"',
     '"packages/*/*(*.js|*.jsx)"',
-    // '"!packages/**/lib/**/*(*.js|*.jsx)"'
   );
 
 try {
-  runCommand(prettierCmd, args);
+  runCommand(prettierCmd, args, path.resolve(__dirname, '..'));
 } catch (e) {
   if (!shouldWrite) {
     // prettier-ignore

b72307a8e2a6aefaff74a90921221de6b40d5d62
Øyvind Marthinsen <oma@knowit.no>
2017-08-14 16:52:42 +0200

    Don't show back link if history length is less than two

--- packages/ndla-ui/src/ErrorMessage/ErrorMessage.jsx
+++ packages/ndla-ui/src/ErrorMessage/ErrorMessage.jsx
@@ -28,6 +28,7 @@ export const ErrorMessage = ({ children, messages }) =>
         {messages.description}
       </p>
       {messages.back &&
+        window.history.length > 1 &&
         <SafeLink
           to={`/#${encodeURI(messages.back)}`}
           onClick={() => window.history.back()}

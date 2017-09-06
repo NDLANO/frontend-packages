65b1be4b025d0501a8185fd7369d060f2c183cec
thomaslgj <thomas@netliferesearch.com>
2017-09-06 16:33:08 +0200

    Cleanup code in concepts

--- packages/designmanual/stories/concepts.js
+++ packages/designmanual/stories/concepts.js
@@ -1,32 +1,28 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { LayoutItem } from 'ndla-ui';
 import { Center } from './helpers';
 
 storiesOf('Konsepter', module).add('Navngivning av innholdselementer', () =>
   <Center>
-    <LayoutItem layout="center">
-      <section className="c-factbox">
-        <h1 className="u-heading">Navngivning</h1>
-        <p />
-      </section>
-      <section>
-        <h2 className="u-heading">Hovedskiller for emner og lærestoff:</h2>
-        <ul>
-          <li>Kjernestoff</li>
-          <li>Tilvalgstoff</li>
-        </ul>
-      </section>
-      <section>
-        <h2 className="u-heading">Innholdskategorier for lærestoff:</h2>
-        <ul>
-          <li>Emnebeskrivelse</li>
-          <li>Lærestoff (Fagtekster, Bilder, Video, Historiske kilder, …)</li>
-          <li>Læringsstier</li>
-          <li>Oppgaver og aktiviteter</li>
-        </ul>
-      </section>
-    </LayoutItem>
+    <section className="c-factbox">
+      <h1 className="u-heading">Navngivning</h1>
+    </section>
+    <section>
+      <h2 className="u-heading">Hovedskiller for emner og lærestoff:</h2>
+      <ul>
+        <li>Kjernestoff</li>
+        <li>Tilvalgstoff</li>
+      </ul>
+    </section>
+    <section>
+      <h2 className="u-heading">Innholdskategorier for lærestoff:</h2>
+      <ul>
+        <li>Emnebeskrivelse</li>
+        <li>Lærestoff (Fagtekster, Bilder, Video, Historiske kilder, …)</li>
+        <li>Læringsstier</li>
+        <li>Oppgaver og aktiviteter</li>
+      </ul>
+    </section>
   </Center>,
 );

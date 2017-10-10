bec7d1dbf9c34b3b3c8d6f17976d0e9b2850106d
Øyvind Marthinsen <oma@knowit.no>
2017-10-10 07:59:53 +0200

    Send licenseRights to Figure

--- packages/ndla-ui/src/Figure/Figure.js
+++ packages/ndla-ui/src/Figure/Figure.js
@@ -12,7 +12,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { uuid } from 'ndla-util';
-import { BY, NC, ND } from 'ndla-licenses';
 import BEMHelper from 'react-bem-helper';
 import LicenseByline from '../LicenseByline';
 
@@ -25,13 +24,19 @@ const classLicenses = new BEMHelper({
   prefix: 'c-',
 });
 
-export const FigureDetails = ({ children, authors, origin, messages }) => (
+export const FigureDetails = ({
+  children,
+  authors,
+  origin,
+  messages,
+  licenseRights,
+}) => (
   <div {...classes('license')} id="figmeta">
     <button {...classes('close')}>{messages.close}</button>
     <div className="u-expanded">
       <div {...classLicenses('details')}>
         <h3 {...classLicenses('title')}>{messages.rulesForUse}</h3>
-        <LicenseByline withDescription licenseRights={[BY, NC, ND]} />
+        <LicenseByline withDescription licenseRights={licenseRights} />
         <a
           className="c-figure-license__link"
           href="https://creativecommons.org/licenses/by-nc-nd/3.0/no/">
@@ -62,7 +67,7 @@ export const FigureDetails = ({ children, authors, origin, messages }) => (
 
 FigureDetails.propTypes = {
   children: PropTypes.node,
-  licenseAbbreviation: PropTypes.string.isRequired,
+  licenseRights: PropTypes.arrayOf(PropTypes.string).isRequired,
   origin: PropTypes.string.isRequired,
   authors: PropTypes.arrayOf(
     PropTypes.shape({

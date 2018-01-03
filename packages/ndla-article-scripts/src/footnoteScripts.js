62d4b0194693a124ae48a660021d8bbfa3cfe56b
Oskar Kvamme <oskar@kvamme.net>
2018-01-03 09:33:07 +0100

    missing files

---
+++ packages/ndla-article-scripts/src/footnoteScripts.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import jump from 'jump.js';
+
+import { forEachElement } from './domHelpers';
+
+export const addFootnoteClickListeners = () => {
+  forEachElement('.c-footnotes__ref sup a, .c-footnotes__cite sup a', el => {
+    const target = el;
+    console.log(target.href, 'href');
+
+    const hash = target.href.slice(target.href.indexOf('#'));
+    target.onclick = () => {
+      jump(hash, { offset: -200, duration: 300 });
+    };
+  });
+};

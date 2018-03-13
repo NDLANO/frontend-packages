6fcc84a03e0ecc8efb8479f622bf2b7ebcbfd9f6
Oskar Kvamme <oskar@kvamme.net>
2018-03-13 11:50:44 +0100

    recover missing icon. icon is used

--- packages/ndla-icons/src/common/index.js
+++ packages/ndla-icons/src/common/index.js
@@ -14,6 +14,7 @@ export { default as ChevronDown } from './ChevronDown';
 export { default as ChevronLeft } from './ChevronLeft';
 export { default as ChevronRight } from './ChevronRight';
 export { default as ChevronUp } from './ChevronUp';
+export { default as Document } from './Document';
 export { default as Fullscreen } from './Fullscreen';
 export { default as Grid } from './Grid';
 export { default as Home } from './Home';

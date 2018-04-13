104b58f88512d34b5abfb57ad2ac6e053e0ce129
Øyvind Marthinsen <oma@knowit.no>
2018-04-13 14:27:50 +0200

    fix: don't load https://platform.twitter.com/widgets.js indirectly

--- packages/ndla-ui/src/Embedded/Twitter.jsx
+++ packages/ndla-ui/src/Embedded/Twitter.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Timeline } from 'react-twitter-widgets';
+/* react-twitter-widgets inserts a script tag which loads https://platform.twitter.com/widgets.js
+   when you import {Timeline} from 'react-twitter-widgets'.
+
+   Import directly to circumvent the issue and let the app determine how to load https://platform.twitter.com/widgets.js
+   */
+import Timeline from 'react-twitter-widgets/dist/components/Timeline';
 
 const EmbeddedTwitter = props => (
   <div>

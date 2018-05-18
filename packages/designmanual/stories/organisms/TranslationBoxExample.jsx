71ad7182ba93e4575e8bad1b2b5ee161760ab949
Oskar Kvamme <oskar@kvamme.net>
2018-05-18 10:28:10 +0200

    styling adjustments translation box

--- packages/designmanual/stories/organisms/TranslationBoxExample.jsx
+++ packages/designmanual/stories/organisms/TranslationBoxExample.jsx
@@ -13,12 +13,76 @@ class TranslationBoxExample extends Component {
           {
             id: 'zh-s',
             title: 'Kinesisk forenklet',
-            content: <p>Bilde innhold</p>,
+            content: (
+              <section lang="zh-HANS">
+                <h1>你好</h1>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th scope="row">李美玉丶马红：</th>
+                      <td>老师，您好。您贵姓？</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">王老师：</th>
+                      <td>你们好！我姓王。你们叫什么名字？</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">李美玉：</th>
+                      <td>我姓李，叫李美玉。</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">王老师：</th>
+                      <td>你呢？</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">马红：</th>
+                      <td>我叫马红。</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">王老师：</th>
+                      <td>认识你们很高兴！</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </section>
+            ),
           },
           {
             id: 'zh-t',
             title: 'Kinesisk tradisjonell',
-            content: <p>Video innhold</p>,
+            content: (
+              <section lang="zh-HANT">
+                <h1>你好！</h1>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th scope="row">李美玉丶馬紅：</th>
+                      <td>老師，您好。您貴姓？</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">王老師：</th>
+                      <td>你們好！我姓王。你們叫什麼名字？</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">李美玉：</th>
+                      <td>我姓李，叫李美玉。</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">王老師：</th>
+                      <td>你呢？</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">馬紅：</th>
+                      <td>我叫馬紅。</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">王老師：</th>
+                      <td>認識你們很高興！</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </section>
+            ),
           },
         ]}
       />
--- packages/ndla-ui/src/Translation/TranslationBox.jsx
+++ packages/ndla-ui/src/Translation/TranslationBox.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { ArticleTabs } from 'ndla-tabs';
 
 const TranslationBox = props => (
-  <div className="c-bodybox">
-    <ArticleTabs {...props} />
+  <div className="c-translation-box">
+    <div className="c-bodybox c-bodybox--translation">
+      <ArticleTabs {...props} />
+    </div>
   </div>
 );
 

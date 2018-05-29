c0b0047ba3d786cc59852cf8b324d651546a201b
Ivar Borthen <ivarborthen@Ivars-MacBook-Pro-2.local>
2018-05-29 11:59:02 +0200

    minor improvement to Animation component, rest params fix

--- packages/ndla-ui/src/Animation/Fade.jsx
+++ packages/ndla-ui/src/Animation/Fade.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
 
-const Fade = ({ children, ...rest }) => (
-  <CSSTransition {...rest} timeout={300} classNames="u-fade" unmountOnExit>
+const Fade = ({ children, timeout, ...rest }) => (
+  <CSSTransition unmountOnExit classNames="u-fade" timeout={timeout} {...rest}>
     {children}
   </CSSTransition>
 );
 
 Fade.propTypes = {
   children: PropTypes.node.isRequired,
+  timeout: PropTypes.number,
 };
+
+Fade.defaultProps = {
+  timeout: 300,
+}
 
 export default Fade;

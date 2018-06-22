828253669f178043cbbbd0b1a202bc286eb5c7b2
Ivar Borthen <ivarborthen@Ivars-MBP-2.lan>
2018-06-22 14:25:59 +0200

    prettier

--- packages/designmanual/images/banners/index.js
+++ packages/designmanual/images/banners/index.js
@@ -7,7 +7,15 @@ import serviceSamferdsel from './service_og_samferdsel.svg';
 export default [
   { desktop: broennfag, mobile: broennfag, name: 'Brønnteknikk' },
   { desktop: kinesisk, mobile: kinesisk, name: 'Kinesisk' },
-  { desktop: markedsfoering, mobile: markedsfoering, name: 'Markedsføring og ledelse' },
+  {
+    desktop: markedsfoering,
+    mobile: markedsfoering,
+    name: 'Markedsføring og ledelse',
+  },
   { desktop: samfunnsfag, mobile: samfunnsfag, name: 'Samfunnsfag' },
-  { desktop: serviceSamferdsel, mobile: serviceSamferdsel, name: 'Service og samferdsel' },
+  {
+    desktop: serviceSamferdsel,
+    mobile: serviceSamferdsel,
+    name: 'Service og samferdsel',
+  },
 ];

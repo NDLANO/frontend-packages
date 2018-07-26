477ace25a0c58865a3b1419e8a74056a7dfa7537
Sebastian Jara <sebastian.jara@knowit.no>
2018-07-26 15:27:54 +0200

    Missing peerDependencies, disabled eslint-rule

--- packages/eslint-config-ndla/base.js
+++ packages/eslint-config-ndla/base.js
@@ -34,7 +34,7 @@ module.exports = {
 
     'import/no-named-as-default': 0,
     'import/prefer-default-export': 0,
-    'import/no-cycle': 1,
+    'import/no-cycle': 0,
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 };

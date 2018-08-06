079fb624f7ecee1ec4a397c9e8bd49cb7317ccbd
Ivar Borthen <ivarborthen@Ivars-MacBook-Pro-2.local>
2018-08-06 12:17:02 +0200

    Tooltip fix for IE and positional fixes

--- packages/ndla-ui/src/Tooltip/Tooltip.jsx
+++ packages/ndla-ui/src/Tooltip/Tooltip.jsx
@@ -9,6 +9,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import BEMHelper from 'react-bem-helper';
+import { isMobile, isIE } from 'react-device-detect';
 
 import { Fade } from '../Animation';
 
@@ -22,14 +23,17 @@ class Tooltip extends Component {
     super(props);
     this.state = {
       showtooltip: false,
+      tooltipWidth: 0,
       disabled: props.delay === 0 || props.disabled,
     };
     this.handleShowTooltip = this.handleShowTooltip.bind(this);
     this.handleHideTooltip = this.handleHideTooltip.bind(this);
     this.delayTimer = null;
-    this.contentRef = null;
+    this.contentRef = React.createRef();
+    this.tooltipRef = React.createRef();
     this.widthRef = 0;
     this.heightRef = 0;
+    this.leftRef = 0;
   }
 
   componentDidMount() {
@@ -48,9 +52,62 @@ class Tooltip extends Component {
 
   handleShowTooltip() {
     if (!this.state.showTooltip && this.state.disabled) {
-      this.widthRef = this.contentRef.offsetWidth;
-      this.heightRef = this.contentRef.offsetHeight;
-      this.setState({ showtooltip: true });
+      this.widthRef = this.contentRef.current.offsetWidth;
+      this.heightRef = this.contentRef.current.offsetHeight;
+      const elementRect = this.contentRef.current.getBoundingClientRect();
+      this.leftRef = elementRect.left;
+      this.setState({ showtooltip: true }, () => {
+        this.currentStyles = {};
+        if (isIE) {
+          // IE is bad with transform % + px..
+          this.currentStyles.left = `-${(this.tooltipRef.current.offsetWidth -
+            this.widthRef) /
+            2}px`;
+          this.currentStyles.top = `-${this.tooltipRef.current.offsetHeight +
+            10}px`;
+        } else if (
+          this.props.align === 'top' ||
+          this.props.align === 'bottom' ||
+          (this.props.align === 'left' &&
+            this.leftRef - this.state.tooltipWidth < 20) ||
+          (this.props.align === 'right' &&
+            this.leftRef + this.widthRef + this.state.tooltipWidth >
+              window.innerWidth - 40)
+        ) {
+          const centeredLeft = this.leftRef + this.widthRef / 2;
+          let moveHorizontal = Math.max(
+            centeredLeft + this.state.tooltipWidth / 2 + 20 - window.innerWidth,
+            0,
+          );
+          if (moveHorizontal === 0) {
+            moveHorizontal = Math.min(
+              -(this.state.tooltipWidth - centeredLeft + 20),
+              0,
+            );
+          }
+          if (this.props.align === 'bottom') {
+            this.currentStyles.transform = `translate3d(calc(-50% + ${this
+              .widthRef /
+              2 -
+              moveHorizontal}px), calc(${this.heightRef}px + 0.25rem), 0)`;
+          } else {
+            this.currentStyles.transform = `translate3d(calc(-50% + ${this
+              .widthRef /
+              2 -
+              moveHorizontal}px), calc(-100% - 0.25rem), 0)`;
+          }
+        } else if (this.props.align === 'left') {
+          this.currentStyles.transform = `translate3d(calc(-100% - 0.25rem), calc(-50% + ${this
+            .heightRef / 2}px), 0)`;
+        } else {
+          this.currentStyles.transform = `translate3d(calc(${
+            this.widthRef
+          }px + 0.25rem), calc(-50% + ${this.heightRef / 2}px), 0)`;
+        }
+        this.setState({
+          tooltipWidth: this.tooltipRef.current.offsetWidth,
+        });
+      });
     }
   }
 
@@ -59,28 +116,18 @@ class Tooltip extends Component {
   }
 
   render() {
-    let transform;
-    switch (this.props.align) {
-      case 'top':
-        transform = `translate3d(calc(-50% + ${this.widthRef /
-          2}px), calc(-100% - 0.25rem), 0)`;
-        break;
-      case 'left':
-        transform = `translate3d(calc(-100% - 0.25rem), calc(-50% + ${this
-          .heightRef / 2}px), 0)`;
-        break;
-      case 'right':
-        transform = `translate3d(calc(${
-          this.widthRef
-        }px + 0.25rem), calc(-50% + ${this.heightRef / 2}px), 0)`;
-        break;
-      case 'bottom':
-        transform = `translate3d(calc(-50% + ${this.widthRef / 2}px), calc(${
-          this.heightRef
-        }px + 0.25rem), 0)`;
-        break;
-      default:
-        break;
+    // If phone ignore all tooltips //
+    if (isMobile) {
+      return (
+        <div
+          className={`${classes('').className} ${
+            this.props.tooltipContainerClass
+          }`}>
+          <span className={`c-tooltip__content ${this.props.className}`}>
+            {this.props.children}
+          </span>
+        </div>
+      );
     }
 
     return (
@@ -91,22 +138,20 @@ class Tooltip extends Component {
         <Fade in={this.state.showtooltip}>
           <span
             role="tooltip"
-            id={this.props.id}
             {...classes('tooltip')}
-            style={{ transform }}>
+            style={this.currentStyles}
+            ref={this.tooltipRef}>
             {this.props.tooltip}
           </span>
         </Fade>
         <span
           role="button"
           tabIndex={0}
-          aria-describedby={this.props.id}
-          ref={r => {
-            this.contentRef = r;
-          }}
+          aria-label={this.props.tooltip}
+          ref={this.contentRef}
           onMouseMove={this.handleShowTooltip}
           onMouseEnter={this.handleShowTooltip}
-          onMouseLeave={this.handleHideTooltip}
+          onMouseOut={this.handleHideTooltip}
           onFocus={this.handleShowTooltip}
           onBlur={this.handleHideTooltip}
           className={`c-tooltip__content ${this.props.className}`}>
@@ -118,7 +163,6 @@ class Tooltip extends Component {
 }
 
 Tooltip.propTypes = {
-  id: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   tooltip: PropTypes.string.isRequired,
   delay: PropTypes.number,

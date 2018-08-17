b4e9b46e33b4afdbfa48fcdf3cb7c668ef9e73b0
Øyvind Marthinsen <oma@knowit.no>
2018-08-17 14:45:14 +0200

    chore: rollback more changes

--- packages/ndla-i18n/src/index.js
+++ packages/ndla-i18n/src/index.js
@@ -9,24 +9,7 @@
 import IntlProvider from './IntlProvider';
 import injectT from './injectT';
 import formatMessage from './formatMessage';
-import phrasesEn from './phrases/phrases-en';
-import phrasesNb from './phrases/phrases-nb';
-import phrasesNn from './phrases/phrases-nn';
 
-const phrases = {
-  en: phrasesEn,
-  nb: phrasesNb,
-  nn: phrasesNn,
-};
-
-export {
-  IntlProvider,
-  injectT,
-  formatMessage,
-  phrases,
-  phrasesEn,
-  phrasesNb,
-  phrasesNn,
-};
+export { IntlProvider, injectT, formatMessage };
 
 export default IntlProvider;

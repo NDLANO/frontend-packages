777250888970086df5c1d2235af4b6486a4a329c
Ivar Borthen <ivarborthen@Ivars-MBP-2.lan>
2018-09-10 21:02:01 +0200

    styling fixes and privacy

--- packages/ndla-ui/src/Footer/privacyTexts.js
+++ packages/ndla-ui/src/Footer/privacyTexts.js
@@ -1,51 +1,20 @@
 import React from 'react';
 
-import { ContentGeneralNb, ContentCookiesNb } from './privacy_nb';
-import { ContentGeneralNn, ContentCookiesNn } from './privacy_nn';
-import { ContentGeneralEn, ContentCookiesEn } from './privacy_en';
+import { PrivacyNb } from './privacy_nb';
+import { PrivacyNn } from './privacy_nn';
+import { PrivacyEn } from './privacy_en';
 
 export const privacyTexts = {
   nb: {
-    linkLabel: 'Informasjonskapsler og personvern',
-    header: 'Retningslinjer for personvern og bruk av informasjonskapsler',
-    tabs: [
-      {
-        title: 'Generelt',
-        content: <ContentGeneralNb />,
-      },
-      {
-        title: 'Informasjonskapsler',
-        content: <ContentCookiesNb />,
-      },
-    ],
+    linkLabel: 'Personvernserklæring',
+    content: <PrivacyNb />,
   },
   nn: {
-    linkLabel: 'Informasjonskapslar og personvern',
-    header: 'Retningslinjer for bruk av informasjonskapslar',
-    tabs: [
-      {
-        title: 'Generelt',
-        content: <ContentGeneralNn />,
-      },
-      {
-        title: 'Informasjonskapslar',
-        content: <ContentCookiesNn />,
-      },
-    ],
+    linkLabel: 'Personvernserklæring',
+    content: <PrivacyNn />,
   },
   en: {
-    linkLabel: 'Privacy and cookies',
-    header:
-      '(Only available in norwegian). Retningslinjer for personvern og bruk av informasjonskapsler',
-    tabs: [
-      {
-        title: 'General info',
-        content: <ContentGeneralEn />,
-      },
-      {
-        title: 'Cookies',
-        content: <ContentCookiesEn />,
-      },
-    ],
+    linkLabel: 'Cookies Policy',
+    content: <PrivacyEn />,
   },
 };

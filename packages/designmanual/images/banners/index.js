8627db1980c463939895eed3e10d35b8804fe3b3
Ivar Borthen <ivarborthen@Ivars-MacBook-Pro-2.local>
2018-09-11 12:07:56 +0200

    new and updated banners

--- packages/designmanual/images/banners/index.js
+++ packages/designmanual/images/banners/index.js
@@ -1,39 +1,41 @@
-import broennfag from './brønnfag.svg';
-import kinesisk from './kinesisk.svg';
-import markedsfoering from './markedsføring.svg';
-import samfunnsfag from './samfunnsfag.svg';
-import serviceSamferdsel from './service_og_samferdsel.svg';
-import serviceSikkerhetSalg from './salg_service_sikkerhet.svg';
-import tysk from './tysk.svg';
-import historie from './historie.svg';
+import Brønnteknikk from './Brønnteknikk.svg';
+import ByggOgAnleggsteknikk from './Bygg-og-anleggsteknikk.svg';
+import Elektro from './Elektro.svg';
+import Engelsk from './Engelsk.svg';
+import Helsearbeiderfag from './Helsearbeiderfag.svg';
+import HelseOgOppvekst from './Helse-og-oppvekst.svg';
+import Historie from './Historie.svg';
+import IKTService from './IKT-service.svg';
+import Kinesisk from './Kinesisk.svg';
+import Markedsføring from './Markedsføring.svg';
+import Matematikk from './Matematikk.svg';
+import MedieuttrykkOgMediesamfunnet from './Medieuttrykk-og-mediesamfunnet.svg';
+import MedieOgInformasjonskunnskap from './Medie-og-informasjonskunnskap.svg';
+import Naturbruk from './Naturbruk.svg';
+import SalgServiceOgSikkerhet from './Salg-service-og-sikkerhet.svg';
+import Samfunnsfag from './Samfunnsfag.svg';
+import ServiceOgSamferdsel from './Service-og-samferdsel.svg';
+import Soersamisk from './Sørsamisk.svg';
+import Tysk from './Tysk.svg';
 
 export default [
-  { desktop: broennfag, mobile: broennfag, name: 'Brønnteknikk' },
-  {
-    desktop: serviceSikkerhetSalg,
-    mobile: serviceSikkerhetSalg,
-    name: 'Salg, service og sikkerhet',
-  },
-  { desktop: kinesisk, mobile: kinesisk, name: 'Kinesisk' },
-  {
-    desktop: markedsfoering,
-    mobile: markedsfoering,
-    name: 'Markedsføring og ledelse',
-  },
-  { desktop: samfunnsfag, mobile: samfunnsfag, name: 'Samfunnsfag' },
-  {
-    desktop: serviceSamferdsel,
-    mobile: serviceSamferdsel,
-    name: 'Service og samferdsel',
-  },
-  {
-    desktop: historie,
-    mobile: historie,
-    name: 'Historie',
-  },
-  {
-    desktop: tysk,
-    mobile: tysk,
-    name: 'Tysk',
-  },
+  { desktop: Brønnteknikk, mobile: Brønnteknikk, name: 'Brønnteknikk' },
+  { desktop: ByggOgAnleggsteknikk, mobile: ByggOgAnleggsteknikk, name: 'Bygg- og anleggsteknikk' },
+  { desktop: Elektro, mobile: Elektro, name: 'Elektro' },
+  { desktop: Engelsk, mobile: Engelsk, name: 'Engelsk' },
+  { desktop: Helsearbeiderfag, mobile: Helsearbeiderfag, name: 'Helse' },
+  { desktop: HelseOgOppvekst, mobile: HelseOgOppvekst, name: 'Helse og oppvekst' },
+  { desktop: Historie, mobile: Historie, name: 'Historie' },
+  { desktop: IKTService, mobile: IKTService, name: 'IKT Service' },
+  { desktop: Kinesisk, mobile: Kinesisk, name: 'Kinesisk' },
+  { desktop: Markedsføring, mobile: Markedsføring, name: 'Markedsføring og ledelse' },
+  { desktop: Matematikk, mobile: Matematikk, name: 'Matematikk' },
+  { desktop: MedieuttrykkOgMediesamfunnet, mobile: MedieuttrykkOgMediesamfunnet, name: 'Medieuttrykk og mediesamfunnet' },
+  { desktop: MedieOgInformasjonskunnskap, mobile: MedieOgInformasjonskunnskap, name: 'Medie- og informasjonskunnskap' },
+  { desktop: Naturbruk, mobile: Naturbruk, name: 'Naturbruk' },
+  { desktop: ServiceOgSamferdsel, mobile: ServiceOgSamferdsel, name: 'Service og samferdsel' },
+  { desktop: SalgServiceOgSikkerhet, mobile: SalgServiceOgSikkerhet, name: 'Salg, service og sikkerhet' },
+  { desktop: Samfunnsfag, mobile: Samfunnsfag, name: 'Samfunnsfag' },
+  { desktop: Soersamisk, mobile: Soersamisk, name: 'Sørsamisk' },
+  { desktop: Tysk, mobile: Tysk, name: 'Tysk' },
 ];

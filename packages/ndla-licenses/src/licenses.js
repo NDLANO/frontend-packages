e2cc39a4e03abb49ce92cf9277dd53744ead8765
Ivar Borthen <ivarborthen@Ivars-MBP-2.lan>
2018-09-14 11:47:44 +0200

    updated links

--- packages/ndla-licenses/src/licenses.js
+++ packages/ndla-licenses/src/licenses.js
@@ -25,7 +25,7 @@ const byncnd = {
     short: restrictedUseNN,
     title: 'Namngiving-IkkjeKommersiell-IngenTilarbeiding',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc-nd/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nc-nd/4.0/deed.no',
     linkText: openLicenseLinkTextNN,
     description:
       'Denne lisensen er den mest restriktive av dei seks kjernelisensane våre. Den tillet andre å lasta ned verket ditt og dela det med andre så lenge du er namngitt som opphavspersonen, men dei kan ikkje endra det på nokon måte, eller bruka det kommersielt.',
@@ -34,7 +34,7 @@ const byncnd = {
     short: restrictedUseNB,
     title: 'Navngivelse-IkkeKommersiell-IngenBearbeidelser',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc-nd/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nc-nd/4.0/deed.no',
     linkText: openLicenseLinkTextNB,
     description:
       'Denne lisensen er den mest restriktive av våre seks kjernelisenser. Den tillater andre å laste ned ditt verk og dele det med andre så lenge du er navngitt som opphavspersonen, men de kan ikke endre det på noen måte, eller bruke det kommersielt.',
@@ -44,7 +44,7 @@ const byncnd = {
     short: restrictedUseEN,
     title: 'Attribution-NonCommercial-NoDerivs',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc-nd/3.0/',
+    url: 'https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en',
     linkText: openLicenseLinkTextEN,
     description:
       'This license is the most restrictive of our six main licenses, only allowing others to download your works and share them with others as long as they credit you, but they can’t change them in any way or use them commercially.',
@@ -58,7 +58,7 @@ const byncsa = {
     short: restrictedUseNN,
     title: 'Namngiving-IkkjeKommersiell-DelPåSameVilkår',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc-sa/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.no',
     linkText: openLicenseLinkTextNN,
     description:
       'Denne lisensen let andre distribuera, endra, remixa, og byggja vidare på verket ditt for ikkje-kommersielle formål. Deira verk må namngje deg som den opprinnelige opphavspersonen og avledeta verk må bera ein lisens som svarar til denne.',
@@ -67,7 +67,7 @@ const byncsa = {
     short: restrictedUseNB,
     title: 'Navngivelse-IkkeKommersiell-DelPåSammeVilkår',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc-sa/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.no',
     linkText: openLicenseLinkTextNB,
     description:
       'Denne lisensen lar andre distribuere, endre, remixe, og bygge videre på ditt verk for ikke-kommersielle formål. Deres verk må navngi deg som den opprinnelige opphavspersonen og avledete verk må bære en tilsvarende lisens.',
@@ -77,7 +77,7 @@ const byncsa = {
     short: restrictedUseEN,
     title: 'Attribution-NonCommercial-ShareAlike',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc-sa/3.0/',
+    url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en',
     linkText: openLicenseLinkTextEN,
     description:
       'This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms.',
@@ -91,7 +91,7 @@ const bync = {
     short: freeUseNN,
     title: 'Namngiving-IkkjeKommersiell',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nc/4.0/deed.no',
     linkText: openLicenseLinkTextNN,
     description:
       'Denne lisensen let andre distribuera, endra, remixa, og byggja videre på verket ditt for ikkje-kommersielle formål. Verket deira må namngje deg som opphavsperson og også vera ikkje-kommersielle, men dei treng ikkje krevja at verk avleda av deira ber dei same vilkåra.',
@@ -100,7 +100,7 @@ const bync = {
     short: freeUseNB,
     title: 'Navngivelse-IkkeKommersiell',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nc/4.0/deed.no',
     linkText: openLicenseLinkTextNB,
     description:
       'Denne lisensen lar andre distribuere, endre, remixe, og bygge videre på ditt verk for ikke-kommersielle formål. Deres verk må navngi deg som opphavsperson og også være ikke-kommersielle, men de behøver ikke kreve at verk avledet fra deres bærer de samme vilkårene.',
@@ -110,7 +110,7 @@ const bync = {
     short: freeUseEN,
     title: 'Attribution-NonCommercial',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nc/3.0/',
+    url: 'https://creativecommons.org/licenses/by-nc/4.0/deed.en',
     linkText: openLicenseLinkTextEN,
     description:
       'This license lets others remix, tweak, and build upon your work non-commercially, and although their new works must also acknowledge you and be non-commercial, they don’t have to license their derivative works on the same terms.',
@@ -124,7 +124,7 @@ const bynd = {
     short: freeUseNN,
     title: 'Namngiving-IngenTilarbeiding',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nd/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nd/4.0/deed.no',
     linkText: openLicenseLinkTextNN,
     description:
       'Denne lisensen gjev høve til å distribuera verket vidare, både for kommersielle og ikkje-kommersielle formål, så lenge det vert gjeve vidare uendra og i sitt heile, og at du vert namngjeven som den som har skapt verket.',
@@ -133,7 +133,7 @@ const bynd = {
     short: freeUseNB,
     title: 'Navngivelse-IngenBearbeidelse',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nd/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-nd/4.0/deed.no',
     linkText: openLicenseLinkTextNB,
     description:
       'Denne lisensen gir mulighet for å videredistribuere verket, både for kommersielle og ikke-kommersielle formål, så lenge det gis videre uendret og i sin helhet, og at du navngis som den som har skapt verket.',
@@ -143,7 +143,7 @@ const bynd = {
     short: freeUseEN,
     title: 'Attribution-NoDerivs',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-nd/3.0/',
+    url: 'https://creativecommons.org/licenses/by-nd/4.0/deed.en',
     linkText: openLicenseLinkTextEN,
     description:
       'This license allows for redistribution, commercial and non-commercial, as long as it is passed along unchanged and in whole, with credit to you.',
@@ -157,7 +157,7 @@ const bysa = {
     short: freeUseNN,
     title: 'Namngiving-DelPåSameVilkår',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-sa/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-sa/4.0/deed.no',
     linkText: openLicenseLinkTextNN,
     description:
       'Fri gjenbruk ved namngiving. Navngiving vil seie at du oppgir namnet til opphavspersonen/ane. Om du deler, må andre også få lov til å dele vidare på same vilkår. Dette sikrar vidare gjenbruk og bidreg til delingskultur.',
@@ -166,7 +166,7 @@ const bysa = {
     short: freeUseNB,
     title: 'Navngivelse-DelPåSammeVilkår',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-sa/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by-sa/4.0/deed.no',
     linkText: openLicenseLinkTextNB,
     description:
       'Fri gjenbruk ved navngivelse. Navngivelse vil si at du oppgir navnet til opphavspersonen(e). Hvis du deler, må andre også få lov til å dele videre på samme vilkår. Dette sikrer videre gjenbruk og bidrar til delingskultur.',
@@ -176,7 +176,7 @@ const bysa = {
     short: freeUseEN,
     title: 'Attribution ShareAlike',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by-sa/3.0/',
+    url: 'https://creativecommons.org/licenses/by-sa/4.0/deed.en',
     linkText: openLicenseLinkTextEN,
     description:
       'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
@@ -190,7 +190,7 @@ const by = {
     short: freeUseNN,
     title: 'Navngivelse',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by/4.0/deed.no',
     linkText: openLicenseLinkTextNN,
     description:
       'Denne lisensen let andre distribuera, endra, remixa, og byggje vidare på verket ditt, også for kommersielle formål, så lenge dei namngir deg som den opphavlege opphavspersonen. Dette er den mest fleksible og opne lisensen vi tilbyr. Den vert tilrådd om du ynsker maksimal spreiing og bruk av materiale under ein CC-lisens.',
@@ -199,7 +199,7 @@ const by = {
     short: freeUseNB,
     title: 'Navngivelse',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by/3.0/no/',
+    url: 'https://creativecommons.org/licenses/by/4.0/deed.no',
     linkText: openLicenseLinkTextNB,
     description:
       'Denne lisensen lar andre distribuere, endre, remixe, og bygge videre på ditt verk, også for kommersielle formål, så lenge de navngir deg som den opprinnelige opphavspersonen. Dette er den mest fleksible og åpne lisensen vi tilbyr. Den anbefales dersom du ønsker maksimal spredning og bruk av materiale under en CC-lisens.',
@@ -209,7 +209,7 @@ const by = {
     short: freeUseEN,
     title: 'Attribution',
     userFriendlyTitle: '',
-    url: 'https://creativecommons.org/licenses/by/3.0/',
+    url: 'https://creativecommons.org/licenses/by/4.0/deed.en',
     linkText: openLicenseLinkTextEN,
     description:
       'This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered. Recommended for maximum dissemination and use of licensed materials.',

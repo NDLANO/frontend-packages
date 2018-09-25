a9ae4a73a786921a03d3a9eaef6b4586e9dadfc7
Øyvind Marthinsen <oma@knowit.no>
2018-09-25 12:04:03 +0200

    fix: remove editor.scss import

--- packages/designmanual/.storybook/config.js
+++ packages/designmanual/.storybook/config.js
@@ -2,7 +2,6 @@ import { configure, addDecorator } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
 import { withKnobs } from '@storybook/addon-knobs';
 import './designmanual.scss';
-import '../../ndla-ui/src/editor.scss';
 import '../../ndla-ui/src/audioSearch.scss';
 import '../../ndla-ui/src/imageSearch.scss';
 import '../../ndla-ui/src/videoSearch.scss';

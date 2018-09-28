aa76388b34f44fa792805b46251f7e8adfd6f0d0
Øyvind Marthinsen <oma@knowit.no>
2018-09-28 15:44:17 +0200

    fix: export mics

--- packages/ndla-core/src/index.js
+++ packages/ndla-core/src/index.js
@@ -2,3 +2,4 @@ export { default as breakpoints } from './breakpoints';
 export { default as colors } from './colors';
 export { default as fonts } from './fonts';
 export { default as spacing } from './spacing';
+export { default as mics } from './mics';

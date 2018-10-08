739329df56db0d6c868a97540b03a349d5e9085d
Øyvind Marthinsen <oma@knowit.no>
2018-10-08 09:18:02 +0200

    refact: use emotion in button component

--- packages/ndla-core/src/fonts.js
+++ packages/ndla-core/src/fonts.js
@@ -1,3 +1,24 @@
+const baseFontSizeUnit = 18;
+const baseLineHeightUnit = 26;
+
+/**
+ * Should do the same as: https://github.com/inuitcss/inuitcss/blob/c14029caf75b7b69d7551a5e22036ec280b02e9f/tools/_tools.font-size.scss
+ */
+function sizes(fontSize, lineHeight) {
+  const fontSizeUnit = parseInt(fontSize, 10);
+  const fontSizeRem = parseInt(fontSize, 10) / baseFontSizeUnit;
+
+  const fontSizeStyling = `font-size: ${fontSize};font-size: ${fontSizeRem}rem;`;
+  if (lineHeight) {
+    return `${fontSizeStyling} line-height: ${lineHeight}`;
+  }
+
+  const defaultLineHeight =
+    Math.ceil(fontSizeUnit / baseLineHeightUnit) *
+    (baseLineHeightUnit / fontSizeUnit);
+  return `${fontSizeStyling} line-height: ${defaultLineHeight}`;
+}
+
 export default {
   sans:
     "'Source Sans Pro',Helvetica,Arial,STKaiti,'华文楷体',KaiTi,SimKai,'楷体',KaiU,DFKai-SB,'標楷體',SongTi,'宋体',sans-serif",
@@ -9,9 +30,5 @@ export default {
     semibold: 600,
     bold: 700,
   },
-  sizes: (fontSize, lineHeightFactor) =>
-    lineHeightFactor
-      ? `font-size: ${fontSize}; line-height: ${parseInt(lineHeightFactor, 10) *
-          parseInt(fontSize, 10)}px;`
-      : `font-size: ${fontSize};`,
+  sizes,
 };

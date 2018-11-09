f2a1523224970681961ebd6108bb6a67816a9ebc
Øyvind Marthinsen <oma@knowit.no>
2018-11-09 10:32:10 +0100

    chore: bump storybook to 4.0 🎉

--- packages/designmanual/.storybook/config.js
+++ packages/designmanual/.storybook/config.js
@@ -2,16 +2,37 @@ import { configure, addDecorator } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
 import { withKnobs } from '@storybook/addon-knobs';
 import './designmanual.scss';
+import { colors, fonts } from '@ndla/core';
+import { themes } from '@storybook/components';
 
 setOptions({
   name: 'NDLA Designmanual',
-  url:
-    'https://github.com/NDLANO/frontend-packages/tree/master/packages/ndla-ui',
+  url: 'https://designmanual.ndla.sh',
   goFullScreen: false,
   showStoriesPanel: true,
   showAddonPanel: false,
   showSearchBox: false,
   addonPanelInRight: false,
+  theme: {
+    ...themes.normal,
+    mainFill: colors.background.default,
+    mainBackground: colors.background.default,
+    mainBorder: `1px solid ${colors.brand.light}`,
+    mainBorder: `none`,
+    mainTextFace: fonts.sans,
+    mainTextColor: colors.text.primary,
+    mainTextSize: 19,
+    dimmedTextColor: colors.text.light,
+    layoutMargin: 0,
+    mainBorderRadius: 0,
+    highlightColor: colors.text.primary,
+    brand: {
+      color: colors.text.light,
+      a: {
+        fontSize: '14px',
+      },
+    },
+  },
 });
 
 addDecorator(withKnobs);
--- scripts/build.js
+++ scripts/build.js
@@ -7,7 +7,7 @@
  */
 
 const chalk = require('chalk');
-const babel = require('babel-core');
+const babel = require('@babel/core');
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');

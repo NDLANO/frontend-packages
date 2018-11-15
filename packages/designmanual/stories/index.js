57d3623e42c1c521164f20aa0a1d230802a4a429
Øyvind Marthinsen <oma@knowit.no>
2018-11-15 12:44:14 +0100

    chore: revert change

--- packages/designmanual/stories/index.js
+++ packages/designmanual/stories/index.js
@@ -1,48 +1,7 @@
 import { addDecorator } from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
-// import { withKnobs } from '@storybook/addon-knobs';
-import { themes } from '@storybook/components';
-import { colors, fonts } from '@ndla/core';
 import { LanguageWrapper } from './LanguageWrapper';
 
-// addDecorator(
-//   LanguageWrapper,
-//   withOptions({
-//     name: 'NDLA Designmanual',
-//   }),
-// );
-
-addDecorator(
-  LanguageWrapper,
-  withOptions({
-    name: 'NDLA Designmanual',
-    url: 'https://designmanual.ndla.sh',
-    goFullScreen: false,
-    showStoriesPanel: true,
-    showAddonPanel: false,
-    showSearchBox: false,
-    addonPanelInRight: false,
-    theme: {
-      ...themes.normal,
-      mainFill: colors.background.default,
-      mainBackground: colors.background.default,
-      mainBorder: `none`,
-      mainTextFace: fonts.sans,
-      mainTextColor: colors.text.primary,
-      mainTextSize: 19,
-      dimmedTextColor: colors.text.light,
-      layoutMargin: 0,
-      mainBorderRadius: 0,
-      highlightColor: colors.text.primary,
-      brand: {
-        color: colors.text.light,
-        a: {
-          fontSize: '14px',
-        },
-      },
-    },
-  }),
-);
+addDecorator(LanguageWrapper);
 
 require('./welcome');
 require('./concepts');

b4ea88943b00374d0b65220fce8a33ee02b3f1a8
IvarBorthen <ivarborthen81@gmail.com>
2018-11-26 09:43:40 +0100

    wip

--- packages/designmanual/stories/pages/NdlaFilmFrontpage.jsx
+++ packages/designmanual/stories/pages/NdlaFilmFrontpage.jsx
@@ -55,6 +55,34 @@ class NdlaFilmExample extends Component {
       firebase.initializeApp(firebaseConfig);
     }
 
+    if (this.props.editor) {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          console.log(result);
+          // ...
+        })
+        .catch(error => {
+          console.log(error);
+          /*
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        */
+        });
+    }
+
     // Fetch all data from database.
     firebase
       .database()

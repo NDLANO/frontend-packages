f8e835b6d30d37d698f49acd861694d2646a7ca3
Øyvind Marthinsen <oma@knowit.no>
2018-12-06 14:45:13 +0100

    feat: some additional rules

--- packages/eslint-config-ndla/base.js
+++ packages/eslint-config-ndla/base.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['react-app'],
+  rules: {
+    'import/order': ['warn', { groups: [['builtin', 'external', 'internal']] }],
+    'import/export': 'warn',
+    'import/no-cycle': ['warn', { maxDepth: Infinity }],
+    'react/no-unused-state': 'warn',
+  },
 };

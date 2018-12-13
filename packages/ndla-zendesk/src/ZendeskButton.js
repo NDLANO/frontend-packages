099c19d7edce7d1e32cafec654cc119b64017294
Øyvind Marthinsen <oma@knowit.no>
2018-12-13 09:36:44 +0100

    feat: add mobile styling to zendesk button

--- packages/ndla-zendesk/src/ZendeskButton.js
+++ packages/ndla-zendesk/src/ZendeskButton.js
@@ -11,13 +11,24 @@ import PropTypes from 'prop-types';
 import Button from '@ndla/button';
 import { css } from 'emotion';
 
+const commonStyling = css`
+  border-radius: 2px;
+  background-color: white;
+`;
+
 const desktopStyling = css`
   position: fixed;
   right: 2rem;
   bottom: 2rem;
   padding: 0.4rem 0.6rem;
-  border-radius: 2px;
-  background-color: white;
+  ${commonStyling}
+`;
+
+const mobileStyling = css`
+  position: absolute;
+  right: 20px;
+  margin-top: -8px;
+  ${commonStyling}
 `;
 
 class ZendeskButton extends React.Component {
@@ -29,6 +40,11 @@ class ZendeskButton extends React.Component {
       if (window.zE) {
         window.zE('webWidget', 'hide');
         window.zE('webWidget', 'setLocale', locale);
+        window.zE('webWidget', 'updateSettings', {
+          webWidget: {
+            zIndex: 9999, // Reset z-index
+          },
+        });
         window.clearInterval(this.interval);
       }
     }, 50);
@@ -47,14 +63,20 @@ class ZendeskButton extends React.Component {
     document.body.appendChild(script);
   }
 
+  handleClick = () => {
+    if (window && window.zE) {
+      window.zE.activate();
+    }
+  };
+
   render() {
-    const { children, locale, ...rest } = this.props;
+    const { children, locale, isMobile, ...rest } = this.props;
 
     return (
       <Button
         type="button"
-        css={desktopStyling}
-        onClick={() => (window && window.zE ? window.zE.activate() : undefined)}
+        css={isMobile ? mobileStyling : desktopStyling}
+        onClick={this.handleClick}
         appearance="outline"
         {...rest}>
         {children}

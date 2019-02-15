11684e10bee34e2fc2c340299e88e9b2ada89d6f
Øyvind Marthinsen <oma@knowit.no>
2019-02-15 14:22:12 +0100

    chore: add react hooks eslint plugin

--- packages/eslint-config-ndla/base.js
+++ packages/eslint-config-ndla/base.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: ['react-app'],
+  plugins: ['react-hooks'],
   rules: {
     'import/order': ['warn', { groups: [['builtin', 'external', 'internal']] }],
     'import/export': 'warn',
     'import/no-cycle': ['warn', { maxDepth: Infinity }],
     'react/no-unused-state': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
   },
 };

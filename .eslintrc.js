1cf4133ce500104515c16ca5a2246ad6d67c716d
Torgeir Haugen <torgeirhaugen@gmail.com>
2019-03-12 10:31:27 +0100

    remove eslint-emotion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   extends: './packages/eslint-config-ndla/index.js',
-  plugins: ['emotion'],
   rules: {
     'import/no-extraneous-dependencies': 0,
-    'emotion/no-vanilla': 'error',
-    'emotion/import-from-emotion': 'error',
-    'emotion/styled-import': 'error',
   },
 };

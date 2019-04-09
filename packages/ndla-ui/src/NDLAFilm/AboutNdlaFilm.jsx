10205a45de43552a0f2f95e8c1200e91dcf582b4
Tale Prestmo <tale.prestmo@knowit.no>
2019-04-09 12:35:21 +0200

    Use about from frontendapi

--- packages/ndla-ui/src/NDLAFilm/AboutNdlaFilm.jsx
+++ packages/ndla-ui/src/NDLAFilm/AboutNdlaFilm.jsx
@@ -36,10 +36,15 @@ const AboutNdlaFilm = ({ aboutNDLAVideo, language, moreAboutNdlaFilm, t }) => {
   return (
     <div className="o-wrapper">
       <StyledAside>
-        <div>{aboutNDLAVideo}</div>
         <div>
-          <h1>{t('ndlaFilm.about.heading')}</h1>
-          <p>{t('ndlaFilm.about.text')}</p>
+          <img
+            src={aboutNDLAVideo.visualElement.url}
+            alt={aboutNDLAVideo.visualElement.alt}
+          />
+        </div>
+        <div>
+          <h1>{aboutNDLAVideo.title}</h1>
+          <p>{t(aboutNDLAVideo.description)}</p>
           <Modal
             activateButton={<Button link>{t('ndlaFilm.about.more')}</Button>}>
             {onClose => (

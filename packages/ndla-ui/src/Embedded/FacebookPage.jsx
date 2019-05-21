f0785ca6b26387cc1cddbf1135a498c45e128c88
Alireza Balouch <alireza.balouch@knowit.no>
2019-05-21 13:56:56 +0200

    added height and 500 as default and set adaptContainerWidth to true

--- packages/ndla-ui/src/Embedded/FacebookPage.jsx
+++ packages/ndla-ui/src/Embedded/FacebookPage.jsx
@@ -5,13 +5,20 @@ import { FacebookProvider, Page } from 'react-facebook';
 const EmbeddedFacebookPage = props => (
   <div>
     <FacebookProvider appId="155745961798881">
-      <Page href={props.href} tabs="timeline" className="fb-embedded" />
+      <Page
+        href={props.href}
+        height={props.height || 500}
+        tabs="timeline"
+        className="fb-embedded"
+        adaptContainerWidth={true}
+      />
     </FacebookProvider>
   </div>
 );
 
 EmbeddedFacebookPage.propTypes = {
   href: PropTypes.string.isRequired,
+  height: PropTypes.number,
 };
 
 export default EmbeddedFacebookPage;

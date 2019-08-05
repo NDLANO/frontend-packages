4a8cb2d7bb17e0cab68f86188833e010854f0429
Torgeir Haugen <torhau@knowit.no>
2019-08-05 13:47:36 +0200

    simplify searchField

--- packages/ndla-ui/src/Search/SearchField.jsx
+++ packages/ndla-ui/src/Search/SearchField.jsx
@@ -11,7 +11,6 @@ import PropTypes from 'prop-types';
 import BEMHelper from 'react-bem-helper';
 import { Search as SearchIcon } from '@ndla/icons/common';
 import { injectT } from '@ndla/i18n';
-import { SearchResultSleeve } from './SearchResultSleeve';
 
 import ActiveFilters from './ActiveFilters';
 
@@ -30,31 +29,9 @@ const messagesShape = PropTypes.shape({
 class SearchField extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputHasFocus: false,
-    };
+
     this.inputRef = React.createRef();
     this.handleOnFilterRemove = this.handleOnFilterRemove.bind(this);
-    this.onInputBlur = this.onInputBlur.bind(this);
-    this.onInputFocus = this.onInputFocus.bind(this);
-  }
-
-  onInputBlur() {
-    this.setState({
-      inputHasFocus: false,
-    });
-    if (this.props.onBlur) {
-      this.props.onBlur();
-    }
-  }
-
-  onInputFocus() {
-    this.setState({
-      inputHasFocus: true,
-    });
-    if (this.props.onFocus) {
-      this.props.onFocus();
-    }
   }
 
   handleOnFilterRemove(value, filterName) {
@@ -68,105 +45,60 @@ class SearchField extends Component {
       value,
       onChange,
       filters,
-      searchResult,
       messages,
-      allResultUrl,
-      onSearch,
-      resourceToLinkProps,
       small,
-      autofocus,
-      onNavigate,
-      modifiers: modifiersProp,
-      hideSleeveHeader,
-      singleColumn,
-      infoText,
-      ignoreContentTypeBadge,
       onClick,
       t,
+      onFocus,
+      onBlur,
     } = this.props;
 
-    const hasSearchResult = searchResult && searchResult.length > 0;
-
-    let searchResultView = null;
-
-    const modifiers = [...modifiersProp];
-
-    if (hasSearchResult) {
-      modifiers.push('has-search-result');
-
-      searchResultView = (
-        <SearchResultSleeve
-          ignoreContentTypeBadge={ignoreContentTypeBadge}
-          result={searchResult}
-          searchString={value}
-          allResultUrl={allResultUrl}
-          resourceToLinkProps={resourceToLinkProps}
-          autofocus={autofocus}
-          hideSleeveHeader={hideSleeveHeader}
-          onNavigate={onNavigate}
-          singleColumn={singleColumn}
-          infoText={infoText}
-          t={t}
-        />
-      );
-    }
-
-    if (filters && filters.length > 0) {
-      modifiers.push('has-filter');
-    }
-
-    if (this.state.inputHasFocus) {
-      modifiers.push('input-has-focus');
-    }
     return (
-      <form action="/search/" {...classes('', modifiers)} onSubmit={onSearch}>
-        <div {...classes('input-wrapper')}>
-          <input
-            ref={this.inputRef}
-            title={messages.searchFieldTitle}
-            type="search"
-            {...classes('input', { small })}
-            aria-autocomplete="list"
-            autoComplete="off"
-            id="search"
-            name="search"
-            placeholder={placeholder}
-            aria-label={placeholder}
-            value={value}
-            onChange={e => onChange(e.target.value)}
-            onBlur={this.onInputBlur}
-            onFocus={this.onInputFocus}
-            onClick={onClick}
-          />
-          {filters && filters.length > 0 && (
-            <div {...classes('filters')}>
-              <ActiveFilters
-                filters={filters}
-                onFilterRemove={this.handleOnFilterRemove}
-              />
-            </div>
-          )}
-          {value !== '' && (
-            <button
-              {...classes('button', 'close')}
-              type="button"
-              onClick={() => {
-                onChange('');
-                this.inputRef.current.focus();
-              }}>
-              {t('welcomePage.resetSearch')}
-            </button>
-          )}
+      <div {...classes('input-wrapper')}>
+        <input
+          ref={this.inputRef}
+          title={messages.searchFieldTitle}
+          type="search"
+          {...classes('input', { small })}
+          aria-autocomplete="list"
+          autoComplete="off"
+          id="search"
+          name="search"
+          placeholder={placeholder}
+          aria-label={placeholder}
+          value={value}
+          onChange={e => onChange(e.target.value)}
+          onBlur={onBlur}
+          onFocus={onFocus}
+          onClick={onClick}
+        />
+        {filters && filters.length > 0 && (
+          <div {...classes('filters')}>
+            <ActiveFilters
+              filters={filters}
+              onFilterRemove={this.handleOnFilterRemove}
+            />
+          </div>
+        )}
+        {value !== '' && (
           <button
-            tabIndex="-1"
-            {...classes('button')}
-            type="submit"
-            value="Search">
-            <SearchIcon />
+            {...classes('button', 'close')}
+            type="button"
+            onClick={() => {
+              onChange('');
+              this.inputRef.current.focus();
+            }}>
+            {t('welcomePage.resetSearch')}
           </button>
-        </div>
-        {searchResultView}
-      </form>
+        )}
+        <button
+          tabIndex="-1"
+          {...classes('button')}
+          type="submit"
+          value="Search">
+          <SearchIcon />
+        </button>
+      </div>
     );
   }
 }
@@ -175,7 +107,6 @@ SearchField.propTypes = {
   value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  onSearch: PropTypes.func.isRequired,
   filters: PropTypes.arrayOf(
     PropTypes.shape({
       value: PropTypes.string.isRequired,
@@ -193,16 +124,11 @@ SearchField.propTypes = {
   t: PropTypes.func.isRequired,
   onFocus: PropTypes.func,
   onBlur: PropTypes.func,
-  modifiers: PropTypes.arrayOf(PropTypes.string),
   hideSleeveHeader: PropTypes.bool,
   singleColumn: PropTypes.bool,
   infoText: PropTypes.node,
   ignoreContentTypeBadge: PropTypes.bool,
   onClick: PropTypes.func,
-};
-
-SearchField.defaultProps = {
-  modifiers: [],
 };
 
 export default injectT(SearchField);

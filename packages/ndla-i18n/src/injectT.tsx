2cb4a6418bb9a3c940a42cf91af3a76802f42e61
Christian Peter <christian150790@gmail.com>
2019-08-12 14:14:33 +0200

    Adds possible fix for ts

--- packages/ndla-i18n/src/injectT.tsx
+++ packages/ndla-i18n/src/injectT.tsx
@@ -9,21 +9,7 @@
 import React, { ComponentType, SFC, ReactElement } from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 import { TFunctionValue } from 't';
-import { any } from 'prop-types';
-
-/*export type ConsistentWith<DecorationTargetProps, InjectedProps> = {
-  [P in keyof DecorationTargetProps]: P extends keyof InjectedProps
-    ? InjectedProps[P] extends DecorationTargetProps[P]
-      ? DecorationTargetProps[P]
-      : InjectedProps[P]
-    : DecorationTargetProps[P]
-};*/
-
-/**
- * a function that takes {component} and returns a component that passes along
- * all the props to {component} except the {InjectedProps} and will accept
- * additional {AdditionalProps}
- */
+import { Subtract } from 'utility-types';
 
 export interface InjectedProps {
   t(arg: string, obj?: { [key: string]: string | boolean | number }): string;
@@ -33,56 +19,29 @@ interface Context {
   [key: string]: any;
   formatMessage: (...args: ConstructorParameters<any>) => any;
 }
-export type WithRouterProps<
-  C extends React.ComponentType<any>
-> = C extends React.ComponentClass
-  ? { wrappedComponentRef?: React.Ref<InstanceType<C>> }
-  : {};
-
-export interface WithRouterStatics<C extends React.ComponentType<any>> {
-  WrappedComponent: C;
-}
-export type Omit<T, K extends keyof T> = T extends any
-  ? Pick<T, Exclude<keyof T, K>>
-  : never;
 
 export function injectT<P extends InjectedProps>(
-  WrappedComponent: React.ComponentType<P>,
+  WrappedComponent: React.ComponentType<P & InjectedProps>,
   prefix: string = '',
-): React.ComponentType<Omit<P, keyof InjectedProps>> {
-  const getDisplayName = (component: ComponentType<P>) =>
-    component.displayName || component.name || 'Component';
+): React.ComponentType<P> {
+  const getDisplayName = (
+    component: ComponentType<P & InjectedProps>,
+  ): string => component.displayName || component.name || 'Component';
 
-  const InjectT = (props: P, context: Context) => {
-    return (
-      <WrappedComponent
-        {...props as P}
-        t={(id: string, value: TFunctionValue = {}): string =>
-          context.formatMessage(prefix + id, value)
-        }
-      />
-    );
+  const InjectT = (
+    props: P,
+    context: Context,
+  ): React.ReactElement<Omit<P, 't'>> => {
+    const composedProps = {
+      ...props,
+      t: (id: string, value: TFunctionValue = {}): string =>
+        context.formatMessage(prefix + id, value),
+    } as P & InjectedProps;
+    return <WrappedComponent {...composedProps} />;
   };
 
   InjectT.displayName = `InjectT(${getDisplayName(WrappedComponent)})`;
-  return hoistNonReactStatics(InjectT, WrappedComponent);
+  return InjectT;
 }
 
 export default injectT;
-
-/*function injectT<P extends {}>(
-  Component: ComponentType<P>,
-  prefix: string = '',
-) {
-  return function(props: any, context: any) {
-    return (
-      <Component
-        {...props}
-        t={(id: string, value: TFunctionValue = {}): string =>
-          context.formatMessage(prefix + id, value)
-        }
-      />
-    );
-  };
-}
-export default injectT;*/

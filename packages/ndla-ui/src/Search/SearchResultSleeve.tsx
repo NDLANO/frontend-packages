5037c163ec48e00f1c62c5745e6955b60a3d5622
Ivar Borthen <ivarborthen@Ivars-MacBook-Pro.local>
2019-08-14 12:42:04 +0200

    new icons + wip rewrite scss to emotion

--- packages/ndla-ui/src/Search/SearchResultSleeve.tsx
+++ packages/ndla-ui/src/Search/SearchResultSleeve.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import BEMHelper from 'react-bem-helper';
+import { css } from '@emotion/core';
+import { spacing, colors } from '@ndla/core';
 // @ts-ignore
 import { StyledButton } from '@ndla/button';
 // @ts-ignore
 import { injectT } from '@ndla/i18n';
-import { css } from '@emotion/core';
 // @ts-ignore
-import { Wrench } from '@ndla/icons/common';
+import { Wrench, Esc, KeyboardReturn, ChevronLeft, ChevronRight } from '@ndla/icons/common';
 import SafeLink from '../common/SafeLink';
 // @ts-ignore
 import ContentTypeResult from './ContentTypeResult';
@@ -14,7 +16,35 @@ import { ContentTypeResultType, Resource } from '../types';
 const AnchorButton = StyledButton.withComponent(SafeLink);
 const classes = new BEMHelper('c-search-field');
 
-interface Props {
+const StyledSearchResultsWrapper = styled.section`
+  background: #fff;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 58px;
+`;
+
+const StyledScrollableContent = styled.div`
+  max-height: 500px;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
+  overflow-x: hidden;
+  padding: ${spacing.large};
+`;
+
+const StyledFooter = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const StyledInstructions = styled.div`
+  background: ${colors.brand.greyLight};
+  flex: 1;
+  padding: ${spacing.xsmall} ${spacing.normal};
+`;
+
+type Props = {
   result: Array<ContentTypeResultType>;
   allResultUrl: string;
   resourceToLinkProps: (resource: Resource) => string;
@@ -23,7 +53,8 @@ interface Props {
   infoText: string;
   ignoreContentTypeBadge: boolean;
   t: (v: string) => {};
-}
+};
+
 const SearchResultSleeve: React.FC<Props> = ({
   result,
   allResultUrl,
@@ -34,42 +65,44 @@ const SearchResultSleeve: React.FC<Props> = ({
   ignoreContentTypeBadge,
   t,
 }) => (
-  <section {...classes('search-result')}>
-    {!hideSleeveHeader && (
-      <h1 {...classes('search-result-heading')}>
-        {t('searchPage.searchField.searchResultHeading')}
-      </h1>
-    )}
-    {infoText && (
-      <aside {...classes('search-result-infotext')}>
-        <Wrench className="c-icon--22" />
-        <span>{infoText}</span>
-      </aside>
-    )}
-    <div>
-      {result.map((contentTypeResult: ContentTypeResultType) => (
-        <ContentTypeResult
-          ignoreContentTypeBadge={ignoreContentTypeBadge}
-          onNavigate={onNavigate}
-          contentTypeResult={contentTypeResult}
-          resourceToLinkProps={resourceToLinkProps}
-          defaultCount={window.innerWidth > 980 ? 7 : 3}
-          key={contentTypeResult.title}
-          messages={{
-            allResultLabel: t(
-              'searchPage.searchField.contentTypeResultShowMoreLabel',
-            ),
-            showLessResultLabel: t(
-              'searchPage.searchField.contentTypeResultShowLessLabel',
-            ),
-            noHit: t('searchPage.searchField.contentTypeResultNoHit'),
-          }}
-        />
-      ))}
-      {result.length === 0 &&
-        t('searchPage.searchField.contentTypeResultNoHit')}
-    </div>
-    <div {...classes('go-to-search')}>
+  <StyledSearchResultsWrapper>
+    <StyledScrollableContent>
+      {!hideSleeveHeader && (
+        <h1 {...classes('search-result-heading')}>
+          {t('searchPage.searchField.searchResultHeading')}
+        </h1>
+      )}
+      {infoText && (
+        <aside {...classes('search-result-infotext')}>
+          <Wrench className="c-icon--22" />
+          <span>{infoText}</span>
+        </aside>
+      )}
+      <div>
+        {result.map((contentTypeResult: ContentTypeResultType) => (
+          <ContentTypeResult
+            ignoreContentTypeBadge={ignoreContentTypeBadge}
+            onNavigate={onNavigate}
+            contentTypeResult={contentTypeResult}
+            resourceToLinkProps={resourceToLinkProps}
+            defaultCount={window.innerWidth > 980 ? 7 : 3}
+            key={contentTypeResult.title}
+            messages={{
+              allResultLabel: t(
+                'searchPage.searchField.contentTypeResultShowMoreLabel',
+              ),
+              showLessResultLabel: t(
+                'searchPage.searchField.contentTypeResultShowLessLabel',
+              ),
+              noHit: t('searchPage.searchField.contentTypeResultNoHit'),
+            }}
+          />
+        ))}
+        {result.length === 0 &&
+          t('searchPage.searchField.contentTypeResultNoHit')}
+      </div>
+    </StyledScrollableContent>
+    <StyledFooter>
       <AnchorButton
         to={allResultUrl}
         css={css`
@@ -77,8 +110,14 @@ const SearchResultSleeve: React.FC<Props> = ({
         `}>
         {t('searchPage.searchField.allResultButtonText')}
       </AnchorButton>
-    </div>
-  </section>
+      <StyledInstructions>
+        <Esc />
+        <KeyboardReturn />
+        <ChevronLeft />
+        <ChevronRight />
+      </StyledInstructions>
+    </StyledFooter>
+  </StyledSearchResultsWrapper>
 );
 
 export default injectT(SearchResultSleeve);

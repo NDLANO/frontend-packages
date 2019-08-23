d4dec9abfae241778472372ead7767a35f84cb38
Torgeir Haugen <torhau@knowit.no>
2019-08-23 13:09:32 +0200

    refactor modal with reach/dialog

---
+++ packages/ndla-modal/src/StyledDialogOverlay.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+// @ts-ignore
+import { DialogOverlay } from '@reach/dialog';
+import { css } from '@emotion/core';
+interface Props {
+  animateIn: boolean;
+  animationDuration: string;
+}
+
+const dialogStyles = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(1, 1, 1, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledDialogOverlay: React.FC<Props> = ({
+  animateIn,
+  animationDuration = '400ms',
+  children,
+  ...props
+}) => {
+  return (
+    <DialogOverlay
+      css={css`
+        animation-name: ${animateIn ? 'fadeIn' : 'fadeOut'};
+        animation-duration: ${animationDuration};
+        ${dialogStyles}
+      `}
+      {...props}>
+      {children}
+    </DialogOverlay>
+  );
+};

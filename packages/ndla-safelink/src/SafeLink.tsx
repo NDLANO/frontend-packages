e86c56f1b6fd12f1fc843c1fb02d78c5398daf68
Christian Peter <christian150790@gmail.com>
2019-08-26 13:06:07 +0200

    Adds safelink package

---
+++ packages/ndla-safelink/src/SafeLink.tsx
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import * as React from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { LocationDescriptor } from 'history';
+import isString from 'lodash/isString';
+import SafeLinkContext from './SafeLinkContext';
+
+const isExternalLink = (to?: LocationDescriptor) =>
+  to &&
+  isString(to) &&
+  (to.startsWith('https://') || to.startsWith('https://'));
+
+export const isOldNdlaLink = (to?: LocationDescriptor) =>
+  to && isString(to) && to.match(/(.*)\/?node\/(\d+).*/) !== null;
+
+// Fallback to normal link if app is missing RouterContext, link is external or is old ndla link
+const SafeLink: React.FunctionComponent<
+  LinkProps & React.HTMLAttributes<HTMLElement>
+> = ({ to, replace, children, ...rest }) => {
+  const isSafeLinkContext = React.useContext(SafeLinkContext);
+  if (!isSafeLinkContext || isExternalLink(to) || isOldNdlaLink(to)) {
+    const href = typeof to === 'string' ? to : '#';
+    return (
+      <a href={href} {...rest}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <Link to={to} replace={replace} {...rest}>
+      {children}
+    </Link>
+  );
+};
+
+export default SafeLink;
---
+++ packages/ndla-safelink/src/SafeLinkButton.tsx
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2019-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React from 'react';
+import { css } from '@emotion/core';
+// @ts-ignore
+import { buttonStyle, appearances } from '@ndla/button';
+import SafeLink from './SafeLink';
+
+interface StylesProps {
+  [key: string]: boolean | undefined;
+}
+
+const getStyles = (modifiers: StylesProps) =>
+  Object.keys(modifiers)
+    .map(key => (modifiers[key] ? appearances[key] : undefined))
+    .filter(appearance => !!appearance);
+
+interface Props {
+  outline?: boolean;
+  stripped?: boolean;
+  link?: boolean;
+  lighter?: boolean;
+  inverted?: boolean;
+  invertedOutline?: boolean;
+  children: React.ReactNode;
+  to: string;
+  className?: string;
+}
+
+const SafeLinkButton: React.FunctionComponent<
+  Props & React.HTMLProps<HTMLAnchorElement>
+> = ({
+  outline,
+  stripped,
+  link,
+  lighter,
+  children,
+  inverted,
+  invertedOutline,
+  to,
+  ...rest
+}) => {
+  const modifierStyles = getStyles({
+    link,
+    outline,
+    lighter,
+    stripped,
+    inverted,
+    invertedOutline,
+  });
+
+  return (
+    <SafeLink
+      to={to}
+      css={css`
+        ${buttonStyle}
+        ${modifierStyles}
+      `}
+      {...rest}>
+      {children}
+    </SafeLink>
+  );
+};
+
+export default SafeLinkButton;

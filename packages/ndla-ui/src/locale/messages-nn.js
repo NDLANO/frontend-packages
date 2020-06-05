e658e1aab4d6f814a2f28ba1afb23d15056a3eb6
Bjørnar Helland <me@bjoernar.com>
2020-06-05 11:51:02 +0200

    Update packages/ndla-ui/src/locale/messages-nn.js
    Co-authored-by: Gunnar Velle <gunnar.velle@gmail.com>

--- packages/ndla-ui/src/locale/messages-nn.js
+++ packages/ndla-ui/src/locale/messages-nn.js
@@ -663,7 +663,7 @@ const messages = {
   frontPageToolbox: {
     heading: 'Verktøykassa',
     text:
-      'Har du lyst til å bli god til å presentere, eller vil du lære å studere smartare ved hjelp av riktig studieteknikk? Treng du råd om korleis du les mest mogleg effektivt til eksamen? I verktøykassen til NDLA finn du masse gode tips og råd!',
+      'Har du lyst til å bli god til å presentere, eller vil du lære å studere smartare ved hjelp av riktig studieteknikk? Treng du råd om korleis du les mest mogleg effektivt til eksamen? I verktøykassa til NDLA finn du masse gode tips og råd!',
     linkText: 'Sjå alle tipsa i verktøykassa her',
   },
   frontpageMultidisciplinarySubject: {

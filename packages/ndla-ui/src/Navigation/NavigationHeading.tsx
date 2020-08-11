ada92b71769312690c060194d64d22211fa6f2b1
petter <petter@keyteq.no>
2020-08-11 13:53:26 +0200

    navigation components invertedStyle

--- packages/ndla-ui/src/Navigation/NavigationHeading.tsx
+++ packages/ndla-ui/src/Navigation/NavigationHeading.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 import { breakpoints, fonts, mq } from '@ndla/core';
 
-const StyledH1 = styled.h1`
+type InvertItProps = {
+  invertedStyle?: boolean;
+};
+
+const StyledH1 = styled.h1<InvertItProps>`
   ${fonts.sizes('24px', '28px')};
   margin: 30px 0 20px 0;
   font-weight: ${fonts.weight.bold};
@@ -16,6 +21,11 @@ const StyledH1 = styled.h1`
     margin: 50px 0 24px;
     ${fonts.sizes('52px', '65px')};
   }
+  ${props =>
+    props.invertedStyle &&
+    css`
+      color: #fff;
+    `}
 `;
 const StyledMainText = styled.span`
   display: block;
@@ -36,10 +46,15 @@ const StyledSubText = styled.span`
 type Props = {
   subHeading?: string;
   children: React.ReactNode;
+  invertedStyle?: boolean;
 };
 
-export const NavigationHeading = ({ subHeading, children }: Props) => (
-  <StyledH1>
+export const NavigationHeading = ({
+  subHeading,
+  children,
+  invertedStyle,
+}: Props) => (
+  <StyledH1 invertedStyle={invertedStyle}>
     <StyledMainText>{children}</StyledMainText>
     {subHeading && <StyledSubText>{subHeading}</StyledSubText>}
   </StyledH1>

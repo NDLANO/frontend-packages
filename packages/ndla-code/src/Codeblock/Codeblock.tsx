bc7f9d2e45421387ab4e99e8498c6db91d1be9c6
Gunnar Velle <gunnar.velle@gmail.com>
2020-10-13 11:27:26 +0200

    Test without async

--- packages/ndla-code/src/Codeblock/Codeblock.tsx
+++ packages/ndla-code/src/Codeblock/Codeblock.tsx
@@ -7,7 +7,7 @@
  */
 
 import React, { FC, useState, useEffect } from 'react';
-import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { coy } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { colors } from '@ndla/core';
 import styled from '@emotion/styled';

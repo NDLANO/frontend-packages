0a968e63dca839c41159eddd9a282212d727387a
Gunnar Velle <gunnar.velle@gmail.com>
2021-02-11 09:45:41 +0100

    Readding rule but with warning

--- packages/eslint-config-ndla/base.js
+++ packages/eslint-config-ndla/base.js
@@ -7,5 +7,6 @@ module.exports = {
     'import/no-cycle': ['warn', { maxDepth: Infinity }],
     'react/no-unused-state': 'warn',
     'react-hooks/rules-of-hooks': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
 };

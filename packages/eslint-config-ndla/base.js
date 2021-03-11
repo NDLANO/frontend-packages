985b83c1bf14519914e89444e2b660c3a2cb8840
Gunnar Velle <gunnar.velle@gmail.com>
2021-03-11 12:41:46 +0100

    Reinstate overrides for now

--- packages/eslint-config-ndla/base.js
+++ packages/eslint-config-ndla/base.js
@@ -9,4 +9,36 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        camelcase: 'off',
+        indent: 'off',
+        'react/prop-types': 'off',
+        'no-array-constructor': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/consistent-type-assertions': 'warn',
+        '@typescript-eslint/no-array-constructor': 'warn',
+        '@typescript-eslint/no-namespace': 'error',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            args: 'none',
+            ignoreRestSiblings: true,
+          },
+        ],
+      },
+    },
+  ],
 };

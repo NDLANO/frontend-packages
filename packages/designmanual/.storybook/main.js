fcbad96ad00ba23def32da96ceb71d494649bef2
beery <me@bjoernar.com>
2021-03-23 08:54:13 +0100

    Upgrade Storybook to v6.1.21

--- packages/designmanual/.storybook/main.js
+++ packages/designmanual/.storybook/main.js
@@ -1,8 +1,23 @@
 module.exports = {
   stories: ['../stories/index.js'],
-  addons: [
-    '@storybook/addon-knobs',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: config => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      loaders: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            config: {
+              path: './.storybook/postcss.config.js',
+            },
+          },
+        },
+        'sass-loader',
+      ],
+    });
+    return config;
+  },
 };

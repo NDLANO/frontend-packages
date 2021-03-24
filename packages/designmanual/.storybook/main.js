16afffe7fea59795cc8154234860eaae9a719742
Gunnar Velle <gunnar.velle@gmail.com>
2021-03-24 11:36:22 +0100

    forgot formatting

--- packages/designmanual/.storybook/main.js
+++ packages/designmanual/.storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../stories/index.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-a11y', '@storybook/addon-links'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
   webpackFinal: config => {
     config.module.rules.push({
       test: /\.scss$/,

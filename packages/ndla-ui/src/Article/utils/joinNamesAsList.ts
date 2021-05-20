47a05ca985d8dd2b01c5cd0223bdcaf197184271
Aron Høyer <aron@hoyer.com>
2021-05-20 12:30:54 +0200

    removes extraneous if check

--- packages/ndla-ui/src/Article/utils/joinNamesAsList.ts
+++ packages/ndla-ui/src/Article/utils/joinNamesAsList.ts
@@ -6,16 +6,12 @@ export const joinNamesAsList = (names: string[], options: ListOptions) => {
   let joinedNames = '';
 
   names.forEach((name, i) => {
-    if (names.length === 0) {
-      joinedNames = names[0];
+    if (i === 0) {
+      joinedNames += name;
+    } else if (i === names.length - 1) {
+      joinedNames += ` ${options.conjunction} ${name}`;
     } else {
-      if (i === 0) {
-        joinedNames += name;
-      } else if (i === names.length - 1) {
-        joinedNames += ` ${options.conjunction} ${name}`;
-      } else {
-        joinedNames += `, ${name}`;
-      }
+      joinedNames += `, ${name}`;
     }
   });
 

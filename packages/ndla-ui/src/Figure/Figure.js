481f2515ff1ee767d99a467ba3d87a45d7ee1441
aod <anders.dahlin@knowit.no>
2021-06-22 10:00:19 +0200

    css og t

--- packages/ndla-ui/src/Figure/Figure.js
+++ packages/ndla-ui/src/Figure/Figure.js
@@ -23,59 +23,61 @@ const classes = new BEMHelper({
   prefix: 'c-',
 });
 
-export const FigureCaption = ({
-  figureId,
-  id,
-  children,
-  caption,
-  authors,
-  reuseLabel,
-  licenseRights,
-  locale,
-  link,
-  hideFigcaption,
-  synstolketVideo,
-  t,
-}) => (
-  <figcaption {...classes('caption', hideFigcaption && 'hidden-caption')}>
-    {caption ? <div {...classes('info')}>{caption}</div> : null}
-    <footer {...classes('byline')}>
-      <div {...classes('byline-licenselist')}>
-        <LicenseByline licenseRights={licenseRights} locale={locale} marginRight>
-          <span {...classes('byline-authors')}>
-            {authors.map(author => author.name).join(', ')}
-          </span>
-          <button
-            type="button"
-            data-dialog-trigger-id={id}
-            data-dialog-source-id={figureId}
-            {...classes('captionbtn')}>
-            <span>{reuseLabel}</span>
-          </button>
-          {synstolketVideo && (
-            <button type="button" {...classes('toggleSynstolket')}>
-              <span className="original">{t('figure.button.synstolket')}</span>
-              <span className="synstolket hidden">{t('figure.button.original')}</span>
+export const FigureCaption = injectT(
+  ({
+    figureId,
+    id,
+    children,
+    caption,
+    authors,
+    reuseLabel,
+    licenseRights,
+    locale,
+    link,
+    hideFigcaption,
+    synstolketVideo,
+    t,
+  }) => (
+    <figcaption {...classes('caption', hideFigcaption && 'hidden-caption')}>
+      {caption ? <div {...classes('info')}>{caption}</div> : null}
+      <footer {...classes('byline')}>
+        <div {...classes('byline-licenselist')}>
+          <LicenseByline licenseRights={licenseRights} locale={locale} marginRight>
+            <span {...classes('byline-authors')}>
+              {authors.map(author => author.name).join(', ')}
+            </span>
+            <button
+              type="button"
+              data-dialog-trigger-id={id}
+              data-dialog-source-id={figureId}
+              {...classes('captionbtn')}>
+              <span>{reuseLabel}</span>
             </button>
+            {synstolketVideo && (
+              <button type="button" {...classes('toggleSynstolket')}>
+                <span className="original">{t('figure.button.synstolket')}</span>
+                <span className="synstolket hidden">{t('figure.button.original')}</span>
+              </button>
+            )}
+            {children}
+          </LicenseByline>
+          {link && (
+            <div {...classes('link-wrapper')}>
+              <SafeLink
+                to={link.url}
+                {...classes('link')}
+                target={link.external ? '_blank' : null}
+                rel={link.external ? 'noopener noreferrer' : null}>
+                <span {...classes('link-text')}>{link.text}</span>
+                <LinkIcon />
+              </SafeLink>
+              {link.description && <p {...classes('link-description')}>{link.description}</p>}
+            </div>
           )}
-          {children}
-        </LicenseByline>
-        {link && (
-          <div {...classes('link-wrapper')}>
-            <SafeLink
-              to={link.url}
-              {...classes('link')}
-              target={link.external ? '_blank' : null}
-              rel={link.external ? 'noopener noreferrer' : null}>
-              <span {...classes('link-text')}>{link.text}</span>
-              <LinkIcon />
-            </SafeLink>
-            {link.description && <p {...classes('link-description')}>{link.description}</p>}
-          </div>
-        )}
-      </div>
-    </footer>
-  </figcaption>
+        </div>
+      </footer>
+    </figcaption>
+  ),
 );
 
 FigureCaption.propTypes = {

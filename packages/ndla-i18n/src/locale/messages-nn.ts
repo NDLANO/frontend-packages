f2cad0853f1a56cae4dc0d546f6a7bc00cd7c479
sigrunnu <sigrun.nummedal@gmail.com>
2021-07-27 14:06:37 +0200

    Format

--- packages/ndla-i18n/src/locale/messages-nn.ts
+++ packages/ndla-i18n/src/locale/messages-nn.ts
@@ -197,8 +197,7 @@ const messages = {
     },
     socialMedia: {
       heading: 'Følg oss',
-      description:
-        'NDLA har mange Facebook- og Twitter-kontoar. Finn den som passar for deg, og følg oss!',
+      description: 'NDLA har mange Facebook- og Twitter-kontoar. Finn den som passar for deg, og følg oss!',
       mainLink: {
         name: 'Følg oss',
       },
@@ -212,8 +211,7 @@ const messages = {
     },
     film: {
       header: 'NDLA film',
-      text:
-        'NDLA film er ei teneste i samarbeid med Norgesfilm. Denne tenesta lar deg sjå ei rekkje spelefilmar, kortfilmar, dokumentarar og seriar. Du kan òg sjå undervisningsfilm og filmklipp. Velkomen inn i filmen si verd!',
+      text: 'NDLA film er ei teneste i samarbeid med Norgesfilm. Denne tenesta lar deg sjå ei rekkje spelefilmar, kortfilmar, dokumentarar og seriar. Du kan òg sjå undervisningsfilm og filmklipp. Velkomen inn i filmen si verd!',
       textShort: 'Velkomen inn i filmen si verd!',
       linkLabel: 'Gå til NDLA film',
     },
@@ -221,8 +219,7 @@ const messages = {
     errorDescription: 'Orsak, ein feil oppstod under lasting av faga.',
   },
   meta: {
-    description:
-      'Kvalitetssikra fritt tilgjengelege nettbaserte læremiddel for vidaregåande opplæring',
+    description: 'Kvalitetssikra fritt tilgjengelege nettbaserte læremiddel for vidaregåande opplæring',
     keywords: 'læremiddel,fag,skole,skule,vidaregåande,lærling,pensum,fagstoff, ',
   },
   masthead: {
@@ -288,8 +285,7 @@ const messages = {
     shortcutsTooltip: 'Vis {count} artiklar i dette emnet',
     dialogTooltip: 'Kva er kjernestoff og tilleggsstoff?',
     dialogHeading: 'Kjernestoff og tilleggsstoff',
-    dialogText1:
-      'Når du lærer deg kjernestoffet, får du den kompetansen som blir beskrive i læreplanen for faget.',
+    dialogText1: 'Når du lærer deg kjernestoffet, får du den kompetansen som blir beskrive i læreplanen for faget.',
     dialogText2:
       'Tilleggsstoff er innhald du kan velje i tillegg til kjernestoffet. Gjennom dette kan du fordjupe deg i eit emne eller nærme deg emnet på ein annan måte.',
     showLess: 'Vis mindre',
@@ -585,8 +581,7 @@ const messages = {
     about: {
       heading: 'Om NDLA Film',
       more: 'Les meir om NDLA film',
-      text:
-        'NDLA film er ei nettbasert filmteneste for elevar og lærarar i vidaregåande skule. Her finn du spelefilmar, kortfilmar, dokumentarfilmar og TV-seriar.',
+      text: 'NDLA film er ei nettbasert filmteneste for elevar og lærarar i vidaregåande skule. Her finn du spelefilmar, kortfilmar, dokumentarfilmar og TV-seriar.',
     },
     search: {
       placeholder: 'Søk på filmnamn',
@@ -705,8 +700,7 @@ const messages = {
   fagfornyelse: {
     frontpage: {
       heading: 'Velkommen til sniktitt på Fagfornyelsen i NDLA',
-      text:
-        'Hausten 2020 og 2021 vil dei nye læreplanane tre i kraft. I NDLA har vi starta dette arbeidet allereie. Dei innhaldsansvarlege i NDLA lagar kvar dag nye supre læringsressursar som er tilrettelagte for dei nye planane. På denne sida kan du sjå dei allereie nå.',
+      text: 'Hausten 2020 og 2021 vil dei nye læreplanane tre i kraft. I NDLA har vi starta dette arbeidet allereie. Dei innhaldsansvarlege i NDLA lagar kvar dag nye supre læringsressursar som er tilrettelagte for dei nye planane. På denne sida kan du sjå dei allereie nå.',
       blogHeading: 'Vil du vite meir?',
     },
     badge: {
@@ -717,16 +711,14 @@ const messages = {
   },
   frontPageToolbox: {
     heading: 'Verktøykassa',
-    text:
-      'Har du lyst til å bli god til å presentere, eller vil du lære å studere smartare ved hjelp av riktig studieteknikk? Treng du råd om korleis du les mest mogleg effektivt til eksamen? I verktøykassa til NDLA finn du masse gode tips og råd!',
+    text: 'Har du lyst til å bli god til å presentere, eller vil du lære å studere smartare ved hjelp av riktig studieteknikk? Treng du råd om korleis du les mest mogleg effektivt til eksamen? I verktøykassa til NDLA finn du masse gode tips og råd!',
     linkTextStudents: 'Sjå alle tipsa for elever her',
     linkTextTeachers: 'Sjå alle tipsa for lærarar her',
     cursorText: 'Tips',
   },
   frontpageMultidisciplinarySubject: {
     heading: 'Tverrfaglege tema',
-    text:
-      'Dei tre tverrfaglege temaa i læreplanverket tek utgangspunkt i aktuelle samfunnsutfordringar som krev engasjement og innsats frå einskildmenneske og fellesskapet i lokalsamfunnet, nasjonalt og globalt.',
+    text: 'Dei tre tverrfaglege temaa i læreplanverket tek utgangspunkt i aktuelle samfunnsutfordringar som krev engasjement og innsats frå einskildmenneske og fellesskapet i lokalsamfunnet, nasjonalt og globalt.',
     linkText: 'Sjå casar for tverrfaglege tema',
     publicHealthTopic: 'Folkehelse og livsmeistring',
     democracyTopic: 'Demokrati og medborgarskap',

8f1972cce037c1b2e9497e62f8e88575afe44a1b
Gunnar Velle <gunnar.velle@gmail.com>
2022-03-02 11:16:11 +0100

    Yet another text change

--- packages/ndla-ui/src/locale/messages-en.ts
+++ packages/ndla-ui/src/locale/messages-en.ts
@@ -48,7 +48,7 @@ const messages = {
   subjectCategories: {
     [subjectCategories.ACTIVE_SUBJECTS]: 'Active',
     [subjectCategories.ARCHIVE_SUBJECTS]: 'Expired',
-    [subjectCategories.BETA_SUBJECTS]: 'Coming',
+    [subjectCategories.BETA_SUBJECTS]: 'Revised',
     [subjectCategories.COMMON_SUBJECTS]: 'Common core subj.',
     [subjectCategories.PROGRAMME_SUBJECTS]: 'Programme subj. SF',
     [subjectCategories.SPECIALIZED_SUBJECTS]: 'Programme subj. YF',
@@ -328,7 +328,7 @@ const messages = {
     newVersion:
       'This learning resource is not updated to the current curriculum. You can find an updated version here: ',
     frontPageBeta:
-      'Coming subjects are adapted to the new curriculum valid from autumn 2022. Beta versions of subjects are subjects we are still working on. We hope, however, that the learning resources available by now may come in useful already.',
+      'Revised subjects have been revised in accordance with the new curriculum that will be put into effect from August 2022. Beta versions of subjects are subjects we are still working on. We hope, however, that the learning resources available by now may come in useful already.',
     frontPageExpired:
       'Expired subjects are not being taught any longer, but it may still be possible to take exams in these subjects.',
     frontPageRevised:

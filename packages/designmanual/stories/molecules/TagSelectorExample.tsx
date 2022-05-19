1a2f39db60c9ae6c2c707e1c87b7e1f9d8cad4af
IvarBorthen <ivarborthen81@gmail.com>
2022-05-19 10:26:46 +0200

    Added tags to storybook

---
+++ packages/designmanual/stories/molecules/TagSelectorExample.tsx
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React from 'react';
+import { TagSelector } from '@ndla/ui';
+
+const TagSelectorExample = () => (
+  <TagSelector tags={[]} tagsSelected={[]} onTagsUpdate={() => {}} onCreateTag={() => {}} />
+);
+
+export default TagSelectorExample;

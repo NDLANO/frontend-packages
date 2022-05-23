ee4d5392069a14398299dd976208591e537eb0f1
Peter <peter_rolfsen@hotmail.com>
2022-05-23 13:34:12 +0200

    changed width to react on resize

--- packages/ndla-ui/src/MyNdla/ResourceDash/ResourcesView.tsx
+++ packages/ndla-ui/src/MyNdla/ResourceDash/ResourcesView.tsx
@@ -181,7 +181,7 @@ export const ResourcesView = ({ folders, resources }: ViewProps) => {
   const [layout, setLayout] = useState('list' as LayoutProps);
   const windowSize = useWindowSize(1000);
   useEffect(() => {
-    if (windowSize.innerWidth < 500) {
+    if (windowSize.innerWidth < 1000) {
       setLayout('list');
     }
   }, [windowSize]);

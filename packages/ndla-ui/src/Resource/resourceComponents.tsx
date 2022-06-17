a62e298fbe9c53537a7e9750ee663596fd9f6ae1
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-06-17 16:37:29 +0200

    Add missing copyright. Revert unintended changes

--- packages/ndla-ui/src/Resource/resourceComponents.tsx
+++ packages/ndla-ui/src/Resource/resourceComponents.tsx
@@ -1,6 +1,14 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React from 'react';
 import styled from '@emotion/styled';
 import { fonts, colors, spacing } from '@ndla/core';
-import React from 'react';
 
 export interface ResourceImageProps {
   alt: string;

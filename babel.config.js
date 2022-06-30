f9a89131321607a85d68564f9f66e9d5a239f72a
Henrik Liodden <henrik.liodden@knowit.no>
2022-06-30 13:42:52 +0200

    Revert babel changes

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,6 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-transform-runtime',
   ],
   env: {
     test: {

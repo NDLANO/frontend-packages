0cef89358908fb92266fbcd31cdfaede6c081cad
Henrik Liodden <henrik.liodden@knowit.no>
2022-07-11 13:18:35 +0200

    Remove unused helper function

--- packages/ndla-ui/src/TreeStructure/helperFunctions.ts
+++ packages/ndla-ui/src/TreeStructure/helperFunctions.ts
@@ -1,19 +1,5 @@
 import { Folder } from './TreeStructure.types';
 
-export const getPathOfFolder = (data: Folder[], findId: string): string[] => {
-  const paths = (folders: Folder[], path: string[]): string[] => {
-    for (const { id, subfolders } of folders) {
-      if (id === findId) {
-        return [...path, id];
-      } else if (subfolders?.length) {
-        return paths(subfolders, [...path, id]);
-      }
-    }
-    return [];
-  };
-  return paths(data, []);
-};
-
 export const flattenFolders = (folders: Folder[], openFolders?: string[]): Folder[] => {
   return folders.reduce((acc, { subfolders, id, ...rest }) => {
     if (!subfolders || (openFolders && !openFolders.includes(id))) {

a3641c7062af09ec07f7711771d4ae75d9e8c218
Jonas Carlsen <jonas.carlsen@knowit.no>
2022-08-09 09:32:23 +0200

    Update FileListExample.tsx

--- packages/designmanual/stories/molecules/FileListExample.tsx
+++ packages/designmanual/stories/molecules/FileListExample.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { initTooltips } from '@ndla/tooltip';
 import { FileList } from '@ndla/ui';
-import { Component } from 'react';
 
 class FileListExample extends Component {
   componentDidMount() {

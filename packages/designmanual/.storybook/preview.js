e6258918efc800a0827536ee0f903a14e02596bc
Peter <peter_rolfsen@hotmail.com>
2022-08-30 14:49:45 +0200

    la til muligheten for fullscreen

--- packages/designmanual/.storybook/preview.js
+++ packages/designmanual/.storybook/preview.js
@@ -3,6 +3,7 @@ import './designmanual.scss';
 export const parameters = {
   layout: 'fullscreen',
   design: {
+    allowFullscreen: true,
     type: 'figma',
     url: 'https://www.figma.com/proto/ElL5ylipht39lMZJEwz0k3/Min-NDLA-2.0-(Ikke-besluttet)?node-id=5418%3A33245&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=5418%3A33245&show-proto-sidebar=1',
   },

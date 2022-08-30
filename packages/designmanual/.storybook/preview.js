3592c82d68992060f8da779cc9da3127f84e0de7
Peter <peter_rolfsen@hotmail.com>
2022-08-30 11:05:40 +0200

    funksjonell prototype

--- packages/designmanual/.storybook/preview.js
+++ packages/designmanual/.storybook/preview.js
@@ -4,6 +4,6 @@ export const parameters = {
   layout: 'fullscreen',
   design: {
     type: 'figma',
-    url: 'https://www.figma.com/file/ElL5ylipht39lMZJEwz0k3/Min-NDLA-2.0-(Ikke-besluttet)?node-id=0%3A1',
+    url: 'https://www.figma.com/proto/ElL5ylipht39lMZJEwz0k3/Min-NDLA-2.0-(Ikke-besluttet)?node-id=5418%3A33245&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=5418%3A33245&show-proto-sidebar=1',
   },
 };

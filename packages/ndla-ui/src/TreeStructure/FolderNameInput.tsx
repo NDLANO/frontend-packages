1b4093a8d190dccae2aff5d025a292cec2d7e9b9
Henrik Liodden <henrik.liodden@knowit.no>
2022-09-12 11:34:34 +0200

    Add source link to custom border style

--- packages/ndla-ui/src/TreeStructure/FolderNameInput.tsx
+++ packages/ndla-ui/src/TreeStructure/FolderNameInput.tsx
@@ -16,6 +16,7 @@ import { IconButton } from '@ndla/button';
 import { Cross } from '@ndla/icons/action';
 import { Done } from '@ndla/icons/editor';
 
+// Source: https://kovart.github.io/dashed-border-generator/
 const borderStyle = `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='${encodeURIComponent(
   colors.brand.tertiary,
 )}' stroke-width='2' stroke-dasharray='8%2c8' stroke-dashoffset='4' stroke-linecap='square'/%3e%3c/svg%3e")`;

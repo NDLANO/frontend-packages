113a327c890c032362096174a4ffe3bc350ae61d
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-09-30 13:00:27 +0200

    Add copyright to modalStyles

--- packages/ndla-modal/src/modalStyles.ts
+++ packages/ndla-modal/src/modalStyles.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { css, SerializedStyles } from '@emotion/core';
 import { spacing } from '@ndla/core';
 import { ModalMargin, ModalSize } from './types';

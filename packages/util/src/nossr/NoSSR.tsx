14223e4d3a2ae3edabe34fc6819e4c5eb9ff9031
Jonas Natten <jonas.natten@knowit.no>
2022-10-03 12:45:09 +0200

    @ndla/util: Add helpers to not render content on server
    Adds `NoSSR` component and HoC `withNoSSR` to avoid rendering parts of
    our tree on the server. This will be useful to avoid caching private
    information in our reverse-proxy/gateway.

---
+++ packages/util/src/nossr/NoSSR.tsx
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import React, { ReactNode } from 'react';
+import { useEffect, useState } from 'react';
+
+interface Props {
+  children: ReactNode;
+  fallback: ReactNode | null;
+}
+
+/**
+ * A component that makes children not run on the server
+ * Useful for not rendering private stuff on the server which will then be cached.
+ */
+const NoSSR = ({ children, fallback }: Props) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{fallback}</>;
+  }
+
+  return <>{children}</>;
+};
+
+export default NoSSR;

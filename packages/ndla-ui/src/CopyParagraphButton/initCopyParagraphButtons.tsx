fdc86e7e5dd8904e51f623cf840a5f81cb79612e
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-10-04 14:14:21 +0200

    Revert CopyParagraphButtons hydration change

--- packages/ndla-ui/src/CopyParagraphButton/initCopyParagraphButtons.tsx
+++ packages/ndla-ui/src/CopyParagraphButton/initCopyParagraphButtons.tsx
@@ -19,7 +19,7 @@ const forEachElement = (selector: string, callback: Function) => {
 const initCopyParagraphButtons = () => {
   forEachElement('[data-header-copy-container]', (el: HTMLElement) => {
     const title = el.getAttribute('data-title');
-    ReactDOM.render(<CopyParagraphButton title={title} content={title} hydrate={true} />, el);
+    ReactDOM.hydrate(<CopyParagraphButton title={title} content={title} hydrate={true} />, el);
   });
 };
 

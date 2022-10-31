3820c5cb917870a2d8c245affa8ec7bd06c3be8e
Henrik Liodden <henrik.liodden@knowit.no>
2022-10-31 11:22:44 +0100

    Add copyright to files. Change default button example name to default

--- packages/designmanual/.storybook/main.js
+++ packages/designmanual/.storybook/main.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 module.exports = {
   stories: ['../stories/index.ts', '../../**/*.stories.@(tsx|mdx)'],
   addons: [

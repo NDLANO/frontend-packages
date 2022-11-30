cb045363ffd984cf78b8429373f58b33c8b71250
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-11-30 14:52:42 +0100

    Add Heading 4 icon

--- packages/ndla-icons/src/editor/index.ts
+++ packages/ndla-icons/src/editor/index.ts
@@ -40,6 +40,7 @@ export { default as H5PBold } from './H5PBold';
 export { default as Heading1 } from './Heading1';
 export { default as Heading2 } from './Heading2';
 export { default as Heading3 } from './Heading3';
+export { default as Heading4 } from './Heading4';
 export { default as ImageNormal } from './ImageNormal';
 export { default as ImageSmall } from './ImageSmall';
 export { default as ImageXsmall } from './ImageXsmall';

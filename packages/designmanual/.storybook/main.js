41fdef79d35388d436031038951b36c6fef92d53
arjajs <arja.sivapiragasam@bouvet.no>
2022-12-05 12:13:42 +0100

    formatting

--- packages/designmanual/.storybook/main.js
+++ packages/designmanual/.storybook/main.js
@@ -64,7 +64,7 @@ module.exports = {
         '@babel/plugin-proposal-optional-chaining',
       ],
     };
-  },  
+  },
   /* For at "Show code" skal legge seg bakerst slik at elementer som drop-down ikke blir skjult bak den */
   previewHead: (head) => `
   <style>
@@ -72,5 +72,5 @@ module.exports = {
       z-index: 0 !important;
     }
   </style>
-  ${head}`
+  ${head}`,
 };

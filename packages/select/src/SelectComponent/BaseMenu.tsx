1fc1b1d14d3c77894a8ff202dfa47bd471704590
Katrine Wist <katrinew0702@gmail.com>
2022-12-16 19:13:56 +0100

    Menu styling overflow hidden

--- packages/select/src/SelectComponent/BaseMenu.tsx
+++ packages/select/src/SelectComponent/BaseMenu.tsx
@@ -8,6 +8,7 @@ const StyledBaseMenu = styled.div<{ base: SerializedStyles }>`
   ${(props) => props.base};
   width: auto;
   min-width: 100%;
+  overflow: hidden;
 `;
 
 const BaseMenu = <T extends boolean>({ ...props }: MenuProps<Option, T>) => {

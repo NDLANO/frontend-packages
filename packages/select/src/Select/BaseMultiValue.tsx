cd510f182a2494dded6e78abbac9e4c4a8fc507e
Henrik Liodden <henrik.liodden@knowit.no>
2022-12-20 14:28:41 +0100

    Remove casting of Option type in baseMultiValue

--- packages/select/src/Select/BaseMultiValue.tsx
+++ packages/select/src/Select/BaseMultiValue.tsx
@@ -5,19 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
+import isArray from 'lodash/isArray';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { MultiValueProps } from 'react-select';
 import { Option } from './types';
 
-const BaseMultiValue = <T extends boolean>({ ...props }: MultiValueProps<Option, T>) => {
+const BaseMultiValue = <T extends boolean>({ selectProps: { value }, data, children }: MultiValueProps<Option, T>) => {
   const { t } = useTranslation();
 
-  const ValueCasted: Option[] = (props.selectProps.value as Option[]) ?? [];
-  const count = ValueCasted.length;
-  const isLastItem = props.data === ValueCasted[count - 1];
+  const count = isArray(value) ? value.length : 0;
+  const isLastItem = isArray(value) && data === value[count - 1];
 
-  if (count === 1) return <span>{props.children}</span>;
+  if (count === 1) return <span>{children}</span>;
   if (isLastItem) return <span>{t('dropdown.selected', { count })}</span>;
   return null;
 };

431d14a4a7298e6ecc66b81cc67b5501a1fc2295
Katrine Wist <katrinew0702@gmail.com>
2022-12-20 12:49:38 +0100

    Remove css object defined inside component

--- packages/select/src/Select/BaseMenu.tsx
+++ packages/select/src/Select/BaseMenu.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { css, SerializedStyles } from '@emotion/react';
+import { colors } from '@ndla/core';
 import { MenuPropsType } from './types';
 
-const StyledBaseMenu = styled.div<{ base: SerializedStyles }>`
-  ${(props) => props.base};
+const StyledBaseMenu = styled.div`
+  position: absolute;
+  top: 100%;
   width: auto;
   min-width: 100%;
   overflow: hidden;
+  background-color: ${colors.white};
+  border: 1px solid ${colors.brand.greyLighter};
+  border-radius: 4px;
+  margin-top: 4px;
 `;
 
 const BaseMenu = <T extends boolean>({ ...props }: MenuPropsType<T>) => {
-  const baseStyling = css`
-    ${props.getStyles('menu', props)}
-  `;
   return (
-    <StyledBaseMenu ref={props.innerRef} base={baseStyling} {...props.innerProps}>
+    <StyledBaseMenu ref={props.innerRef} {...props.innerProps}>
       {props.children}
     </StyledBaseMenu>
   );

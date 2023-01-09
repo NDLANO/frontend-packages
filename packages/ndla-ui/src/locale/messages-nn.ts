181d3d14cca7011143c05071434c4561463d7d75
Gunnar Velle <gunnar.velle@gmail.com>
2023-01-09 04:12:41 -0500

    Update packages/ndla-ui/src/locale/messages-nn.ts

--- packages/ndla-ui/src/locale/messages-nn.ts
+++ packages/ndla-ui/src/locale/messages-nn.ts
@@ -268,7 +268,7 @@ const messages = {
     addConfirmed: '{{subject}} er lagt til som favorittfag',
     subjectFavoritePitch: 'Ønsker du å favorittmerke dette faget?',
     subjectFavoriteGuide:
-      'For å favorittmerke eit fag må du logge inn på Min NDLA. Du finner faget øverst på denne sida etter at du har logga inn.',
+      'For å favorittmerke eit fag må du logge inn på Min NDLA. Du finn faget øverst på denne sida etter at du har logga inn.',
     shows: 'Viser',
   },
   topicPage: {

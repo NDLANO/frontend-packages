a97f728824a9a0bd3f20210aaa413cd14d1ede91
Henrik Liodden <henrik.liodden@knowit.no>
2023-01-13 09:08:14 +0100

    Rewrite intersection observer to reinitialize on prop change

--- packages/hooks/src/useIntersect.ts
+++ packages/hooks/src/useIntersect.ts
@@ -1,7 +1,6 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback, useEffect, RefObject } from 'react';
 import { isIE, browserVersion } from 'react-device-detect';
 
-const DEFAULT_ROOT = null;
 const DEFAULT_ROOT_MARGIN = '0px';
 const DEFAULT_THRESHOLD = [0];
 
@@ -19,11 +18,17 @@ const IntersectionObserverBrowserSupport = () =>
 // For more info:
 // https://developers.google.com/web/updates/2016/04/intersectionobserver
 // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+interface Props extends IntersectionObserverInit {
+  target?: HTMLElement | Document | null;
+}
+
 export function useIntersectionObserver({
-  root = DEFAULT_ROOT,
+  root = null,
+  target = null,
   rootMargin = DEFAULT_ROOT_MARGIN,
   threshold = DEFAULT_THRESHOLD,
-}: IntersectionObserverInit = {}): IntersectionObserverHookResult {
+}: Props = {}): IntersectionObserverHookResult {
   const isIE11 = isIE && parseInt(browserVersion) < 12;
   const observerRef = useRef<IntersectionObserver | null>(null);
   const [entry, setEntry] = useState<IntersectionObserverEntry>();
@@ -37,29 +42,29 @@ export function useIntersectionObserver({
     };
   }, []);
 
+  const getObserver = useCallback(() => {
+    if (observerRef.current) {
+      observerRef.current.disconnect();
+    }
+    observerRef.current = new IntersectionObserver(
+      ([entry]) => {
+        setEntry(entry);
+      },
+      { root, rootMargin, threshold },
+    );
+    return observerRef.current;
+  }, [root, rootMargin, threshold]);
+
   const refCallback = useCallback(
     (node: IntersectionObserverHookRefCallbackNode) => {
-      function getObserver() {
-        // If there is no observer, then create it.
-        // So, we only create it only once.
-        if (!observerRef.current) {
-          observerRef.current = new IntersectionObserver(
-            ([entry]) => {
-              setEntry(entry);
-            },
-            { root, rootMargin, threshold },
-          );
-        }
-        return observerRef.current;
-      }
       const observer = getObserver();
-      observer.disconnect();
       if (node) {
         observer.observe(node);
       }
     },
-    [root, rootMargin, threshold],
+    [getObserver],
   );
+
   if (isIE11 || !IntersectionObserverBrowserSupport()) {
     return [() => {}, { entry }];
   }

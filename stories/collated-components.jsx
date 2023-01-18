14e57d8f228acf3207df81b4ea9de56db44a79a2
Gunnar Velle <gunnar.velle@gmail.com>
2023-01-18 08:51:32 +0100

    Fix storybook example

--- stories/collated-components.jsx
+++ stories/collated-components.jsx
@@ -33,12 +33,7 @@ import Tabs, { TabsControlled } from './molecules/tabs';
 import Resources from './molecules/resources';
 import LicenseBox from './article/LicenseBox';
 import NotionExample from './organisms/NotionExample';
-import {
-  BreadcrumbDefault,
-  BreadcrumbWithHeader,
-  BreadcrumbWithHome,
-  BreadcrumbWithAction,
-} from './molecules/breadcrumbs';
+import { BreadcrumbDefault, BreadcrumbWithHeader, BreadcrumbWithHome } from './molecules/breadcrumbs';
 import RelatedArticleListExample, {
   RelatedArticleExerciseList,
   RelatedArticleMixedList,
@@ -227,8 +222,6 @@ storiesOf('Sammensatte moduler', module)
       <BreadcrumbWithHeader />
       <h2 className="u-heading">Brødsmulesti med forskjellige ikoner</h2>
       <BreadcrumbWithHome />
-      <h2 className="u-heading">Brødsmulesti med Menuknapp</h2>
-      <BreadcrumbWithAction />
     </Center>
   ))
   .add('Begrepsforklaring', () => (

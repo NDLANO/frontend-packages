00afcc814aa936caafbd6b56234a6c90fc0d053d
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-01-18 15:23:11 +0100

    Copyright on LanguageSelector

--- packages/ndla-ui/src/LanguageSelector/LanguageSelector.tsx
+++ packages/ndla-ui/src/LanguageSelector/LanguageSelector.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from 'react';
 import { Root, Trigger, Item, Content, Portal, Arrow } from '@radix-ui/react-dropdown-menu';
 import { ButtonV2 } from '@ndla/button';

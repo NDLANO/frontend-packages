def9ed863878a1799ef77dd89814686347a9506e
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-01-20 15:21:59 +0100

    Re-add copyright

--- packages/switch/src/Switch.tsx
+++ packages/switch/src/Switch.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2019-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from 'react';
 import { Root, Thumb, SwitchProps } from '@radix-ui/react-switch';
 import styled from '@emotion/styled';

b995a0dca1ebec1352eb3edb1bc19b0808b46c15
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-02-01 16:25:03 +0100

    Add @ndla/types-embed package

---
+++ packages/types-embed/src/fileTypes.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { MetaData } from '.';
+
+export interface FileEmbedData {
+  resource: 'file';
+  path: string;
+  title: string;
+  type: string;
+  url: string;
+}
+
+export interface FileData {
+  exists?: boolean;
+}
+
+export type FileMetaData = MetaData<FileEmbedData, FileData>;

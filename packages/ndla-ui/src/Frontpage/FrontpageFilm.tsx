267ac4e05e947f3a2c4dc54f7740b5793bc122dd
John-H-Christiansen <john.christiansen@bouvet.no>
2023-04-11 15:36:23 +0200

    started on new design, added basic blocks

--- packages/ndla-ui/src/Frontpage/FrontpageFilm.tsx
+++ packages/ndla-ui/src/Frontpage/FrontpageFilm.tsx
@@ -5,92 +5,44 @@ import { Forward } from '@ndla/icons/common';
 import { useTranslation } from 'react-i18next';
 import { spacing, spacingUnit, colors, breakpoints, fonts, mq } from '@ndla/core';
 import SectionHeading from '../SectionHeading';
+import NdlaFilmIllustrasjon from '../../../../images/ndla-film/illustrasjon_film.svg';
+import NdlaFilmIllustrasjonRull from '../../../../images/ndla-film/illustrasjon_filmrull.svg';
 
-const StyledSection = styled.section`
-  margin-top: ${spacing.large};
-  margin-bottom: ${spacing.large};
-`;
-
-type StyledImageProps = {
-  imageUrl: string;
-};
-
-const StyledImage = styled.div<StyledImageProps>`
-  background: ${colors.ndlaFilm.filmColorDark};
-  background-repeat: no-repeat;
-  background-position: ${spacing.small} center;
-  background-size: 110px;
-
+const StyledFilmContainer = styled.div`
   display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  min-height: 100px;
-  padding: ${spacingUnit}px ${spacing.medium} ${spacingUnit}px ${spacingUnit}px;
-
-  ${(props: StyledImageProps) =>
-    props.imageUrl &&
-    css`
-      background-image: url(${props.imageUrl});
-    `}
-
-  ${mq.range({ from: breakpoints.mobileWide })} {
-    background-size: contain;
-    min-height: 120px;
-    background-position: ${spacing.medium} center;
-  }
-
-  ${mq.range({ from: breakpoints.desktop })} {
-    min-height: 180px;
-    background-position: 72px center;
-  }
 `;
 
-type StyledTextProps = {
-  narrow?: boolean;
-};
+const StyledLeftIllustration = styled.img``;
 
-const StyledText = styled.span<StyledTextProps>`
-  color: #fff;
-  ${fonts.sizes('14px', '26px')};
-  width: 50%;
+const StyledRightIllustration = styled.img``;
 
-  ${mq.range({ from: breakpoints.tablet })} {
-    ${fonts.sizes('18px', '26px')};
-    padding-right: ${spacingUnit}px;
-    padding-left: 0;
-    width: 66.6%;
-  }
-  ${mq.range({ from: breakpoints.tabletWide })} {
-    display: ${(props: StyledTextProps) => (props.narrow ? 'none' : 'flex')};
-  }
-  ${mq.range({ until: breakpoints.tabletWide })} {
-    display: ${(props: StyledTextProps) => (!props.narrow ? 'none' : 'flex')};
-  }
-`;
+const StyledTextContainer = styled.div``;
+
+const StyledHeading = styled.h2``;
+
+const StyledText = styled.p``;
 
 type Props = {
   url: string;
-  imageUrl: string;
 };
 
-const FrontpageFilm = ({ url, imageUrl }: Props) => {
+const FrontpageFilm = ({ url }: Props) => {
   const { t } = useTranslation();
   return (
-    <StyledSection>
-      <SectionHeading headingLevel="h2" large>
-        {t('welcomePage.film.header')}
-      </SectionHeading>
-      <StyledImage imageUrl={imageUrl}>
+    <StyledFilmContainer>
+      <StyledLeftIllustration src={NdlaFilmIllustrasjon}></StyledLeftIllustration>
+      <StyledTextContainer>
+        <StyledHeading>{t('welcomePage.film.header')}</StyledHeading>
         <StyledText>{t('welcomePage.film.text')}</StyledText>
-        <StyledText narrow>{t('welcomePage.film.textShort')}</StyledText>
-      </StyledImage>
-      <div className="o-text-link__wrapper o-text-link__wrapper--right">
-        <SafeLink className="o-text-link" to={url}>
-          {t('welcomePage.film.linkLabel')}
-          <Forward />
-        </SafeLink>
-      </div>
-    </StyledSection>
+        <div>
+          <SafeLink className="o-text-link" to={url}>
+            {t('welcomePage.film.linkLabel')}
+            <Forward />
+          </SafeLink>
+        </div>
+      </StyledTextContainer>
+      <StyledRightIllustration src={NdlaFilmIllustrasjonRull}></StyledRightIllustration>
+    </StyledFilmContainer>
   );
 };
 

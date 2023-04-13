1bc52e3e79664eb992db363c3b30de4e75c46e80
John-H-Christiansen <john.christiansen@bouvet.no>
2023-04-13 13:00:37 +0200

    mobile view and multi image uppdated

--- packages/ndla-ui/src/Frontpage/FrontpageFilm.tsx
+++ packages/ndla-ui/src/Frontpage/FrontpageFilm.tsx
@@ -17,20 +17,37 @@ const TwoImages: boolean = true;
 const StyledFilmContainer = styled.div`
   display: flex;
   background-color: lightblue;
+  margin-top: 48px;
   ${mq.range({ until: breakpoints.tabletWide })} {
     flex-direction: column;
     max-width: 390px;
+    margin: 48px auto;
   }
+`;
+
+const StyledHeading = styled.h2`
+  margin: 48px 0 24px 20px;
+`;
+
+const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${!TwoImages &&
+  css`
+    flex-direction: column-reverse;
+  `}
 `;
 
 const StyledLeftIllustration = styled.img`
   margin-right: 5px;
   ${mq.range({ until: breakpoints.tabletWide })} {
     width: 160px;
+    margin-bottom: 24px;
   }
   ${!TwoImages &&
   css`
-    display: none;
+    width: 290px;
+    margin: 24px auto;
   `}
 `;
 
@@ -38,57 +55,54 @@ const StyledRightIllustration = styled.img`
   ${mq.range({ until: breakpoints.tabletWide })} {
     width: 200px;
     align-self: flex-end;
-    margin: -72px 0 48px 0;
   }
-  ${!TwoImages && css``}
+  ${!TwoImages &&
+  css`
+    display: none;
+  `}
 `;
 
-const StyledTextContainer = styled.div`
-  margin-bottom: 24px;
+const StyledText = styled.p`
+  font-family: Source Seriff Pro;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 29px;
+  margin: 0;
   ${mq.range({ until: breakpoints.tabletWide })} {
     padding: 0 20px;
   }
 `;
 
-const StyledDesktopHeading = styled.h2`
-  margin: 32px 0 0 0;
+const StyledLinkContainer = styled.div`
   ${mq.range({ until: breakpoints.tabletWide })} {
-    display: none;
+    position: relative;
+    bottom: 120px;
+    padding-left: 20px;
+    ${!TwoImages &&
+    css`
+      position: static;
+      justify-self: flex-end;
+      margin-bottom: 48px;
+    `}
   }
-`;
-
-const StyledMobileHeading = styled.h2`
-  margin: 48px 0 24px 20px;
-  ${mq.range({ from: breakpoints.tabletWide })} {
-    display: none;
-  }
-`;
-
-const StyledText = styled.p`
-  margin: 24px 0px;
-  font-family: Source Seriff Pro;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 29px;
 `;
 
 const FrontpageFilm = ({ url }: Props) => {
   const { t } = useTranslation();
   return (
     <StyledFilmContainer>
-      <StyledMobileHeading>{t('welcomePage.film.header')}</StyledMobileHeading>
-      <StyledLeftIllustration src={NdlaFilmIllustrasjon}></StyledLeftIllustration>
-      <StyledTextContainer>
-        <StyledDesktopHeading>{t('welcomePage.film.header')}</StyledDesktopHeading>
+      <StyledHeading>{t('welcomePage.film.header')}</StyledHeading>
+      <StyledContentContainer>
+        <StyledLeftIllustration src={NdlaFilmIllustrasjon}></StyledLeftIllustration>
         <StyledText>{t('welcomePage.film.text')}</StyledText>
-        <div>
-          <SafeLink className="o-text-link" to={url}>
-            {t('welcomePage.film.linkLabel')}
-            <Forward />
-          </SafeLink>
-        </div>
-      </StyledTextContainer>
-      <StyledRightIllustration src={NdlaFilmIllustrasjonRull}></StyledRightIllustration>
+        <StyledRightIllustration src={NdlaFilmIllustrasjonRull}></StyledRightIllustration>
+      </StyledContentContainer>
+      <StyledLinkContainer>
+        <SafeLink className="o-text-link" to={url}>
+          {t('welcomePage.film.linkLabel')}
+          <Forward />
+        </SafeLink>
+      </StyledLinkContainer>
     </StyledFilmContainer>
   );
 };

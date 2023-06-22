565724f0b8f7c38f56ec3b29ca6bac6513a5dbff
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-06-22 09:34:25 +0200

    Make text and embeds take up same amount of space

--- packages/ndla-ui/src/Embed/ImageEmbed.tsx
+++ packages/ndla-ui/src/Embed/ImageEmbed.tsx
@@ -123,7 +123,7 @@ const ImageEmbed = ({ embed, previewAlt, heartButton: HeartButton }: Props) => {
     <Figure
       id={figureId}
       type={imageSizes ? undefined : figureType}
-      className={imageSizes ? 'c-figure--right expanded' : ''}
+      className={imageSizes ? `c-figure--${embedData.align} expanded` : ''}
     >
       <ImageWrapper src={embedData.pageUrl || data.image.imageUrl} crop={crop} size={embedData.size}>
         <Image

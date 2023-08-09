c298cc504725e5970222631f5976707b88fb15c5
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-08-09 16:13:28 +0200

    Hotfix: Wrap parallax children in a div to ensure parallax effect

--- packages/ndla-ui/src/Grid/Grid.tsx
+++ packages/ndla-ui/src/Grid/Grid.tsx
@@ -8,7 +8,7 @@
 
 import styled from '@emotion/styled';
 import { breakpoints, colors, misc, mq, spacing } from '@ndla/core';
-import { Children, ReactNode } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
 
 export interface GridProps {
   columns: '2' | '4' | '2x2';
@@ -87,12 +87,18 @@ const Grid = ({ columns, border, children, background, size, ...rest }: GridProp
   );
 };
 
-export const GridParallaxItem = styled.div`
+const StyledGridParallaxItem = styled.div`
   position: relative;
   > div {
     top: 0px;
     position: sticky;
   }
 `;
+
+export const GridParallaxItem = ({ children, ...rest }: HTMLAttributes<HTMLDivElement>) => (
+  <StyledGridParallaxItem {...rest}>
+    <div>{children}</div>
+  </StyledGridParallaxItem>
+);
 
 export default Grid;

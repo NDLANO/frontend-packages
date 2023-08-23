d348116892dd687de645557e5e7d6f8c344886b9
MaPoKen <larssoerlie96@gmail.com>
2023-08-23 08:59:52 +0200

    refactored table from scss to styled-components

--- packages/ndla-ui/src/Table/Table.tsx
+++ packages/ndla-ui/src/Table/Table.tsx
@@ -6,15 +6,13 @@
  *
  */
 
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { colors, spacing } from '@ndla/core';
+import { breakpoints, colors, fonts, mq, spacing } from '@ndla/core';
 import throttle from 'lodash/throttle';
 import { ReactNode, UIEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import BEMHelper from 'react-bem-helper';
 
 type ScrollPosition = 'start' | 'end' | 'center';
-
-const classes = BEMHelper('c-table');
 
 const MARGIN = 5;
 
@@ -26,17 +24,17 @@ interface Props {
   };
 }
 
-interface StyledProps {
-  show: boolean;
-}
-
-const ScrollBorder = styled.div<StyledProps>`
+const ScrollBorder = styled.div`
   position: absolute;
   top: 0;
   height: calc(100% - ${spacing.mediumlarge});
   width: 3px;
   background: ${colors.background.dark};
-  display: ${({ show }) => (show ? 'block' : 'none')};
+  display: none;
+
+  &[data-block='true'] {
+    display: block;
+  }
 `;
 
 const RightScrollBorder = styled(ScrollBorder)`
@@ -45,6 +43,158 @@ const RightScrollBorder = styled(ScrollBorder)`
 
 const LeftScrollBorder = styled(ScrollBorder)`
   left: 0;
+`;
+
+export const TableStyling = css`
+  &::-webkit-scrollbar {
+    height: ${spacing.xsmall};
+  }
+  &::-webkit-scrollbar-track-piece {
+    background: ${colors.brand.lighter};
+    border-radius: ${spacing.xsmall};
+  }
+  &::-webkit-scrollbar-thumb {
+    background: ${colors.brand.dark};
+    border-radius: ${spacing.xsmall};
+  }
+  display: inline-block;
+
+  font-size: 85%;
+  max-width: 100%;
+
+  padding-left: ${spacing.xxsmall};
+  padding-right: ${spacing.xxsmall};
+
+  vertical-align: top;
+  table-layout: fixed;
+  overflow-x: auto;
+
+  // optional - enable iOS momentum scrolling
+  -webkit-overflow-scrolling: touch;
+
+  // scrolling shadows on left/right
+  &:after,
+  &:before {
+    content: '';
+    display: table;
+    clear: both;
+  }
+
+  > caption {
+    background-color: transparent;
+    font-weight: ${fonts.weight.bold};
+    font-family: ${fonts.sans};
+    ${fonts.sizes('16px', '20px')}
+    text-align: left;
+    text-transform: uppercase;
+    margin-bottom: ${spacing.small};
+  }
+
+  thead {
+    overflow: hidden;
+  }
+
+  thead tr td,
+  th {
+    font-weight: ${fonts.weight.bold};
+    border-bottom: 3px solid ${colors.brand.tertiary};
+    font-family: ${fonts.sans};
+    vertical-align: text-top;
+
+    ${fonts.sizes('16px', '22px')};
+    ${mq.range({ from: breakpoints.tablet })} {
+      ${fonts.sizes('16px', '30px')};
+    }
+  }
+
+  tbody th {
+    border-bottom: 0;
+    border-right: 3px solid ${colors.brand.tertiary};
+    padding: ${spacing.xsmall};
+  }
+
+  thead tr:nth-child(2) th {
+    border: 1px solid ${colors.brand.lighter};
+    text-transform: none;
+
+    ${fonts.sizes('14px', '18px')};
+    ${mq.range({ from: breakpoints.tablet })} {
+      ${fonts.sizes('15px', '26px')};
+    }
+
+    font-weight: ${fonts.weight.semibold};
+    height: 40px;
+    background-color: ${colors.brand.lighter};
+    padding: ${spacing.xxsmall} ${spacing.normal} ${spacing.xxsmall} ${spacing.xsmall};
+
+    &:empty {
+      background-color: transparent;
+    }
+  }
+
+  td {
+    border: 1px solid ${colors.brand.lighter};
+    vertical-align: top;
+
+    ${fonts.sizes('14px', '22px')};
+    ${mq.range({ from: breakpoints.tablet })} {
+      ${fonts.sizes('15px', '30px')};
+    }
+
+    ol,
+    ul {
+      font-size: unset !important;
+    }
+
+    p {
+      line-height: 1.6em;
+    }
+
+    p:last-child {
+      margin: 0;
+    }
+
+    img {
+      max-width: 100%;
+      min-width: 120px;
+    }
+  }
+
+  td,
+  th {
+    display: table-cell;
+    padding: ${spacing.xsmall} ${spacing.small};
+
+    p {
+      margin: 0;
+    }
+  }
+
+  // Remove excess spacing on headings inside table
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin-top: 0;
+  }
+
+  ul {
+    margin: 0 0 0 ${spacing.nsmall};
+  }
+
+  .c-figure {
+    margin: 0;
+    padding: 0;
+    width: 100% !important;
+    left: 0 !important;
+  }
+`;
+
+const TableWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: ${spacing.small} 0;
 `;
 
 const Table = ({ children, id, ...rest }: Props) => {
@@ -91,18 +241,15 @@ const Table = ({ children, id, ...rest }: Props) => {
   }, [checkScrollPosition]);
 
   return (
-    <div {...classes('wrapper')}>
-      <div {...classes('content')}>
-        <LeftScrollBorder show={scrollPosition === 'end' || scrollPosition === 'center'} {...classes('left-shadow')} />
-        <table ref={tableRef} id={id} onScroll={onScroll} {...classes({ extra: ['o-table'] })} {...rest}>
+    <TableWrapper>
+      <div>
+        <LeftScrollBorder data-block={scrollPosition === 'end' || scrollPosition === 'center'} />
+        <table ref={tableRef} id={id} onScroll={onScroll} css={TableStyling} {...rest}>
           {children}
         </table>
-        <RightScrollBorder
-          show={scrollPosition === 'start' || scrollPosition === 'center'}
-          {...classes('right-shadow')}
-        />
+        <RightScrollBorder data-block={scrollPosition === 'start' || scrollPosition === 'center'} />
       </div>
-    </div>
+    </TableWrapper>
   );
 };
 

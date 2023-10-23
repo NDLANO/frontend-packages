fbe0779f9c1abf3975253f5b215b38b6320c601a
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-10-23 13:20:51 +0200

    Move list story next to OrderedList and UnorderedList

---
+++ packages/ndla-ui/src/List/UnorderedList.stories.tsx
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { Meta, StoryFn } from '@storybook/react';
+import UnOrderedList from './UnOrderedList';
+
+export default {
+  title: 'Base styles/UnorderedList',
+  tags: ['autodocs'],
+  component: UnOrderedList,
+} as Meta<typeof UnOrderedList>;
+
+export const Default: StoryFn = () => (
+  <UnOrderedList>
+    <li>Listepunkt 1</li>
+    <li>Listepunkt 2</li>
+    <li>
+      Listepunkt 3
+      <UnOrderedList>
+        <li>Listepunkt 1</li>
+        <li>Listepunkt 2</li>
+        <li>
+          Listepunkt 3
+          <UnOrderedList>
+            <li>Listepunkt 1</li>
+            <li>Listepunkt 2</li>
+            <li>
+              Listepunkt 3
+              <UnOrderedList>
+                <li>Listepunkt 1</li>
+                <li>Listepunkt 2</li>
+                <li>Listepunkt 3</li>
+              </UnOrderedList>
+            </li>
+          </UnOrderedList>
+        </li>
+      </UnOrderedList>
+    </li>
+    <li>Listepunkt 4</li>
+  </UnOrderedList>
+);

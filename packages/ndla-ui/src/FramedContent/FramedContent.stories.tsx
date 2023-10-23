6d1172b97fd39a20df1961bf13513dd9112e645c
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-10-23 08:42:40 +0200

    Add license

--- packages/ndla-ui/src/FramedContent/FramedContent.stories.tsx
+++ packages/ndla-ui/src/FramedContent/FramedContent.stories.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { Meta, StoryObj } from '@storybook/react';
 import { defaultParameters } from '../../../../stories/defaults';
 import FramedContent from './FramedContent';

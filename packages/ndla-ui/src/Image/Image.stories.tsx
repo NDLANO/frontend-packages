37cc66539eca9e75fa85eefa41b2bf43a89cecc7
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-10-24 10:28:54 +0200

    Fold in lazy load story

--- packages/ndla-ui/src/Image/Image.stories.tsx
+++ packages/ndla-ui/src/Image/Image.stories.tsx
@@ -46,3 +46,18 @@ export const FocalPoint: StoryObj<typeof Image> = {
     sizes: '(min-width: 320px) 320px',
   },
 };
+
+export const LazyLoad: StoryObj<typeof Image> = {
+  args: {
+    alt: 'Lyspære',
+    src: 'https://api.staging.ndla.no/image-api/raw/Ide.jpg',
+    lazyLoad: true,
+    lazyLoadSrc: `data:image/svg+xml;utf8,
+    <svg xmlns="http://www.w3.org/2000/svg" height="400" width="100%">
+      <rect x="0" y="0" width="100%" height="100%" style="fill:#EFF0F2;">
+      <animate attributeType="XML" attributeName="fill" values="#EFF0F2;#E8E3E3;#EFF0F2;" dur="3s" repeatCount="indefinite"/>
+      </rect>
+      </svg>
+      `,
+  },
+};

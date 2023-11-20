09e45953ac34be38fecd687066706e3576b4bde3
Katrine Wist <katrinew0702@gmail.com>
2023-11-20 20:38:12 +0100

    Add subdirectory icon

--- packages/ndla-icons/src/common/index.ts
+++ packages/ndla-icons/src/common/index.ts
@@ -97,6 +97,7 @@ export { default as Search } from './Search';
 export { default as Share } from './Share';
 export { default as ShareArrow } from './ShareArrow';
 export { default as ShortText } from './ShortText';
+export { default as SubdirectoryArrow } from './SubdirectoryArrow';
 export { default as Tag } from './Tag';
 export { default as TagOutlined } from './TagOutlined';
 export { default as ThreeSixty } from './ThreeSixty';

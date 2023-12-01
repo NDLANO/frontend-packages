770eda3f7021c179559d9fef65fe76176a312814
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-12-01 09:33:30 +0100

    Hotfix: Export useFormControl

--- packages/ndla-forms/src/index.ts
+++ packages/ndla-forms/src/index.ts
@@ -22,7 +22,7 @@ export { FormPill, FormPills } from './FormPill';
 export { default as CheckboxItem } from './CheckboxItem';
 export { default as UploadDropZone } from './UploadDropZone';
 export { Label } from './Label';
-export { FormControl } from './FormControl';
+export { FormControl, useFormControl } from './FormControl';
 export { Input as InputV3, InputContainer, TextArea as TextAreaV3 } from './InputV3';
 export { FieldErrorMessage } from './FieldErrorMessage';
 export { FieldHelper } from './FieldHelper';

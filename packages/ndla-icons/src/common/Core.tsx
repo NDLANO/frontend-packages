fb409d76cc1dc96fa4a02d94663fa728aa42284a
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-12-18 14:15:51 +0100

    Remove icon svg styling inconsistencies

--- packages/ndla-icons/src/common/Core.tsx
+++ packages/ndla-icons/src/common/Core.tsx
@@ -11,7 +11,7 @@ const Core = (props: Props) => (
   <Icon data-license="CC-BY 4.0" data-source="Keyteq" viewBox="0 0 23 23" {...props}>
     <g fill="none" fillRule="evenodd">
       <circle cx={11.5} cy={11.5} r={10.5} fill="currentColor" fillRule="nonzero" stroke="currentColor" />
-      <path fill="#FFF" d="M10.8 8.2v2.6L13.6 8 15 9.4l-2.5 2.3 2.7 3.5-1.6 1.1-2.8-3.7v3.5H9V8.2z" />
+      <path fill="#fff" d="M10.8 8.2v2.6L13.6 8 15 9.4l-2.5 2.3 2.7 3.5-1.6 1.1-2.8-3.7v3.5H9V8.2z" />
     </g>
   </Icon>
 );

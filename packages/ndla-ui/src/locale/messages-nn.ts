c187cf473bbaf603300618d4c97919cf6d2e0cc3
Gaute Rauboti <gaute.rauboti@knowit.no>
2024-01-04 12:36:02 +0100

    Translations for ndla frontend navs

--- packages/ndla-ui/src/locale/messages-nn.ts
+++ packages/ndla-ui/src/locale/messages-nn.ts
@@ -896,7 +896,9 @@ const messages = {
     close: 'Lukk faktaboks',
   },
   myNdla: {
+    mainMenu: 'Hovedmeny',
     myNDLA: 'Min NDLA',
+    myNDLAMenu: 'Min NDLA meny',
     support: 'Brukarstøtte',
     resources: '{{count}} ressurs',
     resources_plural: '{{count}} ressursar',
@@ -906,6 +908,7 @@ const messages = {
     showEditOptions: 'Vis redigeringsmoglegheiter',
     folder: {
       folder: 'Mappe',
+      navigation: 'Mappenavigasjon',
       delete: 'Slett mappe',
       deleteShort: 'Slett',
       edit: 'Rediger mappe',

9dd05b852874ea8a9f9eef4336f8d3f160527359
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-02-05 14:02:15 +0100

    Remove redundant eslint-ignore

--- packages/ndla-ui/src/Logo/Logo.tsx
+++ packages/ndla-ui/src/Logo/Logo.tsx
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-/* eslint-disable max-len */
 
 import { useMemo } from "react";
 import { SerializedStyles, css } from "@emotion/react";

7df9475050cc5a29343630664c95ef1bada5d1a9
Gunnar Velle <gunnar.velle@gmail.com>
2024-02-23 13:14:07 +0100

    Wrap caption in color

--- packages/ndla-ui/src/Embed/ImageEmbed.tsx
+++ packages/ndla-ui/src/Embed/ImageEmbed.tsx
@@ -10,7 +10,7 @@ import parse from "html-react-parser";
 import { MouseEventHandler, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "@emotion/styled";
-import { utils } from "@ndla/core";
+import { colors, spacing, utils } from "@ndla/core";
 import { ExpandTwoArrows } from "@ndla/icons/action";
 import { ArrowCollapse, ChevronDown, ChevronUp } from "@ndla/icons/common";
 import { COPYRIGHTED } from "@ndla/licenses";
@@ -48,6 +48,12 @@ export const getLicenseCredits = (copyright?: {
     processors: copyright?.processors ?? [],
   };
 };
+
+const CaptionWrapper = styled.div`
+  background-color: ${colors.brand.lightest};
+  padding: ${spacing.nsmall} ${spacing.normal};
+  border: 1px solid ${colors.brand.light};
+`;
 
 export const errorSvgSrc = `data:image/svg+xml;charset=UTF-8,%3Csvg fill='%238A8888' height='400' viewBox='0 0 24 12' width='100%25' xmlns='http://www.w3.org/2000/svg' style='background-color: %23EFF0F2'%3E%3Cpath d='M0 0h24v24H0V0z' fill='none'/%3E%3Cpath transform='scale(0.3) translate(28, 8.5)' d='M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'/%3E%3C/svg%3E`;
 const isSmall = (size?: string): size is "xsmall" | "small" => size === "xsmall" || size === "small";
@@ -176,7 +182,9 @@ const ImageEmbed = ({
           }
           lang={lang}
         />
-        {parsedDescription}
+        <CaptionWrapper>
+          {parsedDescription}
+        </CaptionWrapper>
       </ImageWrapper>
       {isBylineHidden || (isSmall(embedData.size) && !imageSizes) ? null : (
         <EmbedByline

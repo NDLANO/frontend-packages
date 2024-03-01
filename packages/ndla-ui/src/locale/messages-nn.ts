2b4b4850d5ebc239227bca745ff3f2d82b643744
DeividasSvaikauskas <deividas.svaikauskas@bouvet.no>
2024-03-01 09:32:55 +0100

    move moderator email to be on its own

--- packages/ndla-ui/src/locale/messages-nn.ts
+++ packages/ndla-ui/src/locale/messages-nn.ts
@@ -1062,7 +1062,8 @@ const messages = {
         maxLength: "Maksimal lengd for tekstfeltet er nådd",
         error: "Feltet er påkravd",
       },
-      bottomText: "Saknar du ein kategori? Gi moderatoren vår beskjed på moderator@ndla.no",
+      bottomText: "Saknar du ein kategori? Gi moderatoren vår beskjed på ",
+      moderatorEmail: "moderator@ndla.no",
       notification: {
         title: "Varslingar",
         showAll: "Sjå alle varslingar",

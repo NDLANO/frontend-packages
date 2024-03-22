9907d7ca83ff0820919aea92e3bf3aa5d8f9d26f
Gaute Rauboti <gaute.rauboti@knowit.no>
2024-03-22 12:41:18 +0100

    Add more of the different types to stories

--- packages/ndla-ui/src/Resource/storyComponents.tsx
+++ packages/ndla-ui/src/Resource/storyComponents.tsx
@@ -22,6 +22,10 @@ export const resourceTypesArr = [
   { id: "urn:resourcetype:reviewResource", name: "Vurderingsressurs" },
   { id: "urn:resourcetype:externalResource", name: "Ekstern læringsressurs" },
   { id: "urn:resourcetype:SourceMaterial", name: "Kildemateriale" },
+  { id: "concept", name: "concept" },
+  { id: "image", name: "image" },
+  { id: "video", name: "video" },
+  { id: "audio", name: "audio" },
   { id: "folder", name: "folder" },
 ];
 

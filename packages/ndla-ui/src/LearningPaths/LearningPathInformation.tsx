e6d664af921e867dd74b056c74b6dd2f6e17c998
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-05-03 16:08:19 +0200

    Hotfix: Remove unused imports from LearningPathInformation

--- packages/ndla-ui/src/LearningPaths/LearningPathInformation.tsx
+++ packages/ndla-ui/src/LearningPaths/LearningPathInformation.tsx
@@ -7,9 +7,8 @@
  */
 
 import styled from "@emotion/styled";
-import { spacing, spacingUnit, breakpoints, mq, colors } from "@ndla/core";
+import { spacing, spacingUnit, breakpoints, mq } from "@ndla/core";
 import { getLicenseByAbbreviation } from "@ndla/licenses";
-import { LicenseByline } from "@ndla/notion";
 import { Heading } from "@ndla/typography";
 import LicenseLink from "../LicenseByline/LicenseLink";
 

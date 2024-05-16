0a9b2550c4127e7c8445e65408f3716af7aefa32
Gunnar Velle <gunnar.velle@gmail.com>
2024-05-16 13:34:56 +0200

    amateur

--- packages/hooks/src/useElementHeight.ts
+++ packages/hooks/src/useElementHeight.ts
@@ -10,29 +10,29 @@ import { useEffect, useState } from "react";
 import { resizeObserver } from "@ndla/util";
 
 export const useElementHeight = (elementId: string) => {
-    const [height, setHeight] = useState<number>();
-    const [mounted, setMounted] = useState(false);
-  
-    useEffect(() => {
-      setMounted(true);
-    }, []);
-  
-    useEffect(() => {
-      if (mounted) {
-        const element = document.getElementById(elementId);
-  
-        const handleHeightChange = (el: HTMLElement) => {
-          const newHeight = el.getBoundingClientRect().height;
-          setHeight(newHeight);
-        };
-  
-        if (element) {
-          resizeObserver(element, handleHeightChange);
-        }
+  const [height, setHeight] = useState<number>();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (mounted) {
+      const element = document.getElementById(elementId);
+
+      const handleHeightChange = (el: HTMLElement) => {
+        const newHeight = el.getBoundingClientRect().height;
+        setHeight(newHeight);
+      };
+
+      if (element) {
+        resizeObserver(element, handleHeightChange);
       }
-    }, [mounted, elementId]);
-  
-    return {
-      height,
-    };
+    }
+  }, [mounted, elementId]);
+
+  return {
+    height,
   };
+};

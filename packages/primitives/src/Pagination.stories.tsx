a840cf201148602d4bf715f94a18f5280f1bcc42
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-06-07 16:35:06 +0200

    Add Pagination

---
+++ packages/primitives/src/Pagination.stories.tsx
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { PaginationContext } from "@ark-ui/react";
+import { Meta, StoryObj } from "@storybook/react";
+import { ChevronLeft, ChevronRight } from "@ndla/icons/common";
+import { Button } from "./Button";
+import {
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationNextTrigger,
+  PaginationPrevTrigger,
+  PaginationRoot,
+} from "./Pagination";
+import { Text } from "./Text";
+
+export default {
+  title: "Primitives/Pagination",
+  component: PaginationRoot,
+  tags: ["autodocs"],
+  args: {
+    count: 5000,
+    pageSize: 10,
+    siblingCount: 2,
+  },
+  render: (args) => (
+    <PaginationRoot {...args}>
+      <PaginationPrevTrigger asChild>
+        <Button variant="tertiary">
+          <ChevronLeft />
+          Forrige
+        </Button>
+      </PaginationPrevTrigger>
+      <PaginationContext>
+        {(pagination) =>
+          pagination.pages.map((page, index) =>
+            page.type === "page" ? (
+              <PaginationItem key={index} {...page} asChild>
+                <Button variant={page.value === pagination.page ? "primary" : "tertiary"}>{page.value}</Button>
+              </PaginationItem>
+            ) : (
+              <PaginationEllipsis key={index} index={index} asChild>
+                <Text as="div">&#8230;</Text>
+              </PaginationEllipsis>
+            ),
+          )
+        }
+      </PaginationContext>
+      <PaginationNextTrigger asChild>
+        <Button variant="tertiary">
+          Neste
+          <ChevronRight />
+        </Button>
+      </PaginationNextTrigger>
+    </PaginationRoot>
+  ),
+} as Meta<typeof PaginationRoot>;
+
+export const Default: StoryObj<typeof PaginationRoot> = {};

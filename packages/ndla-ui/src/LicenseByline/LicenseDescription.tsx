82c5744950ab32ce571d16881d9a75e5f1799163
DeividasSvaikauskas <deividas.svaikauskas@bouvet.no>
2024-06-20 11:45:04 +0200

    initial commit for byline functionality on small devices

--- packages/ndla-ui/src/LicenseByline/LicenseDescription.tsx
+++ packages/ndla-ui/src/LicenseByline/LicenseDescription.tsx
@@ -6,9 +6,11 @@
  *
  */
 
-import { ReactNode } from "react";
+import { t } from "i18next";
+import { ReactNode, useState } from "react";
 import styled from "@emotion/styled";
-import { spacing } from "@ndla/core";
+import { ButtonV2 } from "@ndla/button";
+import { breakpoints, colors, fonts, spacing } from "@ndla/core";
 
 interface Props {
   description: ReactNode;
@@ -29,18 +31,62 @@ const StyledFigCaption = styled.figcaption`
   }
 `;
 
-const StyledDescription = styled.span`
-  display: inline;
+const StyledDescription = styled.div`
+  display: inline-flex;
   white-space: pre-wrap;
+  @media (max-width: ${breakpoints.mobileWide}) {
+    &[data-open="true"] {
+      display: inline;
+    }
+    width: calc(100%);
+  }
+`;
+
+const TextContent = styled.span`
+  @media (max-width: ${breakpoints.mobileWide}) {
+    white-space: nowrap;
+    /* maxHeight is set to 40 pixels to hide the rest of the text */
+    max-height: 40px;
+    &[data-open="true"] {
+      white-space: pre-wrap;
+      max-height: none;
+    }
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    transition: max-height 0.7s ease-in;
+    margin: auto 0;
+  }
+`;
+
+const Button = styled(ButtonV2)`
+  color: ${colors.brand.primary};
+  font-weight: ${fonts.weight.semibold};
+  min-width: fit-content;
+  margin-left: ${spacing.small};
+  @media (min-width: ${breakpoints.mobileWide}) {
+    display: none;
+  }
 `;
 
 const LicenseDescription = ({ description, icon, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <StyledFigCaption>
       {icon}
-      <StyledDescription>
-        {description}
-        {children}
+      <StyledDescription data-open={isOpen}>
+        <TextContent data-open={isOpen}>
+          {description}
+          {children}
+        </TextContent>
+        <Button variant="link" onClick={handleToggle}>
+          {isOpen ? `${t("audio.readLessDescriptionLabel")}` : `${t("audio.readMoreDescriptionLabel")}`}
+        </Button>
       </StyledDescription>
     </StyledFigCaption>
   );

53d9aa9eec1340737dfc5de0fc2e577e2a89d422
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-06-26 14:25:18 +0200

    Add Select

---
+++ packages/primitives/src/Select.stories.tsx
@@ -0,0 +1,380 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { useState } from "react";
+import { Portal } from "@ark-ui/react";
+import { Meta, StoryFn } from "@storybook/react";
+import { Cross } from "@ndla/icons/action";
+import { ChevronUp } from "@ndla/icons/common";
+import { Done } from "@ndla/icons/editor";
+import { css } from "@ndla/styled-system/css";
+import { HStack } from "@ndla/styled-system/jsx";
+import { Button, IconButton } from "./Button";
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "./Dialog";
+import { FieldRoot } from "./Field";
+import { FieldErrorMessage } from "./FieldErrorMessage";
+import { FieldHelper } from "./FieldHelper";
+import {
+  SelectClearTrigger,
+  SelectContent,
+  SelectControl,
+  SelectIndicator,
+  SelectItem,
+  SelectItemGroup,
+  SelectItemGroupLabel,
+  SelectItemIndicator,
+  SelectItemText,
+  SelectLabel,
+  SelectPositioner,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "./Select";
+import { europeanCountries, measurements } from "./storybookHelpers/data";
+
+export default {
+  title: "Primitives/Select",
+  component: SelectRoot,
+  tags: ["autodocs"],
+  args: {
+    positioning: { sameWidth: true },
+  },
+} satisfies Meta<typeof SelectRoot>;
+
+export const Default: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  return (
+    <SelectRoot {...args} items={measurements}>
+      <SelectLabel>Measurement</SelectLabel>
+      <SelectControl>
+        <SelectTrigger asChild forwardCssProp>
+          <Button variant="secondary">
+            <SelectValueText placeholder="Select measurement" />
+            <SelectIndicator asChild>
+              <ChevronUp />
+            </SelectIndicator>
+          </Button>
+        </SelectTrigger>
+      </SelectControl>
+      <Portal>
+        <SelectPositioner>
+          <SelectContent>
+            {measurements.map((measurement) => (
+              <SelectItem key={measurement.value} item={measurement}>
+                <SelectItemText>{measurement.label}</SelectItemText>
+                <SelectItemIndicator asChild>
+                  <Done />
+                </SelectItemIndicator>
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectPositioner>
+      </Portal>
+    </SelectRoot>
+  );
+};
+
+export const Disabled: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  return (
+    <SelectRoot {...args} disabled items={measurements}>
+      <SelectLabel>Measurement</SelectLabel>
+      <SelectControl>
+        <SelectTrigger asChild forwardCssProp>
+          <Button variant="secondary">
+            <SelectValueText placeholder="Select measurement" />
+            <SelectIndicator asChild>
+              <ChevronUp />
+            </SelectIndicator>
+          </Button>
+        </SelectTrigger>
+      </SelectControl>
+      <Portal>
+        <SelectPositioner>
+          <SelectContent>
+            {measurements.map((measurement) => (
+              <SelectItem key={measurement.value} item={measurement}>
+                <SelectItemText>{measurement.label}</SelectItemText>
+                <SelectItemIndicator asChild>
+                  <Done />
+                </SelectItemIndicator>
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectPositioner>
+      </Portal>
+    </SelectRoot>
+  );
+};
+
+export const DisabledItems: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  const withDisabled = measurements.map((measurement, index) => ({ ...measurement, disabled: index % 2 === 1 }));
+  return (
+    <SelectRoot {...args} items={withDisabled}>
+      <SelectLabel>Measurement</SelectLabel>
+      <SelectControl>
+        <SelectTrigger asChild forwardCssProp>
+          <Button variant="secondary">
+            <SelectValueText placeholder="Select measurement" />
+            <SelectIndicator asChild>
+              <ChevronUp />
+            </SelectIndicator>
+          </Button>
+        </SelectTrigger>
+      </SelectControl>
+      <Portal>
+        <SelectPositioner>
+          <SelectContent>
+            {withDisabled.map((measurement) => (
+              <SelectItem key={measurement.value} item={measurement}>
+                <SelectItemText>{measurement.label}</SelectItemText>
+                <SelectItemIndicator asChild>
+                  <Done />
+                </SelectItemIndicator>
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectPositioner>
+      </Portal>
+    </SelectRoot>
+  );
+};
+
+const groupedCountries = europeanCountries.reduce<Record<string, (typeof europeanCountries)[0][]>>((acc, country) => {
+  const firstLetter = country.label[0].toUpperCase();
+  if (!acc[firstLetter]) {
+    acc[firstLetter] = [];
+  }
+  acc[firstLetter].push(country);
+  return acc;
+}, {});
+
+export const Grouped: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  return (
+    <SelectRoot {...args} items={europeanCountries}>
+      <SelectLabel>Country</SelectLabel>
+      <SelectControl>
+        <SelectTrigger asChild forwardCssProp>
+          <Button variant="secondary">
+            <SelectValueText placeholder="Choose country" />
+            <SelectIndicator asChild>
+              <ChevronUp />
+            </SelectIndicator>
+          </Button>
+        </SelectTrigger>
+      </SelectControl>
+      <Portal>
+        <SelectPositioner>
+          <SelectContent>
+            {Object.entries(groupedCountries).map(([letter, countries]) => (
+              <SelectItemGroup key={letter}>
+                <SelectItemGroupLabel>{letter}</SelectItemGroupLabel>
+                {countries.map((country) => (
+                  <SelectItem key={country.value} item={country}>
+                    <SelectItemText>{country.label}</SelectItemText>
+                    <SelectItemIndicator>
+                      <Done />
+                    </SelectItemIndicator>
+                  </SelectItem>
+                ))}
+              </SelectItemGroup>
+            ))}
+          </SelectContent>
+        </SelectPositioner>
+      </Portal>
+    </SelectRoot>
+  );
+};
+
+export const Multiple: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  return (
+    <SelectRoot {...args} items={europeanCountries} multiple>
+      <SelectLabel>Countries you've been to</SelectLabel>
+      <SelectControl>
+        <SelectTrigger asChild forwardCssProp>
+          <Button variant="secondary">
+            <SelectValueText placeholder="Choose country" />
+            <SelectIndicator asChild>
+              <ChevronUp />
+            </SelectIndicator>
+          </Button>
+        </SelectTrigger>
+      </SelectControl>
+      <Portal>
+        <SelectPositioner>
+          <SelectContent className={css({ maxHeight: "surface.small" })}>
+            {Object.entries(groupedCountries).map(([letter, countries]) => (
+              <SelectItemGroup key={letter}>
+                <SelectItemGroupLabel>{letter}</SelectItemGroupLabel>
+                {countries.map((country) => (
+                  <SelectItem key={country.value} item={country}>
+                    <SelectItemText>{country.label}</SelectItemText>
+                    <SelectItemIndicator>
+                      <Done />
+                    </SelectItemIndicator>
+                  </SelectItem>
+                ))}
+              </SelectItemGroup>
+            ))}
+          </SelectContent>
+        </SelectPositioner>
+      </Portal>
+    </SelectRoot>
+  );
+};
+
+export const WithClearButton: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  return (
+    <SelectRoot {...args} items={europeanCountries} multiple>
+      <SelectLabel>Countries you've been to</SelectLabel>
+      <SelectControl>
+        <SelectTrigger asChild forwardCssProp>
+          <Button variant="secondary">
+            <SelectValueText placeholder="Choose country" />
+            <SelectIndicator asChild>
+              <ChevronUp />
+            </SelectIndicator>
+          </Button>
+        </SelectTrigger>
+        <SelectClearTrigger asChild forwardCssProp>
+          <IconButton variant="secondary">
+            <Cross />
+          </IconButton>
+        </SelectClearTrigger>
+      </SelectControl>
+      <Portal>
+        <SelectPositioner>
+          <SelectContent className={css({ maxHeight: "surface.small" })}>
+            {Object.entries(groupedCountries).map(([letter, countries]) => (
+              <SelectItemGroup key={letter}>
+                <SelectItemGroupLabel>{letter}</SelectItemGroupLabel>
+                {countries.map((country) => (
+                  <SelectItem key={country.value} item={country}>
+                    <SelectItemText>{country.label}</SelectItemText>
+                    <SelectItemIndicator>
+                      <Done />
+                    </SelectItemIndicator>
+                  </SelectItem>
+                ))}
+              </SelectItemGroup>
+            ))}
+          </SelectContent>
+        </SelectPositioner>
+      </Portal>
+    </SelectRoot>
+  );
+};
+
+/**
+ * This example is not portalled, and does not work properly on the docs page.
+ */
+export const InDialog: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  return (
+    <DialogRoot>
+      <DialogTrigger asChild>
+        <Button>Open dialog</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Choose your measurement</DialogTitle>
+          <DialogCloseTrigger>
+            <IconButton variant="clear">
+              <Cross />
+            </IconButton>
+          </DialogCloseTrigger>
+        </DialogHeader>
+        <DialogBody>
+          <DialogDescription>
+            We need to know your favorite measurement to correctly assert what sort of person you are.
+          </DialogDescription>
+          <SelectRoot
+            {...args}
+            items={measurements}
+            positioning={{ strategy: "fixed", placement: "bottom", sameWidth: true }}
+          >
+            <SelectLabel>Measurement</SelectLabel>
+            <SelectControl>
+              <SelectTrigger asChild forwardCssProp>
+                <Button variant="secondary">
+                  <SelectValueText placeholder="Select measurement" />
+                  <SelectIndicator asChild>
+                    <ChevronUp />
+                  </SelectIndicator>
+                </Button>
+              </SelectTrigger>
+            </SelectControl>
+            <SelectPositioner>
+              <SelectContent>
+                {measurements.map((measurement) => (
+                  <SelectItem key={measurement.value} item={measurement}>
+                    <SelectItemText>{measurement.label}</SelectItemText>
+                    <SelectItemIndicator>
+                      <Done />
+                    </SelectItemIndicator>
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </SelectPositioner>
+          </SelectRoot>
+          <HStack gap="xsmall" className={css({ alignSelf: "flex-end" })}>
+            <DialogCloseTrigger asChild>
+              <Button variant="secondary">Cancel</Button>
+            </DialogCloseTrigger>
+            <Button>Save</Button>
+          </HStack>
+        </DialogBody>
+      </DialogContent>
+    </DialogRoot>
+  );
+};
+
+export const WithField: StoryFn<typeof SelectRoot> = ({ ...args }) => {
+  const [value, setValue] = useState<string[]>([]);
+  const invalid = value.some((val) => val === "cm");
+
+  return (
+    <FieldRoot invalid={invalid}>
+      <SelectRoot value={value} onValueChange={(val) => setValue(val.value)} {...args} items={measurements}>
+        <SelectLabel>Measurement</SelectLabel>
+        <FieldHelper>You cannot choose centimeter.</FieldHelper>
+        <FieldErrorMessage>I told you to choose anything but centimeter...</FieldErrorMessage>
+        <SelectControl>
+          <SelectTrigger asChild forwardCssProp>
+            <Button variant="secondary">
+              <SelectValueText placeholder="Select measurement" />
+              <SelectIndicator asChild>
+                <ChevronUp />
+              </SelectIndicator>
+            </Button>
+          </SelectTrigger>
+        </SelectControl>
+        <Portal>
+          <SelectPositioner>
+            <SelectContent>
+              {measurements.map((measurement) => (
+                <SelectItem key={measurement.value} item={measurement}>
+                  <SelectItemText>{measurement.label}</SelectItemText>
+                  <SelectItemIndicator asChild>
+                    <Done />
+                  </SelectItemIndicator>
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </SelectPositioner>
+        </Portal>
+      </SelectRoot>
+    </FieldRoot>
+  );
+};
---
+++ packages/primitives/src/Select.tsx
@@ -0,0 +1,218 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { forwardRef } from "react";
+import { selectAnatomy } from "@ark-ui/anatomy";
+import { Select } from "@ark-ui/react";
+import { sva } from "@ndla/styled-system/css";
+import { JsxStyleProps } from "@ndla/styled-system/types";
+import { createStyleContext } from "./createStyleContext";
+import { Label } from "./Label";
+import { TextProps } from "./Text";
+
+const selectRecipe = sva({
+  slots: selectAnatomy.keys(),
+  base: {
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "3xsmall",
+    },
+    control: {
+      display: "flex",
+      gap: "4xsmall",
+      alignItems: "center",
+    },
+    content: {
+      display: "flex",
+      gap: "4xsmall",
+      flexDirection: "column",
+      zIndex: "dropdown",
+      background: "surface.default",
+      borderRadius: "xsmall",
+      boxShadow: "large",
+      padding: "xsmall",
+      overflowY: "auto",
+      maxHeight: "surface.xsmall",
+      _focusVisible: {
+        outlineOffset: "-1",
+      },
+      _open: {
+        animation: "fade-shift-in 0.25s ease-out",
+      },
+      _closed: {
+        animation: "fade-shift-out 0.25s ease-out",
+      },
+    },
+    item: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      padding: "xsmall",
+      background: "surface.default",
+      borderRadius: "xsmall",
+      cursor: "pointer",
+      transitionDuration: "fast",
+      transitionProperty: "background, color",
+      transitionTimingFunction: "default",
+      _hover: {
+        background: "surface.hover",
+      },
+      _selected: {
+        textDecoration: "underline",
+        background: "surface.selected",
+        _hover: {
+          background: "surface.hover",
+        },
+        _highlighted: {
+          background: "surface.hover",
+        },
+      },
+      _highlighted: {
+        textDecoration: "underline",
+        background: "surface.hover",
+        _hover: {
+          background: "surface.hover",
+        },
+      },
+      _disabled: {
+        cursor: "not-allowed",
+        color: "text.disabled",
+        background: "surface.disabled",
+        _highlighted: {
+          color: "text.disabled",
+          background: "surface.disabled",
+        },
+        _selected: {
+          color: "text.disabled",
+          background: "surface.disabled",
+        },
+        _hover: {
+          color: "text.disabled",
+          background: "surface.disabled",
+        },
+      },
+    },
+    indicator: {
+      color: "icon.default",
+      transformOrigin: "center",
+      transitionDuration: "normal",
+      transitionProperty: "transform",
+      transitionTimingFunction: "default",
+      _open: {
+        transform: "rotate(180deg)",
+      },
+    },
+    itemIndicator: {
+      color: "stroke.default",
+    },
+    trigger: {
+      justifyContent: "space-between",
+      width: "surface.small",
+      _ariaInvalid: {
+        boxShadowColor: "stroke.error",
+      },
+    },
+    itemGroup: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "4xsmall",
+    },
+  },
+});
+
+const { withProvider, withContext } = createStyleContext(selectRecipe);
+
+export type SelectRootProps<T extends Select.CollectionItem> = Select.RootProps<T> & JsxStyleProps;
+const InternalSelectRoot = withProvider<HTMLDivElement, SelectRootProps<Select.CollectionItem>>(Select.Root, "root");
+
+export const SelectRoot = <T extends Select.CollectionItem>({
+  lazyMount = true,
+  unmountOnExit = true,
+  ...props
+}: SelectRootProps<T>) => (
+  //@ts-expect-error -- T does not necessarily match Select.CollectionItem. However, we prefer to use T over Select.CollectionItem to get the correct type during use.
+  <InternalSelectRoot lazyMount={lazyMount} unmountOnExit={unmountOnExit} {...props} />
+);
+
+export const SelectClearTrigger = withContext<HTMLButtonElement, Select.ClearTriggerProps & JsxStyleProps>(
+  Select.ClearTrigger,
+  "clearTrigger",
+);
+
+export const SelectContent = withContext<HTMLDivElement, Select.ContentProps & JsxStyleProps>(
+  Select.Content,
+  "content",
+);
+
+export const SelectControl = withContext<HTMLDivElement, Select.ControlProps & JsxStyleProps>(
+  Select.Control,
+  "control",
+);
+
+export const SelectIndicator = withContext<HTMLDivElement, Select.IndicatorProps & JsxStyleProps>(
+  Select.Indicator,
+  "indicator",
+);
+
+export const SelectItemGroupLabel = forwardRef<HTMLDivElement, Select.ItemGroupLabelProps & JsxStyleProps & TextProps>(
+  ({ children, ...props }, ref) => (
+    <InternalSelectItemGroupLabel asChild forwardCssProp ref={ref} {...props}>
+      <Label asChild>
+        <div>{children}</div>
+      </Label>
+    </InternalSelectItemGroupLabel>
+  ),
+);
+
+const InternalSelectItemGroupLabel = withContext<HTMLDivElement, Select.ItemGroupLabelProps & JsxStyleProps>(
+  Select.ItemGroupLabel,
+  "itemGroupLabel",
+);
+
+export const SelectItemGroup = withContext<HTMLDivElement, Select.ItemGroupProps & JsxStyleProps>(
+  Select.ItemGroup,
+  "itemGroup",
+);
+
+export const SelectItemIndicator = withContext<HTMLDivElement, Select.ItemIndicatorProps & JsxStyleProps>(
+  Select.ItemIndicator,
+  "itemIndicator",
+);
+
+export const SelectItem = withContext<HTMLDivElement, Select.ItemProps & JsxStyleProps>(Select.Item, "item");
+
+export const SelectItemText = withContext<HTMLDivElement, Select.ItemTextProps & JsxStyleProps>(
+  Select.ItemText,
+  "itemText",
+);
+
+const InternalSelectLabel = withContext<HTMLLabelElement, Select.LabelProps & JsxStyleProps>(Select.Label, "label");
+
+export const SelectLabel = forwardRef<HTMLLabelElement, Select.LabelProps & JsxStyleProps & TextProps>(
+  ({ children, ...props }, ref) => (
+    <InternalSelectLabel asChild forwardCssProp ref={ref} {...props}>
+      <Label>{children}</Label>
+    </InternalSelectLabel>
+  ),
+);
+
+export const SelectPositioner = withContext<HTMLDivElement, Select.PositionerProps & JsxStyleProps>(
+  Select.Positioner,
+  "positioner",
+);
+
+export const SelectTrigger = withContext<HTMLButtonElement, Select.TriggerProps & JsxStyleProps>(
+  Select.Trigger,
+  "trigger",
+);
+
+export const SelectValueText = withContext<HTMLSpanElement, Select.ValueTextProps & JsxStyleProps>(
+  Select.ValueText,
+  "valueText",
+);

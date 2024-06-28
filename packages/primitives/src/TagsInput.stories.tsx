1307fe18477cb22a3c7422c911631cb1bf9c6ee6
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-06-28 09:03:42 +0200

    Initial version of TagsInput

---
+++ packages/primitives/src/TagsInput.stories.tsx
@@ -0,0 +1,304 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { useId, useRef, useState } from "react";
+import { ComboboxContext, ComboboxInputValueChangeDetails, Portal, TagsInputContext } from "@ark-ui/react";
+import { Meta, StoryFn } from "@storybook/react";
+import { Cross } from "@ndla/icons/action";
+import { HStack } from "@ndla/styled-system/jsx";
+import { contains } from "@ndla/util";
+import { IconButton } from "./Button";
+import {
+  ComboboxClearTrigger,
+  ComboboxContent,
+  ComboboxControl,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxItemIndicator,
+  ComboboxItemText,
+  ComboboxLabel,
+  ComboboxPositioner,
+  ComboboxRoot,
+  ComboboxTrigger,
+} from "./Combobox";
+import { FieldRoot } from "./Field";
+import { FieldErrorMessage } from "./FieldErrorMessage";
+import { FieldHelper } from "./FieldHelper";
+import { Input, InputContainer } from "./Input";
+import {
+  TagsInputClearTrigger,
+  TagsInputControl,
+  TagsInputInput,
+  TagsInputItem,
+  TagsInputItemDeleteTrigger,
+  TagsInputItemPreview,
+  TagsInputItemText,
+  TagsInputLabel,
+  TagsInputRoot,
+} from "./TagsInput";
+import { ChevronDown } from "../../ndla-icons/src/common";
+import { Done } from "../../ndla-icons/src/editor";
+
+const meta: Meta<typeof TagsInputRoot> = {
+  title: "Primitives/TagsInput",
+  component: TagsInputRoot,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Default: StoryFn<typeof TagsInputRoot> = ({ ...args }) => {
+  return (
+    <TagsInputRoot {...args} editable>
+      <TagsInputLabel>Favorittfag</TagsInputLabel>
+      <HStack gap="4xsmall">
+        <TagsInputContext>
+          {(api) => (
+            <TagsInputControl forwardCssProp asChild>
+              <InputContainer>
+                {api.value.map((value, index) => (
+                  <TagsInputItem key={index} index={index} value={value}>
+                    <TagsInputItemPreview>
+                      <TagsInputItemText>{value}</TagsInputItemText>
+                      <TagsInputItemDeleteTrigger>
+                        <Cross />
+                      </TagsInputItemDeleteTrigger>
+                    </TagsInputItemPreview>
+                  </TagsInputItem>
+                ))}
+                <TagsInputInput placeholder="Legg til fag" asChild>
+                  <Input />
+                </TagsInputInput>
+              </InputContainer>
+            </TagsInputControl>
+          )}
+        </TagsInputContext>
+        <TagsInputClearTrigger asChild>
+          <IconButton variant="secondary">
+            <Cross />
+          </IconButton>
+        </TagsInputClearTrigger>
+      </HStack>
+    </TagsInputRoot>
+  );
+};
+
+export const Disabled: StoryFn<typeof TagsInputRoot> = ({ ...args }) => {
+  return (
+    <TagsInputRoot {...args} disabled>
+      <TagsInputLabel>Favorittfag</TagsInputLabel>
+      <HStack gap="4xsmall">
+        <TagsInputContext>
+          {(api) => (
+            <TagsInputControl forwardCssProp asChild>
+              <InputContainer>
+                {api.value.map((value, index) => (
+                  <TagsInputItem key={index} index={index} value={value}>
+                    <TagsInputItemPreview>
+                      <TagsInputItemText>{value}</TagsInputItemText>
+                      <TagsInputItemDeleteTrigger>
+                        <Cross />
+                      </TagsInputItemDeleteTrigger>
+                    </TagsInputItemPreview>
+                  </TagsInputItem>
+                ))}
+                <TagsInputInput placeholder="Legg til fag" asChild>
+                  <Input />
+                </TagsInputInput>
+              </InputContainer>
+            </TagsInputControl>
+          )}
+        </TagsInputContext>
+        <TagsInputClearTrigger asChild>
+          <IconButton variant="secondary">
+            <Cross />
+          </IconButton>
+        </TagsInputClearTrigger>
+      </HStack>
+    </TagsInputRoot>
+  );
+};
+
+export const WithField: StoryFn<typeof TagsInputRoot> = ({ ...args }) => {
+  const [value, setValue] = useState<string[]>([]);
+  const invalid = value.length > 3;
+
+  return (
+    <FieldRoot invalid={invalid}>
+      <TagsInputRoot {...args} value={value} onValueChange={(details) => setValue(details.value)}>
+        <TagsInputLabel>Favorittfag</TagsInputLabel>
+        <FieldHelper>Du kan bare velge tre tags</FieldHelper>
+        <FieldErrorMessage>Ikke mer enn tre!!</FieldErrorMessage>
+        <HStack gap="4xsmall">
+          <TagsInputContext>
+            {(api) => (
+              <TagsInputControl forwardCssProp asChild>
+                <InputContainer>
+                  {api.value.map((value, index) => (
+                    <TagsInputItem key={index} index={index} value={value}>
+                      <TagsInputItemPreview>
+                        <TagsInputItemText>{value}</TagsInputItemText>
+                        <TagsInputItemDeleteTrigger>
+                          <Cross />
+                        </TagsInputItemDeleteTrigger>
+                      </TagsInputItemPreview>
+                    </TagsInputItem>
+                  ))}
+                  <TagsInputInput placeholder="Legg til fag" asChild>
+                    <Input />
+                  </TagsInputInput>
+                </InputContainer>
+              </TagsInputControl>
+            )}
+          </TagsInputContext>
+          <TagsInputClearTrigger asChild>
+            <IconButton variant="secondary">
+              <Cross />
+            </IconButton>
+          </TagsInputClearTrigger>
+        </HStack>
+      </TagsInputRoot>
+    </FieldRoot>
+  );
+};
+
+const data = [
+  "BackToSchool",
+  "SchoolLife",
+  "Homework",
+  "SchoolDays",
+  "Classroom",
+  "StudyTime",
+  "TeacherLife",
+  "StudentLife",
+  "SchoolSpirit",
+  "Education",
+];
+
+export const ComboboxTags: StoryFn<typeof ComboboxRoot> = () => {
+  const [options, setOptions] = useState(data);
+  const [value, setValue] = useState<string[]>([]);
+
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const handleChange = (e: ComboboxInputValueChangeDetails) => {
+    const filtered = data.filter((item) => item.toLowerCase().includes(e.inputValue.toLowerCase()));
+    setOptions(filtered);
+  };
+
+  const ids = {
+    root: useId(),
+    input: useId(),
+    control: useId(),
+  };
+
+  const tagsInputId = useId();
+  const comboboxId = useId();
+
+  return (
+    <ComboboxRoot
+      id={comboboxId}
+      items={options}
+      ids={ids}
+      allowCustomValue
+      multiple
+      selectionBehavior="clear"
+      value={value}
+      onInputValueChange={handleChange}
+      onValueChange={(details) => {
+        queueMicrotask(() => {
+          setValue(details.value);
+          setOptions((curr) => curr.filter((item) => !details.value.includes(item)));
+          contentRef.current?.scrollTo(0, 0);
+        });
+      }}
+      asChild
+    >
+      <ComboboxContext>
+        {(comboboxApi) => (
+          <TagsInputRoot
+            id={tagsInputId}
+            ids={ids}
+            editable={false}
+            addOnPaste={false}
+            onInteractOutside={(event) => {
+              const { target } = event.detail.originalEvent;
+              if (contains(target, contentRef.current)) {
+                event.preventDefault();
+              }
+            }}
+            inputValue={comboboxApi.inputValue}
+            value={value}
+            onValueChange={(details) => setValue(details.value)}
+            onInputValueChange={(details) => comboboxApi.setInputValue(details.inputValue)}
+          >
+            <ComboboxLabel>Emneknagger</ComboboxLabel>
+            <TagsInputContext>
+              {(tagsInputApi) => (
+                <HStack gap="4xsmall">
+                  <ComboboxControl asChild forwardCssProp>
+                    <TagsInputControl asChild forwardCssProp>
+                      <InputContainer>
+                        {tagsInputApi.value.map((value, index) => (
+                          <TagsInputItem index={index} value={value} key={index}>
+                            <TagsInputItemPreview>
+                              <TagsInputItemText>{value}</TagsInputItemText>
+                              <TagsInputItemDeleteTrigger>
+                                <Cross />
+                              </TagsInputItemDeleteTrigger>
+                            </TagsInputItemPreview>
+                          </TagsInputItem>
+                        ))}
+                        <ComboboxInput asChild forwardCssProp>
+                          <TagsInputInput asChild forwardCssProp>
+                            <Input
+                              placeholder="Skriv inn en emneknagg"
+                              onKeyDown={(event) => {
+                                if (event.key === "Enter" && options.length === 0) {
+                                  tagsInputApi.addValue(tagsInputApi.inputValue);
+                                }
+                              }}
+                            />
+                          </TagsInputInput>
+                        </ComboboxInput>
+                        <ComboboxClearTrigger asChild forwardCssProp>
+                          <IconButton variant="clear">
+                            <Cross />
+                          </IconButton>
+                        </ComboboxClearTrigger>
+                      </InputContainer>
+                    </TagsInputControl>
+                  </ComboboxControl>
+                  <ComboboxTrigger asChild>
+                    <IconButton variant="secondary">
+                      <ChevronDown />
+                    </IconButton>
+                  </ComboboxTrigger>
+                </HStack>
+              )}
+            </TagsInputContext>
+            <Portal>
+              <ComboboxPositioner>
+                <ComboboxContent>
+                  {options.map((item) => (
+                    <ComboboxItem key={item} item={item}>
+                      <ComboboxItemText>{item}</ComboboxItemText>
+                      <ComboboxItemIndicator>
+                        <Done />
+                      </ComboboxItemIndicator>
+                    </ComboboxItem>
+                  ))}
+                </ComboboxContent>
+              </ComboboxPositioner>
+            </Portal>
+          </TagsInputRoot>
+        )}
+      </ComboboxContext>
+    </ComboboxRoot>
+  );
+};
---
+++ packages/primitives/src/TagsInput.tsx
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { forwardRef } from "react";
+import { tagsInputAnatomy } from "@ark-ui/anatomy";
+import { TagsInput } from "@ark-ui/react";
+import { sva } from "@ndla/styled-system/css";
+import { JsxStyleProps } from "@ndla/styled-system/types";
+import { createStyleContext } from "./createStyleContext";
+import { Label } from "./Label";
+import { TextProps } from "./Text";
+
+const tagsInputRecipe = sva({
+  slots: tagsInputAnatomy.keys(),
+  base: {
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "xxsmall",
+      width: "full",
+    },
+    control: {
+      display: "inline-flex",
+      gap: "xxsmall",
+      alignItems: "center",
+      flexWrap: "wrap",
+    },
+    item: {
+      paddingBlock: "3xsmall",
+    },
+    itemPreview: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: "1",
+      paddingBlock: "4xsmall",
+      paddingInlineEnd: "3xsmall",
+      paddingInlineStart: "xsmall",
+      cursor: "initial",
+      borderRadius: "large",
+      outline: "1px solid",
+      outlineColor: "transparent",
+      backgroundColor: "surface.actionSubtle.selected",
+      color: "text.onAction",
+      transitionDuration: "normal",
+      transitionProperty: "background, outline-color, color",
+      transitionTimingFunction: "default",
+      textStyle: "label.medium",
+      _hover: {
+        backgroundColor: "surface.actionSubtle.hover",
+        outlineColor: "stroke.hover",
+        color: "text.default",
+        "& svg": {
+          color: "stroke.hover",
+        },
+      },
+      _highlighted: {
+        backgroundColor: "surface.actionSubtle.hover",
+        outlineWidth: "3px",
+        outlineOffset: "-1px",
+        outlineColor: "stroke.hover",
+        color: "text.default",
+        "& svg": {
+          color: "stroke.hover",
+        },
+      },
+    },
+    input: {
+      flex: "1",
+    },
+    // TODO: This needs to be looked at. It doesn't behave properly.
+    itemDeleteTrigger: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      cursor: "pointer",
+      height: "medium",
+      width: "medium",
+      lineHeight: "inherit",
+      "& span": {
+        display: "inline-block",
+      },
+      "& svg": {
+        marginInline: "0",
+        marginBlock: "0",
+        width: "medium",
+        height: "medium",
+      },
+    },
+  },
+});
+const { withProvider, withContext } = createStyleContext(tagsInputRecipe);
+
+export type TagsInputRootProps = TagsInput.RootProps & JsxStyleProps;
+export const TagsInputRoot = withProvider<HTMLDivElement, TagsInputRootProps>(TagsInput.Root, "root");
+
+export const TagsInputClearTrigger = withContext<HTMLButtonElement, TagsInput.ClearTriggerProps & JsxStyleProps>(
+  TagsInput.ClearTrigger,
+  "clearTrigger",
+);
+
+export const TagsInputControl = withContext<HTMLDivElement, TagsInput.ControlProps & JsxStyleProps>(
+  TagsInput.Control,
+  "control",
+);
+
+export const TagsInputInput = withContext<HTMLInputElement, TagsInput.InputProps & JsxStyleProps>(
+  TagsInput.Input,
+  "input",
+);
+
+export const TagsInputItemDeleteTrigger = withContext<
+  HTMLButtonElement,
+  TagsInput.ItemDeleteTriggerProps & JsxStyleProps
+>(TagsInput.ItemDeleteTrigger, "itemDeleteTrigger");
+
+export const TagsInputItemInput = withContext<HTMLInputElement, TagsInput.ItemInputProps & JsxStyleProps>(
+  TagsInput.ItemInput,
+  "itemInput",
+);
+
+export const TagsInputItemPreview = withContext<HTMLDivElement, TagsInput.ItemPreviewProps & JsxStyleProps>(
+  TagsInput.ItemPreview,
+  "itemPreview",
+);
+
+export const TagsInputItem = withContext<HTMLDivElement, TagsInput.ItemProps & JsxStyleProps>(TagsInput.Item, "item");
+
+export const TagsInputItemText = withContext<HTMLSpanElement, TagsInput.ItemTextProps & JsxStyleProps>(
+  TagsInput.ItemText,
+  "itemText",
+);
+
+// export const TagsInputItemText = forwardRef<HTMLSpanElement, TagsInput.ItemTextProps & JsxStyleProps & TextProps>(
+//   ({ children, textStyle = "label.medium", ...props }, ref) => (
+//     <InternalTagsInputItemText asChild forwardCssProp ref={ref} {...props}>
+//       <Text textStyle={textStyle} asChild>
+//         <div>{children}</div>
+//       </Text>
+//     </InternalTagsInputItemText>
+//   ),
+// );
+
+const InternalTagsInputLabel = withContext<HTMLLabelElement, TagsInput.LabelProps & JsxStyleProps>(
+  TagsInput.Label,
+  "label",
+);
+
+export const TagsInputLabel = forwardRef<HTMLLabelElement, TagsInput.LabelProps & JsxStyleProps & TextProps>(
+  ({ children, ...props }, ref) => (
+    <InternalTagsInputLabel asChild forwardCssProp ref={ref} {...props}>
+      <Label>{children}</Label>
+    </InternalTagsInputLabel>
+  ),
+);

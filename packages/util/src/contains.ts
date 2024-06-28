084370be9e719536cce3fd127c3ab8a5f5ea46f7
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-06-28 09:03:23 +0200

    Add a contains utility for ark usage

---
+++ packages/util/src/contains.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+type Target = HTMLElement | EventTarget | null | undefined;
+
+export const isHTMLElement = (v: any): v is HTMLElement =>
+  typeof v === "object" && v?.nodeType === Node.ELEMENT_NODE && typeof v?.nodeName === "string";
+
+export const contains = (parent: Target, child: Target) => {
+  if (!parent || !child) return false;
+  if (!isHTMLElement(parent) || !isHTMLElement(child)) return false;
+  return parent === child || parent.contains(child);
+};

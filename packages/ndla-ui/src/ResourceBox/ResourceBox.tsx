d56294ab4b0f57679e3f44c52d4c6641c88ac31f
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-07-10 14:11:36 +0200

    Refactor external and iframe embed

--- packages/ndla-ui/src/ResourceBox/ResourceBox.tsx
+++ packages/ndla-ui/src/ResourceBox/ResourceBox.tsx
@@ -6,65 +6,103 @@
  *
  */
 
-import styled from "@emotion/styled";
-import { breakpoints, fonts, mq, colors, spacing } from "@ndla/core";
 import { Launch } from "@ndla/icons/common";
-import { Image } from "@ndla/primitives";
+import { Heading, Image, Text } from "@ndla/primitives";
 import { SafeLinkButton } from "@ndla/safelink";
+import { styled } from "@ndla/styled-system/jsx";
+import { token } from "@ndla/styled-system/tokens";
 
-const ResourceBoxContainer = styled.div`
-  display: flex;
-  position: relative;
-  padding: ${spacing.nsmall};
-  border-radius: 5px;
-  border: 1px solid ${colors.brand.light};
-  font-family: ${fonts.sans};
-  box-shadow: 0px 20px 35px -15px rgba(32, 88, 143, 0.15);
-  gap: ${spacing.medium};
+const Container = styled("div", {
+  base: {
+    display: "flex",
+    padding: "medium",
+    borderRadius: "xsmall",
+    border: "1px solid",
+    borderColor: "stroke.default",
+    boxShadow: "full",
+    gap: "medium",
+    // TODO: Consider mobile styling
+    //   ${mq.range({ until: breakpoints.desktop })} {
+    //     gap: 0;
+    //     flex-direction: column;
+    //     padding-top: ${spacing.medium};
+    //     text-align: center;
+    //   }
+  },
+});
 
-  ${mq.range({ until: breakpoints.desktop })} {
-    gap: 0;
-    flex-direction: column;
-    padding-top: ${spacing.medium};
-    text-align: center;
-  }
-`;
+const ContentWrapper = styled("div", {
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "xsmall",
+    flex: "1",
+  },
+});
 
-const Title = styled.h3`
-  font-weight: ${fonts.weight.bold};
-  ${fonts.sizes(18)};
-  margin-top: 0;
-`;
+// const ResourceBoxContainer = styled.div`
+//   display: flex;
+//   position: relative;
+//   padding: ${spacing.nsmall};
+//   border-radius: 5px;
+//   border: 1px solid ${colors.brand.light};
+//   font-family: ${fonts.sans};
+//   box-shadow: 0px 20px 35px -15px rgba(32, 88, 143, 0.15);
+//   gap: ${spacing.medium};
+//
+// `;
+//
+// const Title = styled.h3`
+//   font-weight: ${fonts.weight.bold};
+//   ${fonts.sizes(18)};
+//   margin-top: 0;
+// `;
+//
+// const Caption = styled.p`
+//   ${fonts.sizes(14)};
+// `;
+//
+// const ContentWrapper = styled.div`
+//   flex-basis: 0;
+//   flex-grow: 1;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   ${mq.range({ until: breakpoints.desktop })} {
+//     align-items: center;
+//     padding-top: ${spacing.small};
+//   }
+// `;
+//
+// const StyledImage = styled(Image)`
+//   && {
+//     object-fit: cover;
+//     width: 134px;
+//     height: 134px;
+//     border-radius: 5px;
+//
+//     ${mq.range({ until: breakpoints.desktop })} {
+//       width: 200px;
+//       height: 200px;
+//     }
+//   }
+// `;
 
-const Caption = styled.p`
-  ${fonts.sizes(14)};
-`;
+const StyledImage = styled(Image, {
+  base: {
+    objectFit: "cover",
+    borderRadius: "xsmall",
+    height: "100%",
+    width: "min-content",
+    aspectRatio: "1/1",
+  },
+});
 
-const ContentWrapper = styled.div`
-  flex-basis: 0;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  ${mq.range({ until: breakpoints.desktop })} {
-    align-items: center;
-    padding-top: ${spacing.small};
-  }
-`;
-
-const StyledImage = styled(Image)`
-  && {
-    object-fit: cover;
-    width: 134px;
-    height: 134px;
-    border-radius: 5px;
-
-    ${mq.range({ until: breakpoints.desktop })} {
-      width: 200px;
-      height: 200px;
-    }
-  }
-`;
+const StyledText = styled(Text, {
+  base: {
+    flex: "1",
+  },
+});
 
 interface ImageMeta {
   src: string;
@@ -81,18 +119,40 @@ interface Props {
 
 export const ResourceBox = ({ image, title, caption, url, buttonText }: Props) => {
   return (
-    <ResourceBoxContainer>
-      <StyledImage src={image.src} alt={image.alt} />
+    <Container>
+      <StyledImage
+        src={image.src}
+        alt={image.alt}
+        height="150"
+        width="150"
+        fallbackWidth={150}
+        sizes={`(min-width: ${token("breakpoints.desktop")}) 150px, 200px`}
+      />
       <ContentWrapper>
-        <Title>{title}</Title>
-        <Caption>{caption}</Caption>
+        <Heading textStyle="label.large" fontWeight="bold" asChild consumeCss>
+          <h3>{title}</h3>
+        </Heading>
+        <StyledText textStyle="body.medium">{caption}</StyledText>
         <SafeLinkButton to={url} target="_blank" variant="secondary">
           {buttonText}
-          <Launch aria-hidden />
+          <Launch />
         </SafeLinkButton>
       </ContentWrapper>
-    </ResourceBoxContainer>
+    </Container>
   );
+  // return (
+  //   <ResourceBoxContainer>
+  //     <StyledImage src={image.src} alt={image.alt} />
+  //     <ContentWrapper>
+  //       <Title>{title}</Title>
+  //       <Caption>{caption}</Caption>
+  //       <SafeLinkButton to={url} target="_blank" variant="secondary">
+  //         {buttonText}
+  //         <Launch />
+  //       </SafeLinkButton>
+  //     </ContentWrapper>
+  //   </ResourceBoxContainer>
+  // );
 };
 
 export default ResourceBox;

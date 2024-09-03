31b5be34ff5670d4325ae222087c7604ebee7ec4
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-09-03 15:38:47 +0200

    Initial pass at new Error message primitive

---
+++ packages/primitives/src/ErrorMessage/ErrorMessage.stories.tsx
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { Meta, StoryObj } from "@storybook/react";
+import { LoginBoxLine, PresentationLine } from "@ndla/icons/common";
+import { SafeLink } from "@ndla/safelink";
+import {
+  ErrorMessageActions,
+  ErrorMessageContent,
+  ErrorMessageDescription,
+  ErrorMessageRoot,
+  ErrorMessageTitle,
+} from "./ErrorMessage";
+//@ts-ignore
+import Oops from "../../../../images/oops.gif";
+import { Button } from "../Button";
+
+export default {
+  title: "Primitives/ErrorMessage",
+  component: ErrorMessageRoot,
+  tags: ["autodocs"],
+  parameters: {
+    inlineStories: true,
+  },
+  render: (args) => (
+    <ErrorMessageRoot {...args}>
+      <img src={Oops} alt="Systemfeil" />
+      <ErrorMessageContent>
+        <ErrorMessageTitle>Oisann, her gikk noe galt</ErrorMessageTitle>
+        <ErrorMessageDescription>En kort beskrivelse av feilen som oppsto.</ErrorMessageDescription>
+      </ErrorMessageContent>
+      <ErrorMessageActions>
+        <Button variant="link" onClick={() => window.history.back()}>
+          Gå tilbake
+        </Button>
+        <SafeLink to="/">Gå til forsiden</SafeLink>
+      </ErrorMessageActions>
+    </ErrorMessageRoot>
+  ),
+} satisfies Meta<typeof ErrorMessageRoot>;
+
+export const Default: StoryObj<typeof ErrorMessageRoot> = {};
+
+export const AccessDenied: StoryObj<typeof ErrorMessageRoot> = {
+  render: (args) => (
+    <ErrorMessageRoot {...args}>
+      <PresentationLine css={{ width: "surface.xsmall", height: "surface.xsmall" }} />
+      <ErrorMessageContent>
+        <ErrorMessageTitle>Vi beklager, men denne ressursen er bare for lærere innlogget med Feide.</ErrorMessageTitle>
+      </ErrorMessageContent>
+      <ErrorMessageActions>
+        <Button>
+          Logg inn med Feide
+          <LoginBoxLine />
+        </Button>
+        <Button variant="link" onClick={() => window.history.back()}>
+          Gå tilbake
+        </Button>
+        <SafeLink to="/">Gå til forsiden</SafeLink>
+      </ErrorMessageActions>
+    </ErrorMessageRoot>
+  ),
+};

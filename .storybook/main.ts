5e438623c89b63f2c69313247919567c6ce07a4d
Katrine Wist <katrinew0702@gmail.com>
2024-09-03 11:52:32 +0200

    Initial setup panda

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -40,7 +40,8 @@ const config: StorybookConfig = {
           babel: {
             overrides: [
               {
-                exclude: /primitives|preset-panda|styled-system|ndla-icons|ndla-ui|audio-search|image-search/,
+                exclude:
+                  /primitives|preset-panda|styled-system|ndla-icons|ndla-ui|audio-search|image-search|video-search/,
                 plugins: [["@emotion", { autoLabel: "always" }]],
               },
             ],
@@ -50,9 +51,9 @@ const config: StorybookConfig = {
     });
   },
   typescript: {
-    reactDocgen: "react-docgen-typescript",
+    //reactDocgen: "react-docgen-typescript",
     // uncomment this for quicker HMR during dev.
-    // reactDocgen: "react-docgen",
+    reactDocgen: "react-docgen",
   },
   framework: "@storybook/react-vite",
   /* For at "Show code" skal legge seg bakerst slik at elementer som drop-down ikke blir skjult bak den */

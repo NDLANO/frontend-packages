0e4da1fc3ca91ae44bf2a7797e26135c7b771a64
Katrine Wist <katrinew0702@gmail.com>
2024-09-04 08:59:10 +0200

    Add modelRelease value translations to ndla-licenses

---
+++ packages/ndla-licenses/src/modelRelease.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { getLocaleOrDefault, Locale } from "./types";
+
+interface ModelReleaseLocaleInfo {
+  yes: string;
+  no: string;
+  "not-applicable": string;
+  "not-set": string;
+}
+
+interface ModelReleaseType {
+  [lang: string]: ModelReleaseLocaleInfo;
+}
+
+const modelRelease: ModelReleaseType = {
+  nb: { yes: "Ja", no: "Nei", "not-applicable": "Ikke relevant", "not-set": "Ikke valgt" },
+  nn: { yes: "Ja", no: "Nei", "not-applicable": "Ikkje relevant", "not-set": "Ikkje valgt" },
+  en: { yes: "Yes", no: "No", "not-applicable": "Not applicable", "not-set": "Not set" },
+};
+
+export const getModelReleaseValue = (modelReleaseValue: string, locale: string | Locale | undefined): string => {
+  const surelyLocale = getLocaleOrDefault(locale, "nb");
+  switch (modelReleaseValue.replace(/\s/g, "").toLowerCase()) {
+    case "yes":
+      return modelRelease[surelyLocale].yes;
+    case "no":
+      return modelRelease[surelyLocale].no;
+    case "not-applicable":
+      return modelRelease[surelyLocale]["not-applicable"];
+    case "not-set":
+      return modelRelease[surelyLocale]["not-set"];
+    default:
+      return modelReleaseValue;
+  }
+};

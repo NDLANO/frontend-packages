02ecf40f4dbb93d656df2624880b024b85c3ee79
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-09-25 13:58:01 +0200

    fix: wrap disclaimer content in a div to ensure proper article styling is applied to children

--- packages/preset-panda/src/globalCss.ts
+++ packages/preset-panda/src/globalCss.ts
@@ -77,7 +77,7 @@ export const globalCss = defineGlobalStyles({
       },
 
     // Article content is usually wrapped in a section. The rest of the elements in this list contains other elements, and should add margin to them no matter where they are placed in the DOM.
-    '& section:not([class]), section:not([class]) > div:not([class]), [data-embed-type="framed-content"], [data-embed-type="grid"] > div, [data-embed-type="grid-parallax"] > div, [data-embed-type="factbox"] > div, [data-embed-type="copyright"], [data-embed-type="uu-disclaimer"], [data-embed-wrapper], details, blockquote':
+    '& section:not([class]), section:not([class]) > div:not([class]), [data-embed-type="framed-content"], [data-embed-type="grid"] > div, [data-embed-type="grid-parallax"] > div, [data-embed-type="factbox"] > div, [data-embed-type="copyright"], [data-embed-type="uu-disclaimer"] > [data-uu-content], [data-embed-wrapper], details, blockquote':
       {
         '& > :is(h2, [data-embed-type="copy-heading"])': {
           marginBlockStart: "xlarge",
@@ -112,7 +112,7 @@ export const globalCss = defineGlobalStyles({
           marginBlockEnd: "xsmall",
         },
       },
-    '& [data-embed-type="framed-content"], [data-embed-type="grid"] > div, [data-embed-type="grid-parallax"] > div, [data-embed-type="factbox"] > div, [data-embed-type="copyright"], [data-embed-type="uu-disclaimer"], details, blockquote':
+    '& [data-embed-type="framed-content"], [data-embed-type="grid"] > div, [data-embed-type="grid-parallax"] > div, [data-embed-type="factbox"] > div, [data-embed-type="copyright"], [data-embed-type="uu-disclaimer"] > [data-uu-content], details, blockquote':
       {
         "& > :first-child": {
           marginBlockStart: "0",

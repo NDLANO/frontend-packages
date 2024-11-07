caacbeb3806e1155d0139fb82485b09d24ad2c28
Lars Sørlie <larssoerlie96@gmail.com>
2024-11-07 15:11:16 +0100

    export togglegorup item types

--- packages/primitives/src/index.ts
+++ packages/primitives/src/index.ts
@@ -37,7 +37,12 @@ export type { CardRootProps, CardVariantProps } from "./Card/Card";
 export { CardRoot, CardHeading, CardContent, CardImage } from "./Card/Card";
 
 export { ToggleGroupRoot, ToggleGroupItem } from "./ToggleGroup";
-export type { ToggleGroupRootProps, ToggleGroupVariantProps } from "./ToggleGroup";
+export type {
+  ToggleGroupRootProps,
+  ToggleGroupVariantProps,
+  ToggleGroupItemProps,
+  ToggleGroupItemVariantProps,
+} from "./ToggleGroup";
 
 export type { CheckboxVariantProps, CheckboxRootProps } from "./Checkbox";
 

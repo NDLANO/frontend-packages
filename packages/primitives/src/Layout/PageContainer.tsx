04a0baac8989ecd53aaa67aafb17db8e045c6816
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-11-21 13:01:43 +0100

    feat: add PageContainer

---
+++ packages/primitives/src/Layout/PageContainer.tsx
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2024-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { forwardRef } from "react";
+import { HTMLArkProps } from "@ark-ui/react";
+import { css, cva } from "@ndla/styled-system/css";
+import { JsxStyleProps, RecipeVariantProps } from "@ndla/styled-system/types";
+import { PageContent, PageContentVariantProps } from "./PageContent";
+
+const pageContainerRecipe = cva({
+  defaultVariants: {
+    padding: "medium",
+  },
+  variants: {
+    padding: {
+      none: {},
+      small: {
+        paddingBlockStart: "medium",
+        paddingBlockEnd: "5xlarge",
+      },
+      medium: {
+        paddingBlockStart: "xxlarge",
+        paddingBlockEnd: "5xlarge",
+      },
+      large: {
+        paddingBlockStart: "4xlarge",
+        paddingBlockEnd: "5xlarge",
+      },
+    },
+  },
+});
+
+export type PageContainerVariantProps = RecipeVariantProps<typeof pageContainerRecipe>;
+
+export type PageContainerProps = HTMLArkProps<"div"> &
+  JsxStyleProps &
+  PageContentVariantProps &
+  PageContainerVariantProps;
+
+export const PageContainer = forwardRef<HTMLDivElement, PageContainerProps>(
+  ({ padding, css: cssProp, ...props }, ref) => (
+    <PageContent css={css.raw(pageContainerRecipe.raw({ padding }), cssProp)} {...props} ref={ref} />
+  ),
+);

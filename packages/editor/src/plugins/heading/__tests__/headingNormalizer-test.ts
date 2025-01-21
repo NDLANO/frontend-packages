c75f9dde097070d68285957121483bce09ac2ea9
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-01-21 14:22:29 +0100

    port over heading tests

---
+++ packages/editor/src/plugins/heading/__tests__/headingNormalizer-test.ts
@@ -0,0 +1,173 @@
+/**
+ * Copyright (c) 2021-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { Transforms, type Descendant } from "slate";
+import { SECTION_ELEMENT_TYPE } from "../../section/sectionTypes";
+import { PARAGRAPH_ELEMENT_TYPE } from "../../paragraph/paragraphTypes";
+import { HEADING_ELEMENT_TYPE } from "../headingTypes";
+import { sectionPlugin } from "../../section/sectionPlugin";
+import { paragraphPlugin } from "../../paragraph/paragraphPlugin";
+import { headingPlugin } from "../headingPlugin";
+import { markPlugin } from "../../mark/markPlugin";
+import { createSlate } from "../../../utils/createSlate";
+import { softBreakPlugin } from "../../softBreak/softBreakPlugin";
+
+const editor = createSlate({ plugins: [sectionPlugin, paragraphPlugin, headingPlugin, markPlugin, softBreakPlugin] });
+
+describe("heading normalizer tests", () => {
+  test("unwrap empty header if not selected", () => {
+    const editorValue: Descendant[] = [
+      {
+        type: SECTION_ELEMENT_TYPE,
+        children: [
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+          {
+            type: HEADING_ELEMENT_TYPE,
+            level: 2,
+            children: [{ text: "" }],
+          },
+          {
+            type: HEADING_ELEMENT_TYPE,
+            level: 3,
+            children: [{ text: "not empty" }],
+          },
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+        ],
+      },
+    ];
+
+    const expectedValue: Descendant[] = [
+      {
+        type: SECTION_ELEMENT_TYPE,
+        children: [
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+          {
+            type: HEADING_ELEMENT_TYPE,
+            level: 3,
+            children: [{ text: "not empty" }],
+          },
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+        ],
+      },
+    ];
+    editor.children = editorValue;
+    editor.normalize({ force: true });
+    expect(editor.children).toEqual(expectedValue);
+  });
+
+  test("dont remove empty header if selected", () => {
+    const editorValue: Descendant[] = [
+      {
+        type: SECTION_ELEMENT_TYPE,
+        children: [
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+          {
+            type: HEADING_ELEMENT_TYPE,
+            level: 2,
+            children: [{ text: "" }],
+          },
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+        ],
+      },
+    ];
+
+    const expectedValue: Descendant[] = [
+      {
+        type: SECTION_ELEMENT_TYPE,
+        children: [
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+          {
+            type: HEADING_ELEMENT_TYPE,
+            level: 2,
+            children: [{ text: "" }],
+          },
+          {
+            type: PARAGRAPH_ELEMENT_TYPE,
+            children: [{ text: "" }],
+          },
+        ],
+      },
+    ];
+    editor.children = editorValue;
+    Transforms.select(editor, [0, 1, 0]);
+    editor.normalize({ force: true });
+    expect(editor.children).toEqual(expectedValue);
+  });
+});
+
+test("remove bold marker on header", () => {
+  const editorValue: Descendant[] = [
+    {
+      type: SECTION_ELEMENT_TYPE,
+      children: [
+        {
+          type: PARAGRAPH_ELEMENT_TYPE,
+          children: [{ text: "" }],
+        },
+        {
+          type: HEADING_ELEMENT_TYPE,
+          level: 2,
+          children: [{ text: "Test", bold: true }],
+        },
+        {
+          type: PARAGRAPH_ELEMENT_TYPE,
+          children: [{ text: "" }],
+        },
+      ],
+    },
+  ];
+
+  const expectedValue: Descendant[] = [
+    {
+      type: SECTION_ELEMENT_TYPE,
+      children: [
+        {
+          type: PARAGRAPH_ELEMENT_TYPE,
+          children: [{ text: "" }],
+        },
+        {
+          type: HEADING_ELEMENT_TYPE,
+          level: 2,
+          children: [{ text: "Test" }],
+        },
+        {
+          type: PARAGRAPH_ELEMENT_TYPE,
+          children: [{ text: "" }],
+        },
+      ],
+    },
+  ];
+  editor.children = editorValue;
+  editor.normalize({ force: true });
+  expect(editor.children).toEqual(expectedValue);
+});
---
+++ packages/editor/src/plugins/heading/__tests__/headingSerializer-test.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2021-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import type { Descendant } from "slate";
+import { SECTION_ELEMENT_TYPE } from "../../section/sectionTypes";
+import { HEADING_ELEMENT_TYPE } from "../headingTypes";
+import { blockContentToHTML } from "../../../serialization/html/serializeToHtml";
+import { blockContentToEditorValue } from "../../../serialization/html/deserializeFromHtml";
+
+const editor: Descendant[] = [
+  {
+    type: SECTION_ELEMENT_TYPE,
+    children: [
+      { type: HEADING_ELEMENT_TYPE, level: 1, children: [{ text: "title1" }] },
+      { type: HEADING_ELEMENT_TYPE, level: 2, children: [{ text: "title2" }] },
+      { type: HEADING_ELEMENT_TYPE, level: 3, children: [{ text: "title3" }] },
+      { type: HEADING_ELEMENT_TYPE, level: 4, children: [{ text: "title4" }] },
+      { type: HEADING_ELEMENT_TYPE, level: 5, children: [{ text: "title5" }] },
+      { type: HEADING_ELEMENT_TYPE, level: 6, children: [{ text: "title6" }] },
+    ],
+  },
+];
+
+const html =
+  "<section><h1>title1</h1><h2>title2</h2><h3>title3</h3><h4>title4</h4><h5>title5</h5><h6>title6</h6></section>";
+
+describe("heading serializing tests", () => {
+  test("serializing", () => {
+    const res = blockContentToHTML(editor);
+    expect(res).toMatch(html);
+  });
+
+  test("deserializing", () => {
+    const expected: Descendant[] = [
+      {
+        type: SECTION_ELEMENT_TYPE,
+        children: [
+          { type: HEADING_ELEMENT_TYPE, level: 2, children: [{ text: "title1" }] },
+          { type: HEADING_ELEMENT_TYPE, level: 2, children: [{ text: "title2" }] },
+          { type: HEADING_ELEMENT_TYPE, level: 3, children: [{ text: "title3" }] },
+          { type: HEADING_ELEMENT_TYPE, level: 4, children: [{ text: "title4" }] },
+          { type: HEADING_ELEMENT_TYPE, level: 4, children: [{ text: "title5" }] },
+          { type: HEADING_ELEMENT_TYPE, level: 4, children: [{ text: "title6" }] },
+        ],
+      },
+    ];
+    const res = blockContentToEditorValue(html);
+    expect(res).toEqual(expected);
+  });
+});

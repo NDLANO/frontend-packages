dd58bbfa9af15d660ff96ee67cad1273c3ee3dd0
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-01-22 12:36:32 +0100

    trigger break plugin when insertBreak is called

--- packages/editor/src/plugins/break/breakPlugin.ts
+++ packages/editor/src/plugins/break/breakPlugin.ts
@@ -6,14 +6,36 @@
  *
  */
 
+import { Node, Range, Transforms } from "slate";
 import { createPlugin } from "../../core/createPlugin";
+import { PARAGRAPH_ELEMENT_TYPE } from "../paragraph/paragraphTypes";
 import { BREAK_ELEMENT_TYPE } from "./breakTypes";
+import { getCurrentBlock } from "../../queries/getCurrentBlock";
 
 export const breakPlugin = createPlugin({
   type: BREAK_ELEMENT_TYPE,
   name: BREAK_ELEMENT_TYPE,
   isVoid: true,
-  // normalize: (editor, node, path) => {
-  //
-  // }
+  transform: (editor, logger) => {
+    const { insertBreak } = editor;
+
+    editor.insertBreak = () => {
+      if (!editor.selection || !Range.isRange(editor.selection)) return false;
+      // TODO: Should we consider void nodes here? We used to do that in the old implementation
+      const entry = getCurrentBlock(editor, PARAGRAPH_ELEMENT_TYPE);
+
+      if (entry && Node.string(entry[0]) === "") {
+        logger.log("Tried to insert new paragraph, but current paragraph is empty, inserting break instead");
+        Transforms.insertNodes(editor, [
+          { type: BREAK_ELEMENT_TYPE, children: [] },
+          { type: PARAGRAPH_ELEMENT_TYPE, children: [{ text: "" }] },
+        ]);
+        return;
+      }
+
+      return insertBreak();
+    };
+
+    return editor;
+  },
 });

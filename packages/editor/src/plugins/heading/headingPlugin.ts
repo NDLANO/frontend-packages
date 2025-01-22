b2518af8bbe650fa88b85da46f16f6ab3a741954
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-01-22 12:26:54 +0100

    simplify heading handling

--- packages/editor/src/plugins/heading/headingPlugin.ts
+++ packages/editor/src/plugins/heading/headingPlugin.ts
@@ -6,34 +6,27 @@
  *
  */
 
-import { Node, Path, Range, Text, Transforms } from "slate";
+import { Editor, Node, Point, Range, Text, Transforms } from "slate";
 import { createPlugin } from "../../core/createPlugin";
 import { HEADING_ELEMENT_TYPE } from "./headingTypes";
 import { isHeadingElement } from "./queries/headingQueries";
-import isHotkey from "is-hotkey";
-import { headingOnEnter } from "./handlers/headingOnEnter";
+import { getCurrentBlock } from "../../queries/getCurrentBlock";
+import type { Logger } from "../../core";
+
+const onDelete = (editor: Editor, logger: Logger) => {
+  if (!editor.selection || !Range.isCollapsed(editor.selection) || editor.selection.anchor.offset) return;
+  const [match] = editor.nodes({ match: isHeadingElement });
+  if (match?.[0] && Node.string(match[0]) === "") {
+    logger.log("Backspace in empty heading, unwrapping");
+    Transforms.unwrapNodes(editor);
+  }
+};
 
 export const headingPlugin = createPlugin({
   type: HEADING_ELEMENT_TYPE,
   name: HEADING_ELEMENT_TYPE,
-  shortcuts: {
-    // TODO: We used to have a backspace handler here that replaced empty headings with a paragraph. Do we want it?
-    headingOnEnter: {
-      keyCondition: isHotkey("enter"),
-      handler: headingOnEnter,
-    },
-  },
-  normalize: (editor, node, path, logger) => {
+  normalize: (editor, node, _path, logger) => {
     if (!isHeadingElement(node)) return false;
-
-    if (
-      Node.string(node) === "" &&
-      (!editor.selection || (Range.isCollapsed(editor.selection) && !Path.isCommon(path, editor.selection.anchor.path)))
-    ) {
-      logger.log("Removing empty heading that is not selected");
-      Transforms.unwrapNodes(editor, { at: path });
-      return true;
-    }
 
     const boldEntries = Array.from(editor.nodes({ match: (n) => Text.isText(n) && !!n.bold }), (n) => n);
     if (boldEntries.length) {
@@ -47,5 +40,43 @@ export const headingPlugin = createPlugin({
     }
 
     return false;
+  },
+  transform: (editor, logger) => {
+    const { deleteBackward, deleteFragment, insertBreak } = editor;
+    // Modify delete behavior when deleting the last character in a heading.
+    editor.deleteBackward = (unit) => {
+      editor.withoutNormalizing(() => {
+        deleteBackward(unit);
+        onDelete(editor, logger);
+      });
+    };
+
+    // Modify delete behavior when deleting remaining text in a heading.
+    editor.deleteFragment = (options) => {
+      editor.withoutNormalizing(() => {
+        deleteFragment(options);
+        onDelete(editor, logger);
+      });
+    };
+
+    editor.insertBreak = () => {
+      if (!editor.selection || !Range.isRange(editor.selection)) return false;
+
+      const entry = getCurrentBlock(editor, HEADING_ELEMENT_TYPE);
+      if (!entry) return insertBreak();
+
+      const [, path] = entry;
+
+      if (Point.equals(editor.end(path), editor.selection.anchor)) {
+        logger.log("Enter at end of heading, inserting paragraph after");
+        return Transforms.insertNodes(editor, { type: "paragraph", children: [{ text: "" }] });
+      } else if (Point.equals(editor.start(path), editor.selection.anchor)) {
+        logger.log("Enter at start of heading, inserting paragraph");
+        return Transforms.insertNodes(editor, { type: "paragraph", children: [{ text: "" }] }, { at: path });
+      }
+      return insertBreak();
+    };
+
+    return editor;
   },
 });

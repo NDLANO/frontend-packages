1e57be3879e08146aaada4f24353ce4356b20278
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-01-23 15:20:12 +0100

    document types

--- packages/editor/src/core/index.ts
+++ packages/editor/src/core/index.ts
@@ -21,17 +21,41 @@ export interface Logger {
 }
 
 export interface Shortcut {
+  /**
+   * The handler that should be called when the shortcut is triggered
+   * By returning true, you can signal that the event has been handled, and that further processing should be skipped.
+   */
   handler: ShortcutHandler;
+  /**
+   * A key condition that must be met for the handler to be called
+   */
   keyCondition: KeyConditionFn | KeyConditionFn[];
 }
 
 interface SlateCreatePluginProps<TType extends Element["type"]> {
+  /**
+   * The name of the plugin. Used for logging
+   */
   name: string;
   isVoid?: boolean;
   isInline?: boolean;
+  /**
+   * The type of the element that the plugin should handle
+   * Used for detecting whether the element is void, block or inline
+   */
   type?: TType;
+  /**
+   * A set of keyboard shortcuts that the plugin should handle
+   */
   shortcuts?: Record<string, Shortcut>;
+  /**
+   * A simple wrapper around the Slate normalizeNode method.
+   * By returning true, you can signal that the node has been normalized, and that further normalization should be skipped this iteration.
+   */
   normalize?: (editor: Editor, node: Node, path: Path, logger: Logger) => boolean;
+  /**
+   * A function that further transforms the editor. Typically used for extending the editor with functionality not supported by `createPlugin`.
+   */
   transform?: (editor: Editor, logger: Logger) => Editor;
 }
 

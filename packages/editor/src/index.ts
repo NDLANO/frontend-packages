7b28805ca0ec3b56be3f4a9203ec5acd9702e78a
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-01-30 13:57:36 +0100

    export plugin names

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -28,7 +28,7 @@ export { useEditorPopover } from "./hooks/useEditorPopover";
 
 export { breakPlugin } from "./plugins/break/breakPlugin";
 export { breakSerializer } from "./plugins/break/breakSerializer";
-export { BREAK_ELEMENT_TYPE } from "./plugins/break/breakTypes";
+export { BREAK_ELEMENT_TYPE, BREAK_PLUGIN, SOFT_BREAK_PLUGIN } from "./plugins/break/breakTypes";
 export type {
   BreakElementType,
   BreakElement,
@@ -41,14 +41,14 @@ export { softBreakPlugin } from "./plugins/break/softBreakPlugin";
 export { headingPlugin } from "./plugins/heading/headingPlugin";
 export { headingSerializer } from "./plugins/heading/headingSerializer";
 export type { HeadingElement, HeadingElementType } from "./plugins/heading/headingTypes";
-export { HEADING_ELEMENT_TYPE } from "./plugins/heading/headingTypes";
+export { HEADING_ELEMENT_TYPE, HEADING_PLUGIN } from "./plugins/heading/headingTypes";
 export { isHeadingElement } from "./plugins/heading/queries/headingQueries";
 export { toggleHeading } from "./plugins/heading/transforms/toggleHeading";
 
 export { linkPlugin } from "./plugins/link/linkPlugin";
 export { linkSerializer } from "./plugins/link/linkSerializer";
 export type { LinkElement, LinkElementType, LinkEmbedData } from "./plugins/link/linkTypes";
-export { LINK_ELEMENT_TYPE } from "./plugins/link/linkTypes";
+export { LINK_ELEMENT_TYPE, LINK_PLUGIN } from "./plugins/link/linkTypes";
 export { isLinkElement } from "./plugins/link/queries/linkQueries";
 
 export { listPlugin } from "./plugins/list/listPlugin";
@@ -63,7 +63,7 @@ export type {
   ListPluginOptions,
   ListSerializerOptions,
 } from "./plugins/list/listTypes";
-export { LIST_ELEMENT_TYPE, LIST_ITEM_ELEMENT_TYPE, LIST_TYPES } from "./plugins/list/listTypes";
+export { LIST_ELEMENT_TYPE, LIST_ITEM_ELEMENT_TYPE, LIST_TYPES, LIST_PLUGIN } from "./plugins/list/listTypes";
 export { isListElement, isListItemElement } from "./plugins/list/queries/listElementQueries";
 export { toggleList } from "./plugins/list/transforms/toggleList";
 export { useListToolbarButton } from "./plugins/list/hooks/useListToolbarButton";
@@ -74,13 +74,13 @@ export { listOnTab } from "./plugins/list/handlers/listOnTab";
 export { markPlugin } from "./plugins/mark/markPlugin";
 export { markSerializer } from "./plugins/mark/markSerializer";
 export type { CustomText, MarkType } from "./plugins/mark/markTypes";
-export { marks } from "./plugins/mark/markTypes";
+export { marks, MARK_PLUGIN } from "./plugins/mark/markTypes";
 export { toggleMark } from "./plugins/mark/toggleMark";
 export { useMarkToolbarButton } from "./plugins/mark/hooks/useMarkToolbarButton";
 
 export { paragraphPlugin } from "./plugins/paragraph/paragraphPlugin";
 export { paragraphSerializer } from "./plugins/paragraph/paragraphSerializer";
-export { PARAGRAPH_ELEMENT_TYPE } from "./plugins/paragraph/paragraphTypes";
+export { PARAGRAPH_ELEMENT_TYPE, PARAGRAPH_PLUGIN } from "./plugins/paragraph/paragraphTypes";
 export type {
   ParagraphElement,
   ParagraphElementType,
@@ -89,7 +89,7 @@ export type {
 
 export { sectionPlugin } from "./plugins/section/sectionPlugin";
 export { sectionSerializer } from "./plugins/section/sectionSerializer";
-export { SECTION_ELEMENT_TYPE } from "./plugins/section/sectionTypes";
+export { SECTION_ELEMENT_TYPE, SECTION_PLUGIN } from "./plugins/section/sectionTypes";
 export type { SectionElement, SectionElementType, SectionPluginOptions } from "./plugins/section/sectionTypes";
 export { isSectionElement } from "./plugins/section/queries/sectionQueries";
 

aaab5ec368c77766e92134776759e31f47b303a1
Gunnar Velle <gunnar.velle@gmail.com>
2025-04-10 13:38:05 +0200

    Add target blank icon

--- packages/ndla-ui/src/index.ts
+++ packages/ndla-ui/src/index.ts
@@ -132,6 +132,8 @@ export type { Article as ArticleType, HeadingLevel } from "./types";
 
 export { CodeBlock, codeLanguageOptions } from "./CodeBlock";
 
+export { TargetBlankIcon } from "./TargetBlankIcon";
+
 export { ZendeskButton } from "./ZendeskButton/ZendeskButton";
 
 export type { ZendeskButtonProps } from "./ZendeskButton/ZendeskButton";

07937e1196df032b45a987c65b89e7cff9e68ec4
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-06-05 10:39:50 +0200

    feat: replace alt with ⌥ on Mac

--- packages/editor-components/src/utils/platformSpecificTooltip.ts
+++ packages/editor-components/src/utils/platformSpecificTooltip.ts
@@ -10,7 +10,17 @@
 export const IS_MAC = typeof window != "undefined" && /Mac|iPod|iPhone|iPad/.test(window.navigator.platform);
 
 /**
- * Converts `mod`-instances with platform-specific key name.
+ * Converts `mod` and `alt`-instances with platform-specific key names.
  * `mod+s` => `⌘+s` on Mac, `Ctrl+s` on Windows.
+ * `alt+f` => `⌥+f` on Mac, `Alt+f` on Windows.
  */
-export const platformSpecificTooltip = (shortcut: string) => shortcut.replace("mod", IS_MAC ? "⌘" : "Ctrl");
+export const platformSpecificTooltip = (shortcut: string) => {
+  let result = shortcut;
+  if (result.includes("mod")) {
+    result = result.replace("mod", IS_MAC ? "⌘" : "Ctrl");
+  }
+  if (result.includes("alt")) {
+    result = result.replace("alt", IS_MAC ? "⌥" : "Alt");
+  }
+  return result;
+};

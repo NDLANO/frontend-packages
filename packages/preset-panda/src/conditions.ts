f78af979a630eae0352b13365d0be303713b7e92
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-09-29 15:26:57 +0200

    chore: remove user-defined condition that is now built-in

--- packages/preset-panda/src/conditions.ts
+++ packages/preset-panda/src/conditions.ts
@@ -6,11 +6,8 @@
  *
  */
 
-// TODO: We might be able to get rid of some of these. See conditions.d.ts
-
 export const conditions = {
   chinese: "&:where([lang='zh'], [lang='zh-Hans'], [lang='zh-Hant']):not([data-pinyin])",
   ariaInvalid: "&:has([aria-invalid='true']), &[aria-invalid='true']",
-  disabled: "&:is(:disabled, [disabled], [data-disabled], [aria-disabled='true'])",
   on: '&:is([data-state="on"])',
 };

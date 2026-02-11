f5760d7a8925d4c3880989e9e8e983423206bdb9
Jonas Carlsen <jonas.carlsen@knowit.no>
2026-02-11 10:25:15 +0100

    chore: use mjs for postcss

---
+++ postcss.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import panda from "@pandacss/dev/postcss";
+
+export default {
+  plugins: [panda()],
+};
